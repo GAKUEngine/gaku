8294278a6b538e55ce992a36c4a66a399e9b5485
影月 零 <zero@genshin.org>
2012-01-29 20:09:04 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GAKUEngine::Application.config.secret_token = '4b0c904de16ccc2430400b4786596340b984ea00b4a6ad726dc686183839e1fd30c9456787b2626c98b07ca684611295a7b2016ecfbe7598626a47ded59cba59'
