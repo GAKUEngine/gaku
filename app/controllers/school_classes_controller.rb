9232b3bdb4cff8efb6d7656b0f3e024f9441350f
影月 零 <zero@genshin.org>
2012-02-04 16:49:20 +0900

    エラー修正と改善

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,6 +14,7 @@ class SchoolClassesController < ApplicationController
   # GET /school_classes/1.json
   def show
     @school_class = SchoolClass.find(params[:id])
+    @syllabus = Syllabus.find(@school_class.syllabus_id)
 
     respond_to do |format|
       format.html # show.html.erb
