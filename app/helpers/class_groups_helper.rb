8da784200c96a9f1d1d143679ded4060edc7624a
影月 零 <zero@genshin.org>
2012-02-19 00:54:44 +0900

    綺麗に

--- app/helpers/class_groups_helper.rb
+++ app/helpers/class_groups_helper.rb
@@ -1,5 +1,2 @@
 module ClassGroupsHelper
-  def link_to_enroll_student(name, class_group_num, f)
-    link_to name, {:controller => :class_group_enrollments, :action => :new, :format => :html, :active => "class_group", :idnum => class_group_num}, :remote => true, :id => f
-  end
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,2 @@
 module CoursesHelper
-  def link_to_enroll_student(name, coursenum, f)
-    link_to name, {:controller => :course_enrollments, :action => :new, :format => :html, :active => "course", :idnum => coursenum}, :remote => true, :id => f
-  end
 end
