33895c37abd061fa34cc6ba40fa525f9fa65dd9f
影月 零 <zero@genshin.org>
2012-03-02 05:18:15 +0900

    少しだけ調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,4 @@
 //= require wijmo/jquery.wijmo.wijpopup.js
 
 // require jquery.wijmo-open.all.2.0.0.min.js
-//= require jquery.wijmo-complete.all.2.0.0.min.js
+// require jquery.wijmo-complete.all.2.0.0.min.js
