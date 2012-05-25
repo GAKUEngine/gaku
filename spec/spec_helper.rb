ad3a238bf388376ff42cd6b8c970c8fa80c1e5b0
Vassil Kalkov <vassilkalkov@gmail.com>
2012-05-25 09:06:18 +0300

    Edit spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,21 +3,20 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'database_cleaner'
+require 'active_record/fixtures'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+# include local factories
+Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each { |f| require File.expand_path(f)}
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+RSpec.configure do |config|
+
+  config.mock_with :rspec
+  
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
