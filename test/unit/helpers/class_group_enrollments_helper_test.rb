b795b5366cb426132dfffa1bb6f6a6ee20e615c9
Vassil Kalkov <vassilkalkov@gmail.com>
2012-05-27 13:06:57 +0300

    Remove test unit

--- test/unit/helpers/class_group_enrollments_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ClassGroupEnrollmentsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/class_groups_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ClassGroupsHelperTest < ActionView::TestCase
-end
