a24c71a75cf69aa30376ff055b33e76a6e41f682
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 16:25:56 +0300

    [spec/controllers] Added create and a pending update to students_controller_spec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe StudentsController do
 
-  let(:student) { FactoryGirl.build_stubbed(:student) }
+  let!(:student) { FactoryGirl.build_stubbed(:student) }
 
   before do
     login_admin
@@ -18,4 +18,24 @@ describe StudentsController do
       response.should be_success
     end
   end 
+
+  describe "POST create" do
+    it "redirects to the new page" do
+      page.stub :save => true
+
+      post :create
+      created_student = Student.last
+      response.should redirect_to(student_url(created_student))
+    end
+  end
+
+  describe "PUT update" do
+    pending "redirects to the page" do
+      page.stub :update_attributes => true
+
+      post :update, :id => student.id
+
+      response.should redirect_to(student_url(student))
+    end
+  end
 end
