9e40ca51be19a6041be6866189c8825fe397611d
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 09:33:37 +0300

    [travis] Revert all migration fields for user

--- db/migrate/20120216051841_devise_create_users.rb
+++ db/migrate/20120216051841_devise_create_users.rb
@@ -1,9 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users  do |t|
-      t.string :email
-      t.string :encrypted_password
-      t.string :reset_password_token
       t.recoverable
       t.rememberable
       t.trackable
