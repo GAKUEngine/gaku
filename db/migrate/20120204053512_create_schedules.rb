2bcfb82a9cd77f4a468c99d28709c9202e4cc3ee
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 11:10:35 +0300

    [migrations] Change "end" to "stop" in schedule migration, because "end" is reserved word in ruby

--- db/migrate/20120204053512_create_schedules.rb
+++ db/migrate/20120204053512_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration
   def change
     create_table :schedules do |t|
       t.datetime :start
-      t.datetime :end
+      t.datetime :stop
       t.string :repeat
 
       t.timestamps
