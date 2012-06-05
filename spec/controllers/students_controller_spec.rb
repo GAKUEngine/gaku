ae52c6dec85c3567ee7b8665176c53fc9d2bdcd7
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 17:00:42 +0300

    [spec/controllers] Mark as pending destroy action of students_controller_spec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -41,7 +41,7 @@ describe StudentsController do
       controller.should_not set_the_flash
     end
 
-    it "sets the flash" do
+    pending "sets the flash" do
       delete :destroy, :id => student
       controller.should set_the_flash
     end
