c54a370e25827f126a623d444b06445a08a47a86
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 09:09:54 +0300

    [travis] Try to fix users migration

--- db/migrate/20120216051841_devise_create_users.rb
+++ db/migrate/20120216051841_devise_create_users.rb
@@ -1,14 +1,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users  do |t|
-      t.recoverable
-      t.rememberable
-      t.trackable
+      t.string :email
+      t.string :encrypted_password
+      t.string :reset_password_token
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
   end
-
 end
