6617605e92f9aa61c30d82d2f48b08902b0d030e
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-05 17:06:55 +0300

    [spec/controllers] Added update and delete to exams_controller_spec

--- spec/controllers/exams_controller_spec.rb
+++ spec/controllers/exams_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ExamsController do
 
-  let(:exam) { FactoryGirl.build_stubbed(:exam) }
+  let(:exam) { FactoryGirl.create(:exam) }
 
   before do
     login_admin
@@ -21,6 +21,29 @@ describe ExamsController do
 
       post :create
       response.should redirect_to(exam_url(Exam.last))
+    end
+  end
+
+  describe "PUT update" do
+
+    it "redirects to the exam" do
+      page.stub :update_attributes => true
+
+      post :update, :id => exam.id
+      response.should redirect_to(exam_url(exam))
+    end
+  end
+
+  describe "destroying an exam" do
+
+    it "doesn't set the flash on xhr requests'" do
+      xhr :delete, :destroy, :id => exam
+      controller.should_not set_the_flash
+    end
+
+    pending "sets the flash" do
+      delete :destroy, :id => exam
+      controller.should set_the_flash
     end
   end
 end
