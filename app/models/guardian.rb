3cd692e23b4793aaca06a46252167e0f5dac6e35
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-07 18:06:29 +0300

    Fix mistake in connections

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -2,7 +2,7 @@ class Guardian < ActiveRecord::Base
   belongs_to :user
   belongs_to :profile
   has_and_belongs_to_many :addresses
-  has_many :profiles
+  has_many :contacts
 
   attr_accessible :relationship, :profile_id, :user_id
 end
