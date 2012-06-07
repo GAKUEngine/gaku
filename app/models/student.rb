7f730321e791a5ace023ded15fd2f5c0a95d0e95
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-07 21:49:44 +0300

    Link ClassGroups with Students, clean models/specs

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   has_many :course_enrollments
   has_many :courses, :through => :course_enrollments
-  #has_and_belongs_to_many :class_groups
+  has_and_belongs_to_many :class_groups
 
   has_many :exams, :through => :exam_scores
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607182411) do
+ActiveRecord::Schema.define(:version => 20120607184543) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(:version => 20120607182411) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "class_groups_students", :force => true do |t|
+    t.integer "class_group_id"
+    t.integer "student_id"
+  end
+
   create_table "contact_types", :force => true do |t|
     t.string "name"
   end
@@ -86,9 +91,8 @@ ActiveRecord::Schema.define(:version => 20120607182411) do
 
   create_table "courses", :force => true do |t|
     t.string   "code"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "syllabus_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "exam_scores", :force => true do |t|
