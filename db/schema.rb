a46861ee108a62823fcaebd48fb9247e044c52dd
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-07 17:53:35 +0300

    Create Guardian model, spec/model, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606184836) do
+ActiveRecord::Schema.define(:version => 20120607144949) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -100,6 +100,10 @@ ActiveRecord::Schema.define(:version => 20120606184836) do
     t.binary   "data"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "guardians", :force => true do |t|
+    t.string "relationship"
   end
 
   create_table "preferences", :force => true do |t|
