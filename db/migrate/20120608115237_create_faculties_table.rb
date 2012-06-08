77a4ba692c5dfe5f500a967e5e6c5847e6308c67
Kiril Momchilov <momchilov@gmail.com>
2012-06-08 16:22:40 +0300

    [migration] renameing faculty to faculties

---
+++ db/migrate/20120608115237_create_faculties_table.rb
@@ -0,0 +1,7 @@
+class CreateFacultesTable < ActiveRecord::Migration
+  def change
+    create_table :faculties do |t|
+      
+    end
+  end
+end
--- db/migrate/20120608115237_create_faculty_table.rb
+++
@@ -1,5 +0,0 @@
-class CreateFacultyTable < ActiveRecord::Migration
-  def change
-
-  end
-end
