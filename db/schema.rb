f8f214ee276f2fe028cd44e47acd16dde36cc099
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-08 15:45:02 +0300

    Link Student to ExamScores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120608113007) do
+ActiveRecord::Schema.define(:version => 20120608121113) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(:version => 20120608113007) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "exam_id"
+    t.integer  "student_id"
   end
 
   create_table "exams", :force => true do |t|
