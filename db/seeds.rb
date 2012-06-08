3a5fde15aa42763c8878d3e808a30456bea386a5
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-08 10:01:15 +0300

    Clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 # Loads seed data out of default dir
 default_path = File.join(File.dirname(__FILE__), 'default')
 
