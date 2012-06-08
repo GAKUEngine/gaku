f7dba6de76ab57e8a54f3fcb08d03db7d25865b3
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-08 16:49:26 +0300

    Link ExamPortionScores to Exam

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -5,6 +5,7 @@ describe Exam do
     it { should have_valid_factory(:exam) }
     it { should have_many(:exam_scores) }
     it { should have_many(:exam_portions) }
+    it { should have_many(:exam_portion_scores) }
     it { should belong_to(:schedule) }
   end
 end
