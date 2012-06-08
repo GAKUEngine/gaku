1c3935c2fb58456f09bbafb80eb6dfa89e5ff9f4
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-08 14:48:44 +0300

    [factory] Link note and student factories

--- spec/factories/note_factory.rb
+++ spec/factories/note_factory.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :note do
     title "Excellent"
     content "Excellent student"
+    association :student
   end
 end
--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     address { Faker::Address::street_address }
     phone { Faker::PhoneNumber::phone_number }
     email { Faker::Internet::email }
+    after_create { |student| FactoryGirl.create(:note, :student => student) }
   end
 end
