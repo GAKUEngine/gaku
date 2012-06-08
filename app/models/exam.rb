1e34ca274a0152916cdcad9cfcfc30774c2aca75
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-08 15:07:59 +0300

    Link Exam to ExamScores

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,5 @@
 class Exam < ActiveRecord::Base
   belongs_to :course
+  has_many :exam_scores
   attr_accessible :name, :problem_count, :max_score, :weight, :data
 end
--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Exam do
   context "validations" do 
     it { should have_valid_factory(:exam) }
+    it { should have_many(:exam_scores) }
   end
 end
