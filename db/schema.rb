e1e2459c52bc49ebc8a019a79c32647a80c23d01
Kiril Momchilov <momchilov@gmail.com>
2012-06-09 21:31:53 +0300

    Linking Faculty to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120609175618) do
+ActiveRecord::Schema.define(:version => 20120609182941) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -209,6 +209,7 @@ ActiveRecord::Schema.define(:version => 20120609175618) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.integer  "faculty_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
