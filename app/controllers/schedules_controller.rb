476ad5060df51178dea91c86f8f7c4b2f0d59fbf
Kiril Momchilov <momchilov@gmail.com>
2012-06-12 19:23:44 +0300

    Added Schedules controller

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,12 @@
+class SchedulesController < ApplicationController
+  before_filter :authenticate_user!
+  inherit_resources
+
+  actions :index, :show, :new, :create, :update, :edit, :destroy
+
+  def destroy
+    destroy! :flash => !request.xhr?
+  end
+  
+end
+
