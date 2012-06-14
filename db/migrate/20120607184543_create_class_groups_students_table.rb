7a4d622b874f58434a31112f9ad9e587d340ec5e
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-14 11:23:49 +0300

    [migration] Delete create_class_groups_students

--- db/migrate/20120607184543_create_class_groups_students_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateClassGroupsStudentsTable < ActiveRecord::Migration
-  def change
-  	create_table :class_groups_students do |t|
-      t.references :class_group
-      t.references :student
-  	end
-  end
-end
