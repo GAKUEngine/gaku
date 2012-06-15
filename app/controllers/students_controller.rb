9734c49e6929427ac9690205054bf24433309048
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-15 06:31:34 +0300

    students_controller : Refectored to before_filter

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,14 +6,17 @@ class StudentsController < ApplicationController
 
   actions :index, :show, :new, :create, :update, :edit, :destroy
 
+  before_filter :load_class_groups, :only => [:new, :edit]
+
   def destroy
     destroy! :flash => !request.xhr?
   end
 
-  def new
-  	@student = Student.new
-  	@class_groups = ClassGroup.all
-  	@class_group_id = params[:class_group_id].nil? ? 'nil' : params[:class_group_id]
-  end
-  
+  private
+
+    def load_class_groups
+      @class_groups = ClassGroup.all
+  	  @class_group_id = params[:class_group_id].nil? ? 'nil' : params[:class_group_id]
+    end
+
 end
