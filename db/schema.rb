bc4abc2bcb255ef84eb64e97615f8229d4b0852a
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-20 16:54:45 +0300

    [migration] Add gender field to Student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611125810) do
+ActiveRecord::Schema.define(:version => 20120620135227) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(:version => 20120611125810) do
     t.integer  "user_id"
     t.integer  "profile_id"
     t.integer  "faculty_id"
+    t.string   "gender"
   end
 
   create_table "syllabuses", :force => true do |t|
