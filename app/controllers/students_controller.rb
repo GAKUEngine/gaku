1aafa1105aee43c638c3542a563351cd9f354098
Nakaya Yukiharu <snowsunny1107@gmail.com>
2012-06-22 12:42:56 +0900

    Fix students/index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,14 @@ class StudentsController < ApplicationController
 
   before_filter :load_class_groups, :only => [:new, :edit]
 
+  def index
+    @students = Student.all
+    respond_to do |format|
+      format.html
+      format.json {render :json => @students.to_json}
+    end
+  end
+
   def destroy
     destroy! :flash => !request.xhr?
   end
