864eaf173553ab81b4ace25f7510f8f1d615f417
Kiril Momchilov <momchilov@gmail.com>
2012-06-22 14:07:36 +0300

    fixes #27

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611125810) do
+ActiveRecord::Schema.define(:version => 20120620141755) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(:version => 20120611125810) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "locale"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
