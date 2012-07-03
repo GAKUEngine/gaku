4ee74daf7379aaf15760f7791609417417a78a4d
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-03 07:38:14 +0300

    [Semester] Add timestamps

--- db/migrate/20120703041436_create_semesters_table.rb
+++ db/migrate/20120703041436_create_semesters_table.rb
@@ -1,8 +1,10 @@
 class CreateSemestersTable < ActiveRecord::Migration
   def change
- 	  create_table :semesters do |t|
- 		  t.date :starting
- 		  t.date :ending
- 	  end
+    create_table :semesters do |t|
+      t.date :starting
+ 	  t.date :ending
+
+ 	  t.timestamps
+ 	end
   end
 end
