b877ce8599d8d34e9c68e6e49db49ab6b6a06a9f
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-07 13:46:52 +0300

    Schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20120705094337) do
+ActiveRecord::Schema.define(:version => 20120706120900) do
 
   create_table "address_histories", :force => true do |t|
     t.string  "address1"
