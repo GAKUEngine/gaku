d98f9a4b83817b708c55487b57f16d5498cf8699
Georgi Tapalilov <tapalilov@gmail.com>
2012-07-09 17:31:51 +0300

    CLEAN ADDRESS_HISTORY TABLE

--- db/migrate/20120705094337_create_address_history_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateAddressHistoryTable < ActiveRecord::Migration
-	  def change
-	  	create_table :address_histories do |t|
-		  	t.string   :address1, :address2, :city, :zipcode, :state, :state_name
-		  	t.references :country, :state, :faculty, :address 
-  	end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20120705094337) do
-
-  create_table "address_histories", :force => true do |t|
-    t.string  "address1"
-    t.string  "address2"
-    t.string  "city"
-    t.string  "zipcode"
-    t.string  "state"
-    t.string  "state_name"
-    t.integer "country_id"
-    t.integer "state_id"
-    t.integer "faculty_id"
-    t.integer "address_id"
-  end
+ActiveRecord::Schema.define(:version => 20120706120900) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -158,6 +144,7 @@ ActiveRecord::Schema.define(:version => 20120705094337) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.integer  "schedule_id"
+    t.integer  "course_id"
   end
 
   create_table "exams_syllabuses", :force => true do |t|
@@ -215,9 +202,9 @@ ActiveRecord::Schema.define(:version => 20120705094337) do
   end
 
   create_table "roles", :force => true do |t|
+    t.integer "faculty_id"
     t.string  "name"
     t.integer "class_group_enrollment_id"
-    t.integer "faculty_id"
   end
 
   create_table "schedules", :force => true do |t|
@@ -246,8 +233,6 @@ ActiveRecord::Schema.define(:version => 20120705094337) do
     t.string   "name"
     t.string   "surname"
     t.string   "name_reading"
-    t.string   "surname_reading"
-    t.string   "gender"
     t.string   "phone"
     t.string   "email"
     t.date     "birth"
@@ -258,15 +243,17 @@ ActiveRecord::Schema.define(:version => 20120705094337) do
     t.integer  "user_id"
     t.integer  "profile_id"
     t.integer  "faculty_id"
+    t.string   "gender"
+    t.string   "surname_reading"
   end
 
   create_table "syllabuses", :force => true do |t|
     t.string   "name"
-    t.string   "code"
     t.text     "description"
     t.integer  "credits"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "code"
   end
 
   create_table "users", :force => true do |t|
