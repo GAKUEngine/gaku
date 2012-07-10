48d680110365f586cf5589c24935aafd05f5d902
Georgi Tapalilov <tapalilov@gmail.com>
2012-07-10 12:36:49 +0300

    [test] select syllabus in new course form and create new course

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -5,6 +5,7 @@ describe 'Courses' do
     @course = Factory(:course)
     sign_in_as!(Factory(:user))
     within('ul#menu') { click_link "Course List"}
+    @syllabus = Factory(:syllabus)
   end
 
   context "listing courses" do
@@ -16,8 +17,10 @@ describe 'Courses' do
   context "creating new course" do 
     it "should create new course" do 
       click_link "new_course_link"
-      #page.should have_content "was successfully created"
-
+      fill_in 'course_code', :with => 'SUMMER2012'
+      page.select "#{@syllabus.name}", :from => 'course_syllabus_id'
+      click_button 'Create Course'
+      page.should have_content "was successfully created"
     end
   end
 end
