4190cc9af2d2d8de4a07b0012b6e2efc84a623b7
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-11 19:11:31 +0300

    [Syllabus] Add exams to attr_accessible

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -3,5 +3,5 @@ class Syllabus < ActiveRecord::Base
   has_many :assignments
   has_and_belongs_to_many :exams
 
-  attr_accessible :name, :code, :description, :credits
+  attr_accessible :name, :code, :description, :credits, :exams
 end
