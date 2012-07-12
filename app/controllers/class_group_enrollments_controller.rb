24bed6763e3661faae66fca25a2bdfb1e67bdc20
Georgi Tapalilov <tapalilov@gmail.com>
2012-07-12 12:25:24 +0300

    [ajax/wip] add class group enrollment to class group

--- app/controllers/class_group_enrollments_controller.rb
+++ app/controllers/class_group_enrollments_controller.rb
@@ -3,5 +3,11 @@ class ClassGroupEnrollmentsController < ApplicationController
   inherit_resources
 
   actions :show, :new, :create, :update, :edit, :destroy
+
+  def create
+    super do |format|
+      format.js {render 'create'}
+    end  
+  end
   
 end
