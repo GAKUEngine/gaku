eb3b271f5ac7aff8f7342c88771833eb737a442c
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-16 19:50:07 +0300

    Create AssignmentScore model

---
+++ app/models/assignment_score.rb
@@ -0,0 +1,2 @@
+class AssignmentScore < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716163044) do
+ActiveRecord::Schema.define(:version => 20120716164612) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(:version => 20120716163044) do
   create_table "addresses_students", :force => true do |t|
     t.integer "student_id"
     t.integer "address_id"
+  end
+
+  create_table "assignment_scores", :force => true do |t|
+    t.integer  "score"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "assignments", :force => true do |t|
