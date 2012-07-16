5da88a9349207c9d27aad7eb265d0075e2fbb381
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-16 18:37:52 +0300

    [spec/models] Create grading_method spec

---
+++ spec/factories/grading_method_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :grading_method do
+    name "Method 1"
+    method "Method"
+    description "Grading method description"
+  end
+end
---
+++ spec/models/grading_method_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe GradingMethod do
+
+  context "validations" do 
+  	it { should have_valid_factory(:grading_method) }
+  end
+  
+end
