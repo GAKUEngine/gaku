3bb004cedb0687384ad5b5e1a6303a8d6c35a623
Georgi Tapalilov <tapalilov@gmail.com>
2012-07-17 17:50:11 +0300

    [ajax] ajax add student to course/course enrollment/

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
 
+	before_filter :load_before_show, :only => :show
   #before_filter :authenticate_user!
 
   inherit_resources
@@ -10,4 +11,10 @@ class CoursesController < ApplicationController
     destroy! :flash => !request.xhr?
   end
   
+private
+	
+	def load_before_show
+		@new_course_enrollment = CourseEnrollment.new
+	end
+
 end
