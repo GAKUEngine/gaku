478c791b4b2a3a853614196c95750db414846bf5
snowsunny <snowsunny1107@gmail.com>
2012-07-19 11:44:08 +0900

    merge fix

--- db/schema.rb
+++ db/schema.rb
@@ -154,12 +154,10 @@ ActiveRecord::Schema.define(:version => 20120718045347) do
     t.text     "description"
     t.integer  "problem_count"
     t.float    "weight"
-    t.binary   "data"
-    t.datetime "execution_date"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
-    t.integer  "schedule_id"
-    t.text     "adjustments"
+    t.boolean  "dynamic_scoring"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.integer  "grading_method_id"
   end
 
   create_table "exams_syllabuses", :force => true do |t|
