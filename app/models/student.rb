5d718bcc0193ae597384cc64ee8feb38e698c44f
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-19 09:54:17 +0300

    [Student] Change birth to birth_date

--- app/models/student.rb
+++ app/models/student.rb
@@ -16,7 +16,7 @@ class Student < ActiveRecord::Base
   has_many :contacts
   has_many :notes
 
-  attr_accessible :name, :surname, :name_reading, :surname_reading, :phone, :email, :birth, :gender, :admitted, :graduated,
+  attr_accessible :name, :surname, :name_reading, :surname_reading, :phone, :email, :birth_date, :gender, :admitted, :graduated,
                   :class_groups, :class_group_ids, :class_groups_attributes,
                   :guardians, :guardians_attributes, :notes, :notes_attributes, :addresses, :addresses_attributes, 
                   :picture
