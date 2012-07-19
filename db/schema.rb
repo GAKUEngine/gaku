2879c0091e7f9134bd68f1e2d3ee7d19970cadac
snowsunny <snowsunny1107@gmail.com>
2012-07-20 02:27:19 +0900

    fix view student/show

--- db/schema.rb
+++ db/schema.rb
@@ -279,7 +279,6 @@ ActiveRecord::Schema.define(:version => 20120718045347) do
     t.integer  "hours"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "level"
   end
 
   create_table "users", :force => true do |t|
