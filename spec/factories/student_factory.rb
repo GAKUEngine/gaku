bf7115ef408c7fe262cba1ea7c41039fa09b9ec2
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-20 10:31:09 +0300

    [Factory] Add name_reading and surname_reading to student factory

--- spec/factories/student_factory.rb
+++ spec/factories/student_factory.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :student do
     name { Faker::Name.first_name }
     surname { Faker::Name.last_name }
+    name_reading { Faker::Name.first_name }
+    surname_reading { Faker::Name.last_name }
     phone { Faker::PhoneNumber::phone_number }
     email { Faker::Internet::email }
     after_create { |student| FactoryGirl.create(:note, :student => student) }
