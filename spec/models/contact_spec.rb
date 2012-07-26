a50cd92f9b14839a29f9de572c40b9e3781a70d6
Georgi Tapalilov <tapalilov@gmail.com>
2012-07-26 13:00:57 +0300

    [model] add logic for make first contact primary and model spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Contact do
 
+	let(:student) {Factory(:student)}
+
   context "validations" do 
   	it { should have_valid_factory(:contact) }
     it { should belong_to(:contact_type) }
@@ -9,4 +11,20 @@ describe Contact do
     it { should belong_to(:guardian) }
   end
   
+
+  context 'make first student contact primary on create' do
+  	it "should first student address be primary" do
+  		contact = FactoryGirl.build(:contact, :student_id => student.id)
+  		contact.save
+  	  contact.should be_is_primary
+  	end
+
+  	it "should next student address not be primary on create" do
+  		contact = Factory.create(:contact, :student_id => student.id)
+  	  contact2 = FactoryGirl.build(:contact, :student_id => student.id)
+  	  contact2.save
+  	  contact2.should_not be_is_primary 
+  	end
+  end
+
 end
