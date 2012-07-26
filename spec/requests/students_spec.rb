a4cfc7c6d8e6407ebeef4bb110ebc405161aeaf6
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-07-26 11:34:46 +0300

    [rspec] requests/students add students/show tabs - edit options tests

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -37,6 +37,27 @@ describe 'Student' do
       visit student_path(@student1)
       page.should have_link "Enroll to course"
     end
+
+    it "should exist edit link on student nested address" do
+      @student1.addresses << Factory(:address)
+      visit student_path(@student1)
+      @student1.addresses.size.should == 1
+      page.should have_selector('a', href: "/students/1/addresses/1/edit")
+    end
+    
+    it "should exist edit link on student nested guardian" do
+      @student1.guardians << Factory(:guardian)
+      visit student_path(@student1)
+      @student1.guardians.size.should == 1
+      page.should have_selector('a', href: "/students/1/guardians/1/edit")
+    end
+
+    it "should exist edit link on student nested note" do
+      @student1.notes << Factory(:note)
+      visit student_path(@student1)
+      @student1.notes.size.should == 1
+      page.should have_selector('a', href: "/students/1/notes/1/edit")
+    end
   end
 
 end
