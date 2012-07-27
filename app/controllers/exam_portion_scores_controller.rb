5db3ea5698f2d450e4ad45fb376d78ec19adb074
影月 零 <zero@genshin.org>
2012-07-27 22:58:01 +0900

    route nesting

--- app/controllers/exam_portion_scores_controller.rb
+++ app/controllers/exam_portion_scores_controller.rb
@@ -3,13 +3,10 @@ class ExamPortionScoresController < ApplicationController
   actions :new, :index, :create, :update, :edit, :destroy
 
   def index
-    #if !(params.has_key?(:course) && params.has_key?(:exam_portions))
-    #  render :text => "No Course Specified"
-    #  return
-    #end
-
-    
-      
+    if !(params.has_key?(:course_id) && params.has_key?(:exam_id))
+      render :text => "No Course Specified"
+      return
+    end
     render :text => "has course" << Course.find(params[:course]).syllabus.name
   end
 end
