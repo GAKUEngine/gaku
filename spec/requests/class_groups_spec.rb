14c556713dca0b686585972f40ec4a9985673587
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-27 12:29:19 +0300

    [spec/requests] Refactor class_groups_spec

--- spec/requests/class_groups_spec.rb
+++ spec/requests/class_groups_spec.rb
@@ -20,7 +20,6 @@ describe 'ClassGroups' do
       click_button "Create Class group"
 
       page.should have_content "was successfully created"
-
     end
   end
 
@@ -28,30 +27,18 @@ describe 'ClassGroups' do
     it "should list existing enrolled students" do
       visit class_group_path(@class_group)
       page.should have_content "Class Roster"
-    end
-
-    it "should exist add_student link" do
-      visit class_group_path(@class_group)
       page.should have_link "Add Student"
     end
 
     it "should list existing semesters" do
       visit class_group_path(@class_group)
       page.should have_content "Semesters list"
-    end
-
-    it "should exist add_a_semester link" do
-      visit class_group_path(@class_group)
       page.should have_link "Add a semester"
     end
 
     it "should list existing courses" do
       visit class_group_path(@class_group)
       page.should have_content "Courses List"
-    end
-
-    it "should exist add_course link" do
-      visit class_group_path(@class_group)
       page.should have_link "Add course"
     end
   end
