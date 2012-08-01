2c5a3555486e4f92a195a8f609fb4d6fce47f888
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-08-01 16:14:04 +0300

    [migrations] exam_portions weight - default value 100

--- db/migrate/20120608131907_create_exam_portions_table.rb
+++ db/migrate/20120608131907_create_exam_portions_table.rb
@@ -2,7 +2,7 @@ class CreateExamPortionsTable < ActiveRecord::Migration
   def change
   	create_table :exam_portions do |t|
   	  t.string   :name
-  	  t.float    :max_score, :weight
+  	  t.float    :max_score, :weight, :default => 100
   	  t.integer  :problem_count
       t.text     :description, :adjustments
       t.datetime :execution_date
