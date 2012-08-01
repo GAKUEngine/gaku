13658f658f58e804782b4d4cd033ee35008e59a4
影月 零 <zero@genshin.org>
2012-08-02 01:14:12 +0900

    pre merge commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731071440) do
+ActiveRecord::Schema.define(:version => 20120731070008) do
 
   create_table "addresses", :force => true do |t|
     t.string   "address1"
