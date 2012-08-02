25783b1a40ecb9ccb19e0eb75ca741674e1121dd
Vassil Kalkov <vassilkalkov@gmail.com>
2012-08-02 10:02:13 +0300

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,6 @@ ActiveRecord::Schema.define(:version => 20120801124531) do
     t.datetime "updated_at",                    :null => false
   end
 
-  create_table "addresses_guardians", :force => true do |t|
-    t.integer "address_id"
-    t.integer "guardian_id"
-  end
-
   create_table "assignment_scores", :force => true do |t|
     t.integer  "score"
     t.integer  "student_id"
