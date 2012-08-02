0c1e57ce750d207bb17821e9c6c900130a76d1b2
Vassil Kalkov <vassilkalkov@gmail.com>
2012-08-02 11:30:37 +0300

    [spec/requests] Add spec for adding student address under students_spec

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Student' do
   before do
-    @student1 = Factory(:student)
+    @student = Factory(:student)
     sign_in_as!(Factory(:user))
     within('ul#menu') { click_link "Students"}
   end
@@ -24,71 +24,97 @@ describe 'Student' do
 
   context "show student" do
     it "should exist enroll_to_class link" do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_link "Enroll to class"
     end
   
     it "should list enrolled courses" do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_content "Courses List"
     end
 
     it "should exist enroll_to_course link" do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_link "Enroll to course"
     end
 
-    it "should exist edit link on student nested address", :js => true do
-      @student1.addresses << Factory(:address)
-      visit student_path(@student1)
-      @student1.addresses.size.should == 1
-      page.should have_selector('a', href: "/students/1/addresses/1/edit")
+    it "should allow to add a student address", :js => true do
+      Factory(:country, :name => "Japan")
+      visit student_path(@student)
+
       click_link 'new_address_link'
-      sleep 2
-      save_and_open_page
+      click_link 'new_student_address_link'
+
+      wait_until { page.has_content?('Country') } 
+      #required
+      select "Japan", :from => 'country_dropdown'
+      fill_in "student_addresses_attributes_0_city", :with => "Nagoya"
+      fill_in "student_addresses_attributes_0_address1", :with => "Subaru str."
+
+      fill_in "student_addresses_attributes_0_title", :with => "John Doe main address"
+      fill_in "student_addresses_attributes_0_zipcode", :with => "00359"
+      fill_in "student_addresses_attributes_0_address2", :with => "Toyota str."
+      #fill_in "student_addresses_attributes_0_state", :with => "Aichi"
+     
+
+      click_button "Save address"
+
+      @student.addresses.size.should == 1
+      page.should have_selector('a', href: "/students/1/addresses/1/edit")
+
+      #required
+      page.should have_content("Japan")
+      page.should have_content("Nagoya")
+      page.should have_content("Subaru str.")
+      
+      page.should have_content("John Doe main address")
+      page.should have_content("00359")
+      page.should have_content("Toyota str.")
+      #page.should have_content("Aichi")
+      
     end
     
     it "should exist edit link on student nested guardian" do
-      @student1.guardians << Factory(:guardian)
-      visit student_path(@student1)
-      @student1.guardians.size.should == 1
+      @student.guardians << Factory(:guardian)
+      visit student_path(@student)
+      @student.guardians.size.should == 1
       page.should have_selector('a', href: "/students/1/guardians/1/edit")
     end
 
     it "should exist edit link on student nested note" do
-      @student1.notes << Factory(:note)
-      visit student_path(@student1)
-      @student1.notes.size.should == 1
+      @student.notes << Factory(:note)
+      visit student_path(@student)
+      @student.notes.size.should == 1
       page.should have_selector('a', href: "/students/1/notes/1/edit")
     end
   end
 
   context "studets contacts"  do
     before(:each) do
-      @contact = Factory(:contact, :student_id => @student1)    
+      @contact = Factory(:contact, :student_id => @student)    
     end 
     it "should have contact" do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_content("#{@contact.data}") 
     end
 
     it "should have right contact classes in tr" do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_css("tr.student_contact.contact_#{@contact.id}") 
     end
 
     it "should have primary column with right class " do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_css('td.primary_contact') 
     end
 
     it "should have action column with right class for button " do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_css('td.primary_button') 
     end
 
     it "should have form with new_student_contact_form class " do
-      visit student_path(@student1)
+      visit student_path(@student)
       page.should have_css('form.new_student_contact_form') 
     end
 
