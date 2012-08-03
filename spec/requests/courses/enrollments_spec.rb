e19d7f354417314ca8b492f8ef2e03904ad879e0
MKostova <mkostova@camplight.net>
2012-08-03 21:21:03 +0300

    [spec/cources] enroll student to a course

--- spec/requests/courses/enrollments_spec.rb
+++ spec/requests/courses/enrollments_spec.rb
@@ -1,27 +1,32 @@
 require 'spec_helper'
 
-describe "CourseEnrollment"  do
+describe "CourseEnrollments"  do
   before do
     @course = Factory(:course)
     @student = Factory(:student)
-    sign_in_as!(Factory(:user))
-    within('ul#menu') { click_link "Courses"}
+    @class_group = Factory(:class_group_enrollment)
+    sign_in_as!(Factory(:user)) 
+    within('ul#menu') do
+      click_link "Course Management"
+      click_link "Courses"
+    end
   end
-  it "should add and show student course", :js => true do
+
+  it "should add and show student to a course", :js => true do
     visit course_path(@course)
 
     click_link 'Enrollment'
-    click_link 'new_student_enrollment_link'
-
-    wait_until { page.has_content?('Enroll Student') } 
-    #required
-    select student.name, :from => 'course_enrollment_student_id'
-   
-    click_button "Enroll Student"
-
-    @course.course_enrollments.size.should == 1
+    click_link 'add_student_enrollment'
+    wait_until { page.has_content?('Students') } 
     
     #required
+    select @student.name, :from => 'course_enrollment_student_id'
+    
+    click_button "Enroll Student"
+    wait_until { page.has_content?('View Assignments')}
+
+    @course.course_enrollments.size.should == 1
     page.should have_content(@student.name)      
   end
+
 end
