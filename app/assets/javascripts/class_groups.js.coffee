adf387d79a9f95b2563d3d94b310bf2241d9a691
Georgi Tapalilov <tapalilov@gmail.com>
2012-08-06 18:45:45 +0300

    [ajax] add new class group from class_groups/index with ajax

--- app/assets/javascripts/class_groups.js.coffee
+++ app/assets/javascripts/class_groups.js.coffee
@@ -5,3 +5,8 @@ $ ->
 	$('#add_student_enrollment').on 'click','a.btn', (event) ->
     event.preventDefault()
     $('#enrollment_form').slideToggle()
+
+
+  $('#new_class_group_link').on 'click', (event) ->
+  	event.preventDefault()
+  	$('#new_class_group_form').slideToggle()
--- app/controllers/class_groups_controller.rb
+++ app/controllers/class_groups_controller.rb
@@ -5,6 +5,14 @@ class ClassGroupsController < ApplicationController
   actions :index, :show, :new, :create, :update, :edit, :destroy
 
   before_filter :load_before_show, :only => :show
+  before_filter :load_before_index, :only => :index
+
+  def create
+    super do |format|
+      format.js { render }
+    end
+    
+  end
 
   def destroy
     destroy! :flash => !request.xhr?
@@ -16,6 +24,10 @@ class ClassGroupsController < ApplicationController
       @new_class_group_enrollment = ClassGroupEnrollment.new
       @new_semester = Semester.new
       @new_course = Course.new
+    end
+
+    def load_before_index
+      @class_group = ClassGroup.new
     end
 
 end
