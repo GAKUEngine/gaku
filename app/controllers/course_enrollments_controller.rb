eb129445746cb73d7d670fedcef67bd6e6a45916
MKostova <mkostova@camplight.net>
2012-08-08 14:01:04 +0300

    [Courses|Enroll_Student][WIP]validations

--- app/controllers/course_enrollments_controller.rb
+++ app/controllers/course_enrollments_controller.rb
@@ -3,26 +3,23 @@ class CourseEnrollmentsController < ApplicationController
   inherit_resources
 
   actions :show, :new, :create, :update, :edit, :destroy
-  
-  # creating course_enrollment form students/show
-  def create
-    super do |format|
-      @student = Student.find(params[:course_enrollment][:student_id])      
-      format.js { render 'create' }
-    end  
-  end
 
   # creating course_enrollment from courses/show
   def enroll_student
- 		@course_enrollment = CourseEnrollment.new(params[:course_enrollment])
- 		# handle not saving course enrollment
- 		if @course_enrollment.save
- 			respond_to do |format|
- 				format.js { render 'enroll_student' }
- 			end
-    else 
-      flash[:error] = "Student already enrolled"
- 		end
+    @course_enrollment = CourseEnrollment.new(params[:course_enrollment])
+    @course = Course.find(params[:course_enrollment][:course_id])
+    if @course_enrollment.save
+      respond_with(@course_enrollment) do |format|
+        format.js { render 'enroll_student' }
+      end
+    else
+      @errors = @course_enrollment.errors
+      respond_with(@course_enrollment) do |format|
+        format.js { render 'error' }
+      end
+    end
+
   end
+
 
 end
