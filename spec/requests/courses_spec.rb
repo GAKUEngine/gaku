bfc4bd8a991e9299b2e0747c6fa3c53110a25097
MKostova <mkostova@camplight.net>
2012-08-22 15:39:46 +0300

    [spec/requests]add delete and edit from show course specs

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -4,6 +4,7 @@ describe 'Courses' do
   before do
     
     @syllabus = Factory(:syllabus, :name => 'biology2012', :code => 'bio', :id => '123' )
+    @syllabus2 = Factory(:syllabus, :name => 'biology2013Syllabus', :code => 'biology', :id => '1234' )
     @course = Factory(:course, :syllabus_id =>'123')
     sign_in_as!(Factory(:user))
     within('ul#menu') { click_link "Course Management"}
@@ -40,17 +41,46 @@ describe 'Courses' do
 
   context "edit a course" , :js => true do
     it "should edit a course" do
-      @syllabus2 = Factory(:syllabus, :name => 'biology2013Syllabus', :code => 'biology', :id => '1234' )
       page.should have_content("Courses List")
       within('.table  tr:nth-child(2)') { click_link "edit_link" }
+
       page.should have_content("Edit Course") 
       fill_in 'course_code', :with => 'biology2013'
       page.select "biology2013Syllabus", :from => 'course_syllabus_id'
       click_button 'Update Course'
+
       page.should have_content "was successfully updated"
       page.should have_content "biology2013Syllabus"
       page.should have_content "biology2013"
     end
+
+    it "should edit a course from show" do
+      page.should have_content("Courses List")
+      within('.table  tr:nth-child(2)') { click_link "show_link" }
+      page.should have_content("Show")
+
+      find_link("Edit").click
+      page.should have_content("Edit Course") 
+      fill_in 'course_code', :with => 'biology2013'
+      page.select "biology2013Syllabus", :from => 'course_syllabus_id'
+      click_button 'Update Course'
+
+      page.should have_content "was successfully updated"
+      page.should have_content "biology2013Syllabus"
+      page.should have_content "biology2013"
+    end
+  end
+
+  context "delete a course" , :js => true do
+    it "should delete a course" do
+      page.should have_content("Courses List")
+      page.should have_content(@course.code)
+      within('.table  tr:nth-child(2)') { click_link "delete_link" }
+      
+      page.driver.browser.switch_to.alert.accept
+      page.should_not have_content(@course.code)
+    end
+
   end
 
 end
