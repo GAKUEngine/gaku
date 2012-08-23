90dee1a0e4e0955bf906facb80adf3dfaa43ddff
Vassil Kalkov <vassilkalkov@gmail.com>
2012-08-23 15:03:38 +0300

    [spec/requests] Refactor courses_spec

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -2,30 +2,10 @@ require 'spec_helper'
 
 describe 'Courses' do
   before do
-    
-    @syllabus = Factory(:syllabus, :name => 'biology2012', :code => 'bio', :id => '123' )
-    @syllabus2 = Factory(:syllabus, :name => 'biology2013Syllabus', :code => 'biology', :id => '1234' )
-    @course = Factory(:course, :syllabus_id =>'123')
+    @syllabus = Factory(:syllabus, :name => 'biology2012', :code => 'bio')
     sign_in_as!(Factory(:user))
-    within('ul#menu') { click_link "Course Management"}
-    within('ul#menu') { click_link "Courses"}
-  end
-
-  context "listing courses", :js => true do
-    it "should list existing courses" do
-      page.should have_content("Courses List")
-      within(".table tbody tr td"){
-        page.should have_content("bio")  
-      }
-      
-      # show
-      within('.table  tr:nth-child(2)') { click_link "show_link" }
-      wait_until {
-        page.has_content?('Course Code')
-        page.has_content?('bio')
-        page.has_content?('Number Enrolled')
-      }
-    end
+    within('ul#menu') { click_link "Course Management" }
+    within('ul#menu') { click_link "Courses" }
   end
 
   context "creating new course" do 
@@ -35,13 +15,30 @@ describe 'Courses' do
       fill_in 'course_code', :with => 'SUMMER2012'
       page.select "#{@syllabus.name}", :from => 'course_syllabus_id'
       click_button 'Create Course'
+
       page.should have_content "was successfully created"
+      page.should have_content("#{@syllabus.name}")
     end
   end
 
-  context "edit a course" , :js => true do
+  context "index, edit, delete", :js => true do
+
+    before do
+      @syllabus2 = Factory(:syllabus, :name => 'biology2013Syllabus', :code => 'biology')
+      @course = Factory(:course, :syllabus => @syllabus) 
+      visit courses_path
+    end
+
+    it "should list and show existing courses" do
+      within(".table tbody tr td") { page.should have_content("bio") }
+      within('.table tr:nth-child(2)') { click_link "show_link" }
+      
+      page.should have_content('Course Code')
+      page.should have_content('bio')
+      page.should have_content('Number Enrolled')
+    end
+
     it "should edit a course" do
-      page.should have_content("Courses List")
       within('.table  tr:nth-child(2)') { click_link "edit_link" }
 
       page.should have_content("Edit Course") 
@@ -55,11 +52,10 @@ describe 'Courses' do
     end
 
     it "should edit a course from show" do
-      page.should have_content("Courses List")
       within('.table  tr:nth-child(2)') { click_link "show_link" }
       page.should have_content("Show")
 
-      find_link("Edit").click
+      click_link('Edit')
       page.should have_content("Edit Course") 
       fill_in 'course_code', :with => 'biology2013'
       page.select "biology2013Syllabus", :from => 'course_syllabus_id'
@@ -69,18 +65,18 @@ describe 'Courses' do
       page.should have_content "biology2013Syllabus"
       page.should have_content "biology2013"
     end
-  end
 
-  context "delete a course" , :js => true do
     it "should delete a course" do
+      @syllabus.courses.count.should == 1
       page.should have_content("Courses List")
       page.should have_content(@course.code)
+
       within('.table  tr:nth-child(2)') { click_link "delete_link" }
       
       page.driver.browser.switch_to.alert.accept
       page.should_not have_content(@course.code)
+      @syllabus.courses.count.should == 0
     end
-
   end
 
 end
