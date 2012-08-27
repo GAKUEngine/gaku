149b7446a5b9ced33de984d3dabc17cb7be55f60
Georgi Tapalilov <tapalilov@gmail.com>
2012-08-27 17:12:35 +0300

    [models] add importer_type to ImporterFile

--- db/migrate/20120826094804_create_import_files.rb
+++ db/migrate/20120826094804_create_import_files.rb
@@ -2,6 +2,7 @@ class CreateImportFiles < ActiveRecord::Migration
   def up
     create_table :import_files do |t|
       t.string :context
+    	t.string :importer_type
     end
 
     add_attachment :import_files, :data_file
