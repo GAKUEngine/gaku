9b1f642a3a6d9d2b2beebe749093c67addb3a30b
snowsunny <snowsunny1107@gmail.com>
2012-09-05 11:59:02 +0900

    fix application.js and miniFix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,9 +30,10 @@
 //= require backbone
 //
 //= require .//gaku_engine
-//
-//= require_tree ../templates/
-//= require_tree .//models
-//= require_tree .//collections
-//= require_tree .//views
-//= require_tree .//routers
+
+// FIXME need fix
+// require_tree ../templates/
+// require_tree .//models
+// require_tree .//collections
+// require_tree .//views
+// require_tree .//routers
--- db/schema.rb
+++ db/schema.rb
@@ -260,9 +260,8 @@ ActiveRecord::Schema.define(:version => 20120904040313) do
     t.string   "title"
     t.text     "content"
     t.integer  "student_id"
-    t.integer  "lesson_plan_id"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "presets", :force => true do |t|
@@ -298,8 +297,8 @@ ActiveRecord::Schema.define(:version => 20120904040313) do
     t.string  "name"
     t.string  "abbr"
     t.string  "name_ascii"
-    t.integer "code"
     t.integer "country_numcode"
+    t.integer "code"
   end
 
   create_table "student_addresses", :force => true do |t|
