477ecdd1c42946602590f50fcfe6f14e309e127d
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-09-10 11:00:24 +0300

    [controllers] refactor class_group_enrollments to work with encrypted student info

--- app/controllers/class_group_enrollments_controller.rb
+++ app/controllers/class_group_enrollments_controller.rb
@@ -3,14 +3,23 @@ class ClassGroupEnrollmentsController < ApplicationController
   def filtered_students
     @class_group_enrolled_students = ClassGroupEnrollment.where(:class_group_id => params[:class_group_id]).pluck(:student_id)
    
-   if @class_group_enrolled_students.blank?
+    if @class_group_enrolled_students.blank?
       @students = Student.find(:all, :include => [:addresses, :class_groups, :class_group_enrollments])
     else 
       @students = Student.find(:all, :conditions => ['id not in (?)', @class_group_enrolled_students], :include => [:addresses, :class_groups, :class_group_enrollments])
     end
 
+    @students_json = @students.as_json(:methods => [:address_widget, :class_group_widget,:seat_number_widget])
+    i = 0
+    @students_json.each {|student|
+      student[:name] = @students[i].name
+      student[:surname] = @students[i].surname
+      student[:phone] = @students[i].phone
+      i += 1
+    }
+
     respond_to do |format|
-      format.json { render :json => @students.as_json(:methods => [:address_widget, :class_group_widget,:seat_number_widget])}
+      format.json { render :json => @students_json.as_json }
     end
   end
 
