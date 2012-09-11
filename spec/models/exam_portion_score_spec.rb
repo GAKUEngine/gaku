9d8df66f490e7d474773c8dfe7086a7f51d6163a
Vassil Kalkov <vassilkalkov@gmail.com>
2012-09-11 18:16:23 +0300

    WIP : Clean specs

--- spec/models/exam_portion_score_spec.rb
+++ spec/models/exam_portion_score_spec.rb
@@ -7,23 +7,6 @@ describe ExamPortionScore do
 
     it { should have_valid_factory(:exam_portion_score) }
     it { should belong_to(:exam_portion) }
-    it { should belong_to(:student)}
-    #it { should validate_presence_of(:score) }
-
-    pending "errors when score is nil" do
-      exam_portion_score.score = nil
-      exam_portion_score.should_not be_valid
-    end
-
-    pending "should validate score is greater than 0" do
-      exam_portion_score.score = -1
-      exam_portion_score.should be_invalid
-    end
-
-    pending "should validate score is 0" do
-      exam_portion_score.score = 0
-      exam_portion_score.should be_valid
-    end
-  end
-  
+    it { should belong_to(:student)}  
+  end  
 end
--- spec/requests/class_groups/courses_spec.rb
+++ spec/requests/class_groups/courses_spec.rb
@@ -5,26 +5,24 @@ describe 'ClassGroup Courses' do
   
   before do
     @class_group = Factory(:class_group, :grade => '1', :name => "Not so awesome class group", :homeroom => 'A1')
-    #within('ul#menu') { click_link "Class Management" }
-    #within('ul#menu') { click_link "Class Listing" }
     visit class_group_path(@class_group)
     click_link 'class_group_courses_tab_link'
   end
 
   it 'should add and show course to a class group', :js => true do
-    
     click_link 'add_class_group_course_link'
     wait_until { page.find('#add_class_group_course').visible? }
     fill_in 'course_code', :with => 'Biology321'
     click_button 'submit_button'
 
     page.should have_content ('Biology321') #TODO test the redirect from class group to a course
+    
     visit class_group_path(@class_group)
     click_link 'class_group_courses_tab_link'
     within('.tab-content') { page.should have_content 'Biology321'}
   end
+
   pending 'should not add a course if course code is empty', :js => true do #FIXME cannot load the validation code properly
-    
     click_link 'show_course_form'
     wait_until { page.find('#add_class_group_course').visible? }
     click_button 'submit_button'
@@ -33,9 +31,13 @@ describe 'ClassGroup Courses' do
   end
 
   context 'Class group with added course' do
-    pending 'should not add a course if it is already added' do  #need to be implemented in the main logic
+    pending 'should not add a course if it is already added' do  
+      #need to be implemented in the main logic
     end
-    pending 'should delete a course from class group' do  #need to be implemented in the main logic
+
+    pending 'should delete a course from class group' do  
+      #need to be implemented in the main logic
     end
   end
+
 end
