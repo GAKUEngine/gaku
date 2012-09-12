b7f91ac2cc81c2552bec1d70867c184f5ab2463d
Vassil Kalkov <vassilkalkov@gmail.com>
2012-09-12 13:06:32 +0300

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -221,14 +221,14 @@ ActiveRecord::Schema.define(:version => 20120911001159) do
   end
 
   create_table "guardians", :force => true do |t|
-    t.string   "name"
-    t.string   "surname"
-    t.string   "name_reading"
-    t.string   "surname_reading"
-    t.string   "relationship"
+    t.string   "encrypted_name"
+    t.string   "encrypted_surname"
+    t.string   "encrypted_name_reading"
+    t.string   "encrypted_surname_reading"
+    t.string   "encrypted_relationship"
     t.integer  "user_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "guardians_students", :force => true do |t|
@@ -281,8 +281,9 @@ ActiveRecord::Schema.define(:version => 20120911001159) do
     t.string   "title"
     t.text     "content"
     t.integer  "student_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "lesson_plan_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "presets", :force => true do |t|
@@ -318,8 +319,8 @@ ActiveRecord::Schema.define(:version => 20120911001159) do
     t.string  "name"
     t.string  "abbr"
     t.string  "name_ascii"
-    t.integer "country_numcode"
     t.integer "code"
+    t.integer "country_numcode"
   end
 
   create_table "student_addresses", :force => true do |t|
@@ -331,13 +332,13 @@ ActiveRecord::Schema.define(:version => 20120911001159) do
   end
 
   create_table "students", :force => true do |t|
-    t.string   "name"
+    t.string   "encrypted_name"
     t.string   "middle_name"
-    t.string   "surname"
-    t.string   "name_reading",                 :default => ""
-    t.string   "surname_reading",              :default => ""
+    t.string   "encrypted_surname"
+    t.string   "encrypted_name_reading",       :default => ""
+    t.string   "encrypted_surname_reading",    :default => ""
     t.boolean  "gender"
-    t.string   "phone"
+    t.string   "encrypted_phone"
     t.string   "email"
     t.date     "birth_date"
     t.date     "admitted"
