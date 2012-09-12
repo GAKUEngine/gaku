f28aff7c0874abd3eee32bb5e66bc4828e8d443f
Georgi Tapalilov <tapalilov@gmail.com>
2012-09-12 17:09:04 +0300

    [fix] assets for backbonejs in applicationjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,5 +33,5 @@
 //= require_tree ..//templates
 //= require_tree .//models
 //= require_tree .//collections
-// require_tree .//views
-// require_tree .//routers
+//= require_tree .//views
+//= require_tree ./routers
