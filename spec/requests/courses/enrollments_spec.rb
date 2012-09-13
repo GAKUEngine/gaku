e372ee19199c3fca4fb656f9118497177f740467
Vassil Kalkov <vassilkalkov@gmail.com>
2012-09-13 12:46:41 +0300

    WIP : Refactor courses

--- spec/requests/courses/enrollments_spec.rb
+++ spec/requests/courses/enrollments_spec.rb
@@ -10,18 +10,18 @@ describe "CourseEnrollment"  do
   it "should enroll and show student", :js => true do
     Factory(:student, :name => "John", :surname => "Doe")
     visit course_path(@course)
+    @course.students.size.should eql(0)
 
-    click_link 'add_student_enrollment'
-    @course.students.size.should eql(0) 
-    wait_until { page.has_content?('Choose Student') } 
+    click_link 'add_student_enrollment'  
+    wait_until { page.find('#add_student_enrollment_form').visible? }
     select "John Doe", :from => 'course_enrollment_student_id'
-    click_button "Enroll Student"
-
-    page.should have_content("John")
-    sleep 1 #TODO change sleep 
-    @course.students.size.should eql(1)     
+    click_button 'submit_student_button'
+    
+    wait_until { !page.find('#add_student_enrollment_form').visible? } 
+    page.should have_content("Doe John")
     page.should have_content("View Assignments")
-    #TODO check if the student is added to the table visually without redirect
+    page.should have_content("View Exams")
+    @course.students.size.should eql(1)     
   end
 
   it "should enroll student only once for a course", :js => true  do
@@ -32,12 +32,12 @@ describe "CourseEnrollment"  do
     @course.students.size.should eql(1)
 
     click_link 'add_student_enrollment'
-    wait_until { page.has_content?('Choose Student') } 
+    wait_until { page.find('#add_student_enrollment_form').visible? }
 
     select "Toni Rtoe", :from => 'course_enrollment_student_id'
-    click_button "Enroll Student"
+    click_button 'submit_student_button'
+    wait_until { !page.find('#add_student_enrollment_form').visible? } 
     page.should have_content ("Student Already enrolled to course!")
-    sleep 1 #TODO change sleep
     @course.students.size.should eql(1)
   end
 
