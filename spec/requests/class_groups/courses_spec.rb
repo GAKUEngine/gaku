c3544830d5cd60c8d8b087ba7d4a04f2203f9bc6
MKostova <mkostova@camplight.net>
2012-09-13 15:17:54 +0300

    [spec/requests] delete a course from class group

--- spec/requests/class_groups/courses_spec.rb
+++ spec/requests/class_groups/courses_spec.rb
@@ -35,12 +35,29 @@ describe 'ClassGroup Courses' do
   end
 
   context 'Class group with added course' do
+    before do
+      @class_group.courses<<@course
+      visit class_group_path(@class_group)
+      click_link 'class_group_courses_tab_link'
+      
+    end
+
     pending 'should not add a course if it is already added' do  
       #TODO need to be implemented in the main logic
     end
 
-    pending 'should delete a course from class group' do  
-      #TODO need to be implemented in the main logic
+    it 'should delete a course from class group', :js => true do  
+      ClassGroupCourseEnrollment.count.should == 1
+      @class_group.courses.count.should == 1
+      page.all('#courses_index tbody tr').size.should == 1
+      within("#courses_index tbody"){
+        page.should have_content ("#{@course.code}")
+        click_on "delete_link"
+      }
+      page.driver.browser.switch_to.alert.accept
+      wait_until { page.all('#courses_index tbody tr').size.should == 0 }
+      ClassGroupCourseEnrollment.count.should == 0
+      @class_group.courses.count.should == 0
     end
   end
 
