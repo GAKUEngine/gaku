890dd689e01678737f597741ae62a4d7760cfd12
Georgi Tapalilov <tapalilov@gmail.com>
2012-09-14 16:00:05 +0300

    [ajax/view] course group ajax crud + translations + error handling

--- app/controllers/course_groups_controller.rb
+++ app/controllers/course_groups_controller.rb
@@ -3,6 +3,33 @@ class CourseGroupsController < ApplicationController
 
 	before_filter :load_before_show, :only => [:show]
 
+	def create
+		super do |format|
+			@course_groups = CourseGroup.all
+			format.js { render 'create'}
+		end
+	end
+
+	def edit
+		super do |format|
+			format.js { render 'edit' }
+		end
+	end
+
+	def update
+		super do |format|
+			@course_groups = CourseGroup.all
+			format.js { render 'update'}
+		end
+	
+	end
+
+	def destroy
+		super do |format|
+    format.js { render :nothing => true }
+  end
+  end
+
 	private
 
 	def load_before_show
