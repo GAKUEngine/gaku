aeb677dc0c5edc1e3570635e3c0b9aad4930cdbe
Georgi Tapalilov <tapalilov@gmail.com>
2012-09-25 14:10:22 +0300

    [delete] delete migration and old preset form

--- db/migrate/20120706100825_create_exams_syllabuses_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateExamsSyllabusesTable < ActiveRecord::Migration
-  def change
-    create_table :exams_syllabuses do |t|
-      t.references :exam
-      t.references :syllabus
-    end 
-  end
-end
