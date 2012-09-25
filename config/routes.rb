1fddcd336e6635f109096ed87383411b79787a86
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-09-25 17:10:30 +0300

    [routes] remove autocomplete for address

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,6 @@ GAKUEngine::Application.routes.draw do
 
     resources :addresses, :controller => 'students/addresses' do
       post :make_primary, :on => :member
-      get :load_autocomplete_data
     end
     resources :contacts, :controller => 'students/contacts' do
       post :make_primary, :on => :member
