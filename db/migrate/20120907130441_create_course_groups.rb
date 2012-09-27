c5dc4fe2b16bc8090ce28e72b40faaf91e7480ab
Georgi Tapalilov <tapalilov@gmail.com>
2012-09-27 13:51:25 +0300

    add is_deleted to course group for soft deletes

--- db/migrate/20120907130441_create_course_groups.rb
+++ db/migrate/20120907130441_create_course_groups.rb
@@ -1,8 +1,8 @@
 class CreateCourseGroups < ActiveRecord::Migration
   def change
     create_table :course_groups do |t|
-      t.string :name
-
+      t.string 	:name
+      t.boolean :is_deleted, :default => false
       t.timestamps
     end
   end
