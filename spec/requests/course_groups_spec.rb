831e546dd31fd00da8a373db4013d503b43e9ac8
Vassil Kalkov <vassilkalkov@gmail.com>
2012-09-28 11:48:46 +0300

    [spec/requests] Fix course_groups_spec

--- spec/requests/course_groups_spec.rb
+++ spec/requests/course_groups_spec.rb
@@ -74,11 +74,7 @@ describe 'CourseGroups' do
     it 'should edit course group from show view', :js => true do #TODO to be implemented
       CourseGroup.count.should eql(1)
       visit course_group_path(@course_group)
-<<<<<<< HEAD
       find(".edit-link").click
-=======
-      click_link("edit-course-group-link")
->>>>>>> f52b7f54731d0624289f01c1108bebbaff198d74
 
       wait_until { find('#edit-course-group-modal').visible? }
       fill_in 'course_group_name', :with => '2012 Courses'
