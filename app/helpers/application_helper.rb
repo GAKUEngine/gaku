b675ff558793cfa90df2e97cdc0e44837aafc2be
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-02 10:23:13 +0300

    Modify render_js_partial helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -108,8 +108,12 @@ module ApplicationHelper
     object.count != 0 ? text + "(" + object.count.to_s + ")" : text
   end
 
-  def render_js_partial(partial, locals)
-    escape_javascript(render :partial => partial, :formats => [:html], :handlers => [:erb, :slim], :locals => locals) 
+  def render_js_partial(partial, locals = {})
+    unless locals == {}
+      escape_javascript(render :partial => partial, :formats => [:html], :handlers => [:erb, :slim], :locals => locals) 
+    else
+      escape_javascript(render :partial => partial, :formats => [:html], :handlers => [:erb, :slim]) 
+    end
   end
 
   def sortable(column, title = nil)
