b3be6d2fb6b0c45c6de264aee8876ce7679091f9
MKostova <mkostova@camplight.net>
2012-10-02 22:41:57 +0300

    [views][controllers] exam portion update/delete to update weight widget

--- app/controllers/exam_portions_controller.rb
+++ app/controllers/exam_portions_controller.rb
@@ -26,10 +26,10 @@ class ExamPortionsController < ApplicationController
 
   def destroy
     #destroy! :flash => !request.xhr?
-
     @exam_portion = ExamPortion.find(params[:id])
     @portion_id = @exam_portion.id
     @exam_portion.destroy
+    @total_weight=get_total_weight(@exam.exam_portions)
     super do |format|
       format.js { render 'exams/exam_portions/destroy' }
     end
@@ -38,5 +38,13 @@ class ExamPortionsController < ApplicationController
   private 
     def load_exam
       @exam = Exam.find(params[:exam_id])
+    end
+
+    def get_total_weight(portions)
+      total=0
+      portions.each do |portion|
+        total+=portion.weight
+      end
+      total
     end
 end
