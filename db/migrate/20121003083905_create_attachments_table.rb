ed94e9b054450dfe1f3b77e3261d0fe218d12f2d
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-04 18:08:52 +0300

    Fix migrations names

--- db/migrate/20121003083905_create_attachments_table.rb
+++ db/migrate/20121003083905_create_attachments_table.rb
@@ -1,4 +1,4 @@
-class CreateAttachmentTable < ActiveRecord::Migration
+class CreateAttachmentsTable < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
     	t.string 		 :name
