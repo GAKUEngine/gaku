3a09bf175a0ded0883f26194cde835cd4ab4881d
Nakaya Yukiharu <snowsunny1107@gmail.com>
2012-10-11 14:59:04 +0900

    mini fix

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(:version => 20121003083905) do
     t.datetime "updated_at",                    :null => false
   end
 
-  create_table "assets", :force => true do |t|
-    t.integer  "exam_portion_id"
-    t.integer  "lesson_plan_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
   create_table "assignment_scores", :force => true do |t|
     t.integer  "score"
     t.integer  "student_id"
@@ -79,8 +72,9 @@ ActiveRecord::Schema.define(:version => 20121003083905) do
     t.string   "name"
     t.integer  "school_id"
     t.integer  "address_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.boolean  "is_master",  :default => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
   create_table "class_group_course_enrollments", :force => true do |t|
