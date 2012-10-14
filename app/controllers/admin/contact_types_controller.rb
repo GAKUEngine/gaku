0e077910b0949336e98de0e37c5a3bed67a46b6d
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-14 13:25:13 +0300

    Add inherited_resource actions to admin/contact_types_controller

--- app/controllers/admin/contact_types_controller.rb
+++ app/controllers/admin/contact_types_controller.rb
@@ -2,7 +2,8 @@ module Admin
   class ContactTypesController < Admin::BaseController
    	
     inherit_resources 
-
+    actions :index, :show, :new, :create, :update, :edit, :destroy
+    
     respond_to :js, :html
 
     before_filter :contact_types_count, :only => [:create, :destroy]
