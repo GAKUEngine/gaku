ebf3ab915710e4f7547aaf44f628434c0ac15fb1
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-14 14:23:27 +0300

    Refactor controllers under admin/schools

--- app/controllers/admin/schools/campuses_controller.rb
+++ app/controllers/admin/schools/campuses_controller.rb
@@ -2,11 +2,11 @@ module Admin
   class Schools::CampusesController < Admin::BaseController
 
     inherit_resources
+    actions :index, :show, :new, :update, :edit, :destroy
 
-    actions :index, :show, :new, :create, :update, :edit, :destroy
+    respond_to :js, :html
 
-    before_filter :load_campus, :only => [:destroy, :update, :show]
-    before_filter :load_school, :only => [:index, :new, :create, :edit, :update, :show, :destroy]
+    before_filter :load_school
     
     def create
       @campus = Campus.new(params[:campus])
@@ -18,33 +18,7 @@ module Admin
       end
     end
 
-    def edit
-      super do |format|
-        format.js { render 'edit' }  
-      end  
-    end
-
-    def update
-      if @campus.update_attributes(params[:campus])
-        respond_to do |format|
-          format.js
-        end  
-      end
-    end
-
-    def destroy
-      if @campus.destroy
-        respond_to do |format|
-          format.js { render 'destroy' }
-        end
-      end
-    end
-
     private
-      def load_campus
-        @campus = Campus.find(params[:id])
-      end
-
       def load_school
         @school = School.find(params[:school_id])
       end
