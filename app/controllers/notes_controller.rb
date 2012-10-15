a81bbde5edf5f793fb7c2e33ff21db5bd40694cd
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-10-15 15:10:16 +0300

    [controllers] refactor notes controller action load_notable

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -52,8 +52,10 @@ class NotesController < ApplicationController
 
 private
 	def load_notable
-		resource, id = request.path.split('/')[1, 2]
-		@notable = resource.singularize.classify.constantize.find(id)
+		#resource, id = request.path.split('/')[1, 2]
+		#@notable = resource.singularize.classify.constantize.find(id)
+    klass = [Student, LessonPlan, Syllabus].detect { |c| params["#{c.name.underscore}_id"] }
+    @notable = klass.find(params["#{klass.name.underscore}_id"])
     @notable_resource = @notable.class.to_s.downcase
 	end
 
