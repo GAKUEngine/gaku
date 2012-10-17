cc9d0d2cff61383b18a97c1607e52956da11d9d6
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-17 13:20:25 +0300

    Fix indentation

--- spec/requests/admin/schools/campuses/campuses_spec.rb
+++ spec/requests/admin/schools/campuses/campuses_spec.rb
@@ -48,22 +48,22 @@ describe 'Campuses' do
 
   context 'index, edit and delete' do 
 
-  	it 'should edit school campus', :js => true do
-  	  within(table) { click edit_link }
-  	  
-  	  wait_until_visible modal 
-  	  fill_in 'campus_name', :with => 'Nagoya Campus'
-  	  click submit_button 
+    it 'should edit school campus', :js => true do
+      within(table) { click edit_link }
+      
+      wait_until_visible modal 
+      fill_in 'campus_name', :with => 'Nagoya Campus'
+      click submit_button 
 
-  	  wait_until_invisible modal
+      wait_until_invisible modal
       within(table) do 
-  	    page.should have_content('Nagoya Campus')
-  	    page.should_not have_content('Nagoya University') 
+        page.should have_content('Nagoya Campus')
+        page.should_not have_content('Nagoya University') 
       end
-  	  @school.campuses.count.should eq 1
-  	end
+      @school.campuses.count.should eq 1
+    end
 
-  	it 'should delete school', :js => true do
+    it 'should delete school', :js => true do
       @school.campuses.count.should eq 1 
       tr_count = size_of table_rows
 
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,21 +1,21 @@
 module Helpers
   module Request
 
-  	def edit_link 
-  	  '.edit-link'
-  	end
+    def edit_link 
+      '.edit-link'
+    end
 
-  	def delete_link 
-  	  '.delete-link'
-  	end
+    def delete_link 
+      '.delete-link'
+    end
 
-  	def accept_alert
-  		page.driver.browser.switch_to.alert.accept
-  	end
+    def accept_alert
+      page.driver.browser.switch_to.alert.accept
+    end
 
-  	def size_of(selector)
-  		page.all(selector).size
-  	end
+    def size_of(selector)
+      page.all(selector).size
+    end
   
     def wait_until_visible(selector)
       wait_until { find(selector).visible? }
