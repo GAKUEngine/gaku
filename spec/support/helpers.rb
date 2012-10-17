d1aeb7ccde3b071cf4a362611593072441a6d4a4
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-17 13:13:50 +0300

    Add spec/request helpers

---
+++ spec/support/helpers.rb
@@ -0,0 +1,37 @@
+module Helpers
+  module Request
+
+  	def edit_link 
+  	  '.edit-link'
+  	end
+
+  	def delete_link 
+  	  '.delete-link'
+  	end
+
+  	def accept_alert
+  		page.driver.browser.switch_to.alert.accept
+  	end
+
+  	def size_of(selector)
+  		page.all(selector).size
+  	end
+  
+    def wait_until_visible(selector)
+      wait_until { find(selector).visible? }
+    end
+
+    def wait_until_invisible(selector)
+      wait_until { !page.find(selector).visible? }
+    end
+
+    def click(selector)
+      find(selector).click
+    end
+
+  end
+end
+
+RSpec.configure do |config|
+  config.include Helpers::Request, :type => :request
+end
