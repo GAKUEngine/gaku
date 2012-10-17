85d4fc4d9af45007313eedbd0d52637eb9ad939e
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-17 13:14:53 +0300

    [spec/requests] Refactor admin/schools/campuses/campuses_spec to use new helpers

--- spec/requests/admin/schools/campuses/campuses_spec.rb
+++ spec/requests/admin/schools/campuses/campuses_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe 'Campuses' do
+
+  form = '#new-admin-school-campus'
+  new_link = '#new-admin-school-campus-link'
+  modal = '#edit-campus-modal'
+
+  submit_button = '#submit-admin-school-campus-button'
+  cancel_link = '#cancel-admin-school-campus-link'
+  
+  table = 'table#admin-school-campuses-index'
+  table_rows = 'table#admin-school-campuses-index tr'
+
   stub_authorization!
 
   before do 
@@ -10,44 +21,42 @@ describe 'Campuses' do
 
   context 'create and show' do
     before do 
-      click_link 'new-admin-school-campus-link'
-      wait_until { find('#new-admin-school-campus').visible? }
+      click new_link
+      wait_until_visible submit_button
     end
     it 'should create and show school campus', :js => true do 
       @school.campuses.count.should eq 1
 
       fill_in 'campus_name', :with => 'Nagoya Campus'
-      click_button 'submit-admin-school-campus-button'
+      click submit_button
 
-      wait_until { !page.find('#new-admin-school-campus').visible? }
+      wait_until_invisible form
 
       page.should have_content('Nagoya Campus')
       @school.campuses.count.should eq 2
     end 
 
     it 'should cancel creating', :js => true do
-      wait_until { !page.find('#cancel-admin-school-campus-link').visible? }
-      click_link 'cancel-admin-school-campus-link'
+      click cancel_link
 
-      wait_until { !page.find('#new-admin-school-campus').visible? }
-      click_link 'new-admin-school-campus-link'
+      wait_until_invisible form
+      click new_link
 
-      wait_until { find('#new-admin-school-campus').visible? }
+      wait_until_visible submit_button
     end
   end
 
   context 'index, edit and delete' do 
 
   	it 'should edit school campus', :js => true do
-  	  within('table#admin-school-campuses-index tbody') { find('.edit-link').click }
+  	  within(table) { click edit_link }
   	  
-  	  wait_until { find('#edit-campus-modal').visible? } 
+  	  wait_until_visible modal 
   	  fill_in 'campus_name', :with => 'Nagoya Campus'
-  	  click_button 'submit-admin-school-campus-button' 
+  	  click submit_button 
 
-  	  wait_until { !page.find('#edit-campus-modal').visible? }
-
-      within('table#admin-school-campuses-index tbody') do 
+  	  wait_until_invisible modal
+      within(table) do 
   	    page.should have_content('Nagoya Campus')
   	    page.should_not have_content('Nagoya University') 
       end
@@ -56,12 +65,12 @@ describe 'Campuses' do
 
   	it 'should delete school', :js => true do
       @school.campuses.count.should eq 1 
-      tr_count = page.all('table#admin-school-campuses-index tr').size
+      tr_count = size_of table_rows
 
-      find('.delete-link').click 
-      page.driver.browser.switch_to.alert.accept
+      click delete_link 
+      accept_alert
         
-      wait_until { page.all('table#admin-school-campuses-index tr').size == tr_count - 1 }
+      wait_until { size_of(table_rows) == tr_count - 1 }
       @school.campuses.count.should eq 0
     end
   end
