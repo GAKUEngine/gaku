680be45de6c6eab1bd5da1d023f8e4456cbfb932
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-10-19 09:34:51 +0300

    [specs] add notes specs for exams

---
+++ spec/requests/exams/notes_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+
+describe 'Exam Notes' do
+  stub_authorization!
+  
+  before do
+    @exam = create(:exam)
+    visit exam_path(@exam) 
+  end
+
+  context 'new' do
+    before do 
+      click_link 'new-note-link'
+      wait_until { find("#submit-exam-note-button").visible? }
+    end
+
+    it "should add and show exam note", :js => true do
+      @exam.notes.size.should eql(0)
+      tr_count = page.all('table#exam-notes-index tr').size
+ 
+      fill_in "note_title", :with => "The note title"
+      fill_in "note_content", :with => "The note content"
+      click_button "submit-exam-note-button"
+
+      wait_until { !page.find('#new-note form').visible? } 
+      page.should have_selector('a', href: "/exams/1/notes/1/edit")
+      page.should have_content("The note title")
+      page.should have_content("The note content")
+      page.all('table#exam-notes-index tr').size == tr_count + 1
+      within('.exam-notes-count') { page.should have_content('Notes list(1)') }
+      @exam.reload
+      @exam.notes.size.should eql(1)
+    end
+
+    it "should error if there are empty fields", :js => true do 
+      click_button "submit-exam-note-button"
+      wait_until do
+         page.should have_selector('div.note_titleformError') 
+         page.should have_selector('div.note_contentformError') 
+      end
+      @exam.notes.size.should eql(0)
+    end
+
+    it 'should cancel adding', :js => true do 
+      click_link 'cancel-note-link'
+      wait_until { !page.find('#new-note form').visible? }
+      find('#new-note-link').visible?
+
+      click_link 'new-note-link'
+      wait_until { find('#new-note form').visible? }
+      !page.find('#new-note-link').visible?
+    end
+  end
+
+  context "edit and delete" do 
+    before do 
+      @note = create(:note, :notable => @exam)
+      visit exam_path(@exam)
+    end
+
+    it "should edit a exam note", :js => true do 
+      find(".edit-link").click 
+
+      wait_until { find('#edit-note-modal').visible? } 
+      fill_in 'note_title', :with => 'Edited note title'
+      fill_in 'note_content', :with => 'Edited note content'
+      click_button 'submit-exam-note-button'
+
+      wait_until { !page.find('#edit-note-modal').visible? }
+      page.should have_content('Edited note title')
+      page.should have_content('Edited note content')
+    end
+
+    it 'should cancel editting', :js => true do
+      find(".edit-link").click
+
+      wait_until { find('#edit-note-modal').visible? }
+      click_link 'cancel-exam-note-link'
+      wait_until { !page.find('#edit-note-modal').visible? }
+    end
+
+    it "should delete a exam note", :js => true do
+      @exam.notes.size.should eql(1)
+      wait_until { page.has_content?('Notes') }
+        
+      tr_count = page.all('table#exam-notes-index tr').size
+      page.should have_content(@note.title)
+      
+      find('.delete-link').click 
+      page.driver.browser.switch_to.alert.accept
+
+      wait_until { page.all('table#exam-notes-index tr').size == tr_count - 1 } 
+      within('.exam-notes-count') { page.should_not have_content('Notes list(1)') }
+      page.should_not have_content(@note.title)
+      @exam.notes.size.should eql(0)
+    end
+  end
+end
