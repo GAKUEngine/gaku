0e57a5f262b11b6ebbe37256b23e46c9f495c40e
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-19 12:11:15 +0300

    [spec/requests] More flash helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -59,6 +59,18 @@ module Helpers
       page.should have_selector("#notice", :text => text)
     end
 
+    def flash_created?
+      flash 'successfully created'
+    end
+
+    def flash_updated?
+      flash 'successfully updated'
+    end
+
+    def flash_destroyed?
+      flash 'successfully destroyed'
+    end
+
     def flash_error_for(field)
       page.should have_selector("div.#{field}formError")
     end
