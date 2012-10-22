34da60ce7cb3c2b12555c576041fb37af0369185
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-10-22 13:35:05 +0300

    [views][locales] add flash notices to notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,6 +19,7 @@ class NotesController < ApplicationController
   def create
   	@note = @notable.notes.new(params[:note])
   	@note.save
+    flash.now[:notice] = t('notes.created')
   	render 'create', :locals => {:notable_resource => @notable_resource}
   end
 
@@ -44,6 +45,7 @@ class NotesController < ApplicationController
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
+    flash.now[:notice] = t('notes.destroyed')
     respond_to do |format|
       format.js { render 'destroy', :locals => {:notable_resource => @notable_resource} }
     end
