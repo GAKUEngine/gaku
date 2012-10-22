83bfee7b2ba946a8fee9d23390e4bad9fd66249f
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-22 17:01:58 +0300

    [spec/requests] Refactor syllabus/notes_spec

--- spec/requests/syllabuses/notes_spec.rb
+++ spec/requests/syllabuses/notes_spec.rb
@@ -1,98 +1,91 @@
 require 'spec_helper'
 
-describe 'Syllabus Notes' do
+describe 'Course Notes' do
+
   stub_authorization!
+
+  before :all do
+    Helpers::Request.resource("syllabus-note")
+  end
   
   before do
     @syllabus = create(:syllabus)
     visit syllabus_path(@syllabus) 
   end
 
-  context 'new' do
+  context 'new', :js => true do
     before do 
-      click_link 'new-note-link'
-      wait_until { find("#submit-syllabus-note-button").visible? }
+      click new_link
+      wait_until_visible submit
     end
 
-    it "should add and show syllabus note", :js => true do
-      @syllabus.notes.size.should eql(0)
-      tr_count = page.all('table#syllabus-notes-index tr').size
- 
-      fill_in "note_title", :with => "The note title"
-      fill_in "note_content", :with => "The note content"
-      click_button "submit-syllabus-note-button"
-
-      wait_until { !page.find('#new-note form').visible? } 
-      page.should have_selector('a', href: "/syllabuses/1/notes/1/edit")
-      page.should have_content("The note title")
-      page.should have_content("The note content")
-      page.all('table#syllabus-notes-index tr').size == tr_count + 1
-      within('.syllabus-notes-count') { page.should have_content('Notes list(1)') }
-      @syllabus.reload
-      @syllabus.notes.size.should eql(1)
+    it "creates and shows" do
+      expect do 
+        fill_in "note_title",   :with => "The note title"
+        fill_in "note_content", :with => "The note content"
+        click submit
+        wait_until_invisible form
+      end.to change(@syllabus.notes, :count).by 1
+       
+      page.should have_content "The note title"
+      page.should have_content "The note content"
+      within(count_div) { page.should have_content 'Notes list(1)' }
+      flash_created?
     end
 
-    it "should error if there are empty fields", :js => true do 
-      click_button "submit-syllabus-note-button"
+    it "errors without required fields"  do 
+      click submit
       wait_until do
-         page.should have_selector('div.note_titleformError') 
-         page.should have_selector('div.note_contentformError') 
+         flash_error_for 'note_title' 
+         flash_error_for 'note_content' 
       end
-      @syllabus.notes.size.should eql(0)
     end
 
-    it 'should cancel adding', :js => true do 
-      click_link 'cancel-note-link'
-      wait_until { !page.find('#new-note form').visible? }
-      find('#new-note-link').visible?
-
-      click_link 'new-note-link'
-      wait_until { find('#new-note form').visible? }
-      !page.find('#new-note-link').visible?
+    it 'cancels creating', :js => true do 
+      ensure_cancel_creating_is_working
     end
   end
 
-  context "edit and delete" do 
+  context "existing", :js => true do 
     before do 
       @note = create(:note, :notable => @syllabus)
       visit syllabus_path(@syllabus)
     end
 
-    it "should edit a syllabus note", :js => true do 
-      find(".edit-link").click 
+    context 'edit' do 
+      before do 
+        within(table) { click edit_link }
+        wait_until_visible modal
+      end
 
-      wait_until { find('#edit-note-modal').visible? } 
-      fill_in 'note_title', :with => 'Edited note title'
-      fill_in 'note_content', :with => 'Edited note content'
-      click_button 'submit-syllabus-note-button'
+      it "edits" do 
+        fill_in 'note_title',   :with => 'Edited note title'
+        fill_in 'note_content', :with => 'Edited note content'
+        click submit
 
-      wait_until { !page.find('#edit-note-modal').visible? }
-      page.should have_content('Edited note title')
-      page.should have_content('Edited note content')
+        wait_until_invisible modal
+        page.should have_content 'Edited note title'
+        page.should have_content 'Edited note content'
+        flash_updated?
+      end
+
+      it 'cancels editting'  do
+        ensure_cancel_modal_is_working
+      end
     end
 
-    it 'should cancel editting', :js => true do
-      find(".edit-link").click
+    it "deletes" do
+      page.should have_content @note.title
+      within(count_div) { page.should have_content 'Notes list(1)' }
 
-      wait_until { find('#edit-note-modal').visible? }
-      click_link 'cancel-syllabus-note-link'
-      wait_until { !page.find('#edit-note-modal').visible? }
-    end
-
-    it "should delete a syllabus note", :js => true do
-      @syllabus.notes.size.should eql(1)
-      wait_until { page.has_content?('Notes') }
-        
-      tr_count = page.all('table#syllabus-notes-index tr').size
-      page.should have_content(@note.title)
+      expect do 
+        ensure_delete_is_working
+      end.to change(@syllabus.notes, :count).by -1
       
-      find('.delete-link').click 
-      page.driver.browser.switch_to.alert.accept
-
-      wait_until { page.all('table#syllabus-notes-index tr').size == tr_count - 1 } 
-      within('.syllabus-notes-count') { page.should_not have_content('Notes list(1)') }
-      page.should_not have_content(@note.title)
-      @syllabus.notes.size.should eql(0)
+      within(count_div) { page.should_not have_content 'Notes list(1)' }
+      page.should_not have_content @note.title
+      flash_destroyed?
     end
   end
+
 end
