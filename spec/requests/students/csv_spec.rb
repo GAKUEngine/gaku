5f65e0781ea0884d87b6c375bdfbb88d21af2aa8
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-22 09:56:24 +0300

    [spec/requests] Refactor students/csv_spec

--- spec/requests/students/csv_spec.rb
+++ spec/requests/students/csv_spec.rb
@@ -1,43 +1,45 @@
 require 'spec_helper'
 
-describe 'CSV' do
+describe 'Student CSV' do
+
   stub_authorization!
 
   context 'download' do 
-    it 'should export students as CSV' do
+    it 'exports as CSV' do
       create(:student, :name => 'John', :surname => 'Doe')
       create(:student, :name => 'Susumu', :surname => 'Yokota')
       visit students_path
 
       click_link 'export-students-link'
       page.response_headers['Content-Type'].should eq "text/csv"
-      page.should have_content('surname,name')
-      page.should have_content('Doe,John')
-      page.should have_content('Yokota,Susumu')
+      page.should have_content 'surname,name'
+      page.should have_content 'Doe,John'
+      page.should have_content 'Yokota,Susumu'
     end
 
-    it 'should download student registration csv' do 
+    it 'downloads registration CSV' do 
       visit students_path
       click_link 'import-students-link'
       click_link 'get_registration_csv'
 
       page.response_headers['Content-Type'].should eq "text/csv"
-      page.should have_content('surname,name,surname_reading,name_reading,gender,phone,email,birth_date,admitted')
+      page.should have_content 'surname,name,surname_reading,name_reading,gender,phone,email,birth_date,admitted'
     end
   end
 
   context 'upload' do 
-    it 'should import students from CSV' do
-      Student.all.count.should eql(0)
+    it 'imports from CSV' do
       visit students_path
-      click_link 'import-students-link'
-      select "GAKU Engine", :from => 'importer_importer_type'
-      absolute_path = Rails.root + "spec/support/students.csv"
-      attach_file('importer_data_file', absolute_path)
-      click_button 'Submit'
 
-      page.should have_content('created students:2')
-      Student.all.count.should eql(2)
+      expect do 
+        click_link 'import-students-link'
+        select "GAKU Engine", :from => 'importer_importer_type'
+        absolute_path = Rails.root + "spec/support/students.csv"
+        attach_file 'importer_data_file', absolute_path
+        click_button 'Submit'
+      end.to change(Student, :count).by 2
+
+      page.should have_content 'created students:2'
     end
   end
   
