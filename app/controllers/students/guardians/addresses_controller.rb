b310be9fe8b2e016dee2338124145cbbe11b76c4
Georgi Tapalilov <tapalilov@gmail.com>
2012-10-23 13:51:40 +0300

    [view/fix] adding flash message to guardian address

--- app/controllers/students/guardians/addresses_controller.rb
+++ app/controllers/students/guardians/addresses_controller.rb
@@ -27,6 +27,7 @@ class Students::Guardians::AddressesController < ApplicationController
       if @address.id == @primary_address_id
         @guardian.guardian_addresses.first.make_primary unless @guardian.guardian_addresses.blank?
         respond_to do |format|
+          flash.now[:notice] = t('addresses.destroyed')
           format.js { render }
         end
       else
