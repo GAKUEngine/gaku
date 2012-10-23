98c9b87acf311bfac953189be1654a575b51bcad
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-23 10:26:26 +0300

    [spec/requests] Refactor students/guardians/contacts

--- spec/requests/students/guardians/contacts_spec.rb
+++ spec/requests/students/guardians/contacts_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
-describe 'Guardian Contacts' do
+describe 'Student Guardian Contacts' do
+
   stub_authorization!
+
+  tab_link = "#student-guardians-tab-link"
+
+  before :all do
+    Helpers::Request.resource("student-guardian-contact") 
+  end
 
   before(:each) do
     @student = create(:student)
@@ -11,137 +18,125 @@ describe 'Guardian Contacts' do
     @contact_type = create(:contact_type, :name => 'mobile')
 
     visit student_path(@student) 
-    click_link 'new-student-guardian-tab-link'
-    wait_until { page.has_content?('Guardians list') } 
+    click tab_link
+    wait_until { page.has_content? 'Guardians list' } 
   end
 
-  context 'new' do 
+  context 'new', :js => true  do 
+
     context 'thru modal' do 
-      it "should add and show contact to a student guardian thru modal", :js => true do 
-        @student.guardians.first.contacts.count.should eql(0)
-        tr_count = page.all('table#student-guardian-contacts-index tr').size
-        click_link "new-student-guardian-contact-link"
-
-        wait_until { find('#new-contact-modal').visible? } 
-        select 'mobile', :from => 'contact_contact_type_id'
-        fill_in 'contact_data',    :with => '777'
-
-        click_button 'submit-student-guardian-contact-button'
-        wait_until { !page.find('#new-contact-modal').visible? }
-
-        find('.show-link').click
-        page.should have_content 'mobile'
-        page.should have_content '777'
-        @student.guardians.first.contacts.count.should eql(1)
+      before do 
+        click new_link
+        wait_until_visible modal
       end
 
-      it 'should cancel adding thru modal', :js => true do 
-        click_link "new-student-guardian-contact-link"
-        wait_until { find('#new-contact-modal').visible? }
+      it "creates and shows" do  
+        expect do 
+          select 'mobile',           :from => 'contact_contact_type_id'
+          fill_in 'contact_data',    :with => '777'
 
-        click_link 'cancel-student-guardian-contact-link'
-        wait_until { !page.find('#new-contact-modal').visible? }
+          click submit
+          wait_until_invisible modal
+        end.to change(@student.guardians.first.contacts, :count).by 1
+
+        click show_link
+        page.should have_content 'mobile'
+        page.should have_content '777'
+        within(count_div) { page.should have_content 'Contacts list(1)' }
+      end
+
+      it 'cancels creating thru modal' do 
+        ensure_cancel_modal_is_working
       end
     end
     
     context 'thru slide form' do 
-      it "should add and show contact to a student guardian thru slide form", :js => true do 
-        find('.show-link').click
-        @student.guardians.first.contacts.count.should eql(0)
-        tr_count = page.all('table#student-guardian-contacts-index tr').size
-
-        click_link "new-student-guardian-contact-link"
-
-        wait_until { find('#new-student-guardian-contact form').visible? } 
-        select 'mobile', :from => 'contact_contact_type_id'
-        fill_in 'contact_data',    :with => '777'
-
-        click_button 'submit-student-guardian-contact-button'
-
-        wait_until { !page.find('#new-student-guardian-contact form').visible? }
-        page.should have_content 'mobile'
-        page.should have_content '777'
-        page.all('table#student-guardian-contacts-index tr').size == tr_count + 1
-        within('.student-guardian-contacts-count') { page.should have_content('Contacts list(1)') }
-        @student.guardians.first.contacts.count.should eql(1)
+      before do 
+        click show_link
+        click new_link
+        wait_until_visible submit
       end
 
-      it 'should cancel adding thru slide form', :js => true do 
-        find('.show-link').click
-        click_link "new-student-guardian-contact-link"
-        wait_until { find('#submit-student-guardian-contact-button').visible? }
-        !page.find('#new-student-guardian-contact-link').visible?
+      it "creates and shows" do 
+        expect do 
+          select 'mobile', :from => 'contact_contact_type_id'
+          fill_in 'contact_data',    :with => '777'
 
-        click_link 'cancel-student-guardian-contact-link'
-        wait_until { !page.find('#new-student-guardian-contact form').visible? }
-        find('#new-student-guardian-contact-link').visible?
+          click submit
+         wait_until_invisible form
+        end.to change(@student.guardians.first.contacts, :count).by 1
 
-        click_link "new-student-guardian-contact-link"
-        wait_until { find('#new-student-guardian-contact form').visible? }
-        !page.find('#new-student-guardian-contact-link').visible?
+        page.should have_content 'mobile'
+        page.should have_content '777'
+        within(count_div) { page.should have_content 'Contacts list(1)' }
+        flash_created?
+      end
+
+      it 'cancels creating' do 
+        ensure_cancel_creating_is_working
       end
     end
   end
 
-  context 'edit, delete and set primary' do 
-
+  context 'existing' do 
     before do 
       mobile1 = create(:contact, :data => 123, :contact_type => @contact_type)
-      mobile2 = create(:contact, :data => 321, :contact_type => @contact_type)
-      @student.guardians.first.contacts << [ mobile1, mobile2 ]
+      @student.guardians.first.contacts << mobile1
       @student.reload
-      find('.show-link').click
+    end
+    
+    context 'edit', :js => true do 
+      before do 
+        click show_link
+        page.should have_content '123'
+        within(table) { click edit_link }
+        wait_until_visible modal
+      end
+
+      it 'edits' do 
+        fill_in 'contact_data', :with => '777'
+        click submit
+
+        wait_until_invisible modal
+        page.should have_content '777'
+        page.should_not have_content '123'
+        flash_updated?
+      end
+
+      it 'cancels editting' do 
+        ensure_cancel_modal_is_working
+      end
     end
 
-    it 'should edit contact for student guardian', :js => true do 
-      page.should have_content '321'
+    it 'deletes', :js => true  do
+      click show_link
+      page.should have_content '123'
+      within(count_div) { page.should have_content 'Contacts list(1)' }
 
-      within('table#student-guardian-contacts-index tr#contact-2') { find('.edit-link').click }
-      wait_until { find('#edit-contact-modal').visible? } 
+      expect do
+        ensure_delete_is_working 
+      end.to change(@student.guardians.first.contacts, :count).by -1
 
-      fill_in 'contact_data', :with => '777'
-      click_button 'submit-student-guardian-contact-button'
-      wait_until { !page.find('#edit-contact-modal').visible? }
-      page.should have_content '777'
-      page.should_not have_content '321'
+      within(count_div) { page.should_not have_content 'Contacts list(1)' }
+      page.should_not have_content '123'
+      flash_destroyed?
     end
 
-    it 'should cancel editting', :js => true do 
-      within('table#student-guardian-contacts-index tr#contact-2') { find('.edit-link').click }
-      wait_until { find('#edit-contact-modal').visible? } 
+    it 'sets primary', :js => true do 
+      mobile2 = create(:contact, :data => 321, :contact_type => @contact_type)
+      @student.guardians.first.contacts << mobile2
+      click show_link
 
-      click_link 'cancel-student-guardian-contact-link'
-      wait_until { !page.find('#edit-contact-modal').visible? }
-    end
-
-    it 'should delete a contact for student guardian', :js => true  do
-      page.should have_content '321'
-      within('.student-guardian-contacts-count') { page.should have_content('Contacts list(2)') }
-      @student.guardians.first.contacts.size.should eql(2)
-      tr_count = page.all('#student-guardian-contacts-index tr').size
-      
-      find('.delete-link').click
-      page.driver.browser.switch_to.alert.accept
-
-      wait_until { page.all('#student-guardian-contacts-index tr').size == tr_count - 1 } 
-      within('.student-guardian-contacts-count') { page.should_not have_content('Contacts list(2)') }
-      @student.guardians.first.contacts.size.should eql(1)
-      #page.should_not have_content '321'
-    end
-
-    it 'should make a primary contact to student guardian', :js => true do 
       @student.guardians.first.contacts.first.is_primary? == true
       @student.guardians.first.contacts.second.is_primary? == false
 
-      within("table#student-guardian-contacts-index tr#contact-#{@student.guardians.first.contacts.second.id}") do
+      within("#{table} tr#contact-#{@student.guardians.first.contacts.second.id}") do
         click_link 'set-primary-link' 
       end 
-      page.driver.browser.switch_to.alert.accept
+      accept_alert
 
       @student.guardians.first.contacts.first.is_primary? == false
       @student.guardians.first.contacts.second.is_primary? == true
-
-      #TODO Check the css classes of primary button
     end
 
   end
