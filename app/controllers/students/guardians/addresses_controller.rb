43d8906aeedf5e1baf74741dc9a01f287c08b2ec
Georgi Tapalilov <tapalilov@gmail.com>
2012-10-23 22:14:45 +0300

    [view/refactor] refactor guardian and students addresses to use right partials and fix updating and make primary show after updating

--- app/controllers/students/guardians/addresses_controller.rb
+++ app/controllers/students/guardians/addresses_controller.rb
@@ -13,7 +13,7 @@ class Students::Guardians::AddressesController < ApplicationController
 	def create
     super do |format|
       if @guardian.addresses << @address
-        @primary_address_id = @guardian.guardian_addresses.find_by_is_primary(true).address.id rescue nil
+        load_primary_address        
         format.js { render 'create' }  
       end
     end  
@@ -57,8 +57,7 @@ class Students::Guardians::AddressesController < ApplicationController
     end
 
 	  def load_primary_address
-		  @guardian = Guardian.find(params[:guardian_id])
-      @primary_address_id = @guardian.guardian_addresses.find_by_is_primary(true).address.id rescue nil
+      @primary_address = @guardian.guardian_addresses.find_by_is_primary(true)
     end
 
 end
