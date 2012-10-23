0db68587f0d9955f7d677846ee15540040c35855
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-23 11:08:47 +0300

    [spec/requests] Refactor students/guardians/guardians_spec

--- spec/requests/students/guardians/guardians_spec.rb
+++ spec/requests/students/guardians/guardians_spec.rb
@@ -1,105 +1,107 @@
 require 'spec_helper'
 
-describe 'Guardian' do
+describe 'Student Guardians' do
+
   stub_authorization!
+
+  tab_link = "#student-guardians-tab-link"
+  delete_link = ".delete-student-guardian-link"
+
+  before :all do
+    Helpers::Request.resource("student-guardian") 
+  end
 
   before do
     @student = create(:student)
     visit student_path(@student)
   end
 
-  context 'new' do 
+  context 'new', :js => true do 
     before do 
-      click_link 'new-student-guardian-tab-link'
-      click_link 'new-student-guardian-link'
-      wait_until { find('#submit-student-guardian-button').visible? } 
+      click tab_link
+      click new_link
+      wait_until_visible submit
     end
 
-    it "should add and show student guardian", :js => true do
-      @student.guardians.size.should eql(0)
+    it "creates and shows" do
+      expect do 
+        #required 
+        fill_in "guardian_surname",         :with => "Doe"
+        fill_in "guardian_name",            :with => "John"
 
-      #required 
-      fill_in "guardian_surname", :with => "Doe"
-      fill_in "guardian_name", :with => "John"
+        fill_in "guardian_surname_reading", :with => "Phonetic Doe"
+        fill_in "guardian_name_reading",    :with => "Phonetic John"
+        fill_in "guardian_relationship",    :with => "Father"
 
-      fill_in "guardian_surname_reading", :with => "Phonetic Doe"
-      fill_in "guardian_name_reading", :with => "Phonetic John"
-      fill_in "guardian_relationship", :with => "Father"
+        click submit
+        wait_until_invisible form
+      end.to change(@student.guardians, :count).by 1
 
-      click_button "submit-student-guardian-button"
-
-      wait_until { !page.find('#new-student-guardian form').visible? }
-      page.should have_selector('a', href: "/students/1/guardians/1/edit")
       #required
-      page.should have_content("Doe")
-      page.should have_content("John")
+      page.should have_content "Doe"
+      page.should have_content "John"
 
-      page.should have_content("Phonetic Doe")
-      page.should have_content("Phonetic John")
-      page.should have_content("Father")
-      within('.student-guardians-count') { page.should have_content('Guardians list(1)') }
-      within('#new-student-guardian-tab-link') { page.should have_content('Guardians(1)') }
-      @student.reload
-      @student.guardians.size.should eql(1)
+      page.should have_content "Phonetic Doe"
+      page.should have_content "Phonetic John"
+      page.should have_content "Father"
+      within(count_div) { page.should have_content 'Guardians list(1)' }
+      within(tab_link)  { page.should have_content 'Guardians(1)' }
+      flash_created?
     end
 
-    it 'should cancel adding', :js => true do
-      click_link 'cancel-student-guardian-link'
-      wait_until { !page.find('#new-student-guardian form').visible? }  
-      find('#new-student-guardian-link').visible?
-
-      click_link 'new-student-guardian-link'
-      wait_until { find('#new-student-guardian form').visible? }  
-      !page.find('#new-student-guardian-link').visible?
+    it 'cancels creating' do
+      ensure_cancel_creating_is_working
     end
   end
 
-  context "edit and delete" do 
+  context "existing" do 
     before(:each) do 
       @guardian = create(:guardian)
       @student.guardians << @guardian
       @student.reload
 
       visit student_path(@student) 
-      click_link 'new-student-guardian-tab-link'
-      wait_until { page.has_content?('Guardians list') } 
+      click tab_link
+      wait_until { page.has_content? 'Guardians list' } 
     end
 
-    it "should edit a student guardian", :js => true do 
-      find(".edit-link").click 
-      wait_until { find('#edit-guardian-modal').visible? } 
+    context 'edit', :js => true do 
+      before do 
+        click edit_link
+        wait_until_visible modal
+      end
 
-      fill_in 'guardian_name',    :with => 'Edited guardian name'
-      fill_in 'guardian_surname', :with => 'Edited guardian surname'
-      click_button "submit-student-guardian-button"
+      it "edits" do 
+        fill_in 'guardian_name',    :with => 'Edited guardian name'
+        fill_in 'guardian_surname', :with => 'Edited guardian surname'
+        click submit
 
-      wait_until { !page.find('#edit-guardian-modal').visible? }
-      page.should have_content('Edited guardian name')
-      page.should have_content('Edited guardian surname')
+        wait_until_invisible modal
+        page.should have_content 'Edited guardian name'
+        page.should have_content 'Edited guardian surname'
+        flash_updated?
+      end
+
+      it 'cancels editting' do 
+        ensure_cancel_modal_is_working
+      end
     end
 
-    it 'should cancel editting', :js => true do 
-      find(".edit-link").click 
-      wait_until { find('#edit-guardian-modal').visible? }
+    it "deletes", :js => true do
+      page.should have_content @guardian.name
+      within(count_div) { page.should have_content 'Guardians list(1)' }
+      within(tab_link)  { page.should have_content 'Guardians(1)' }
 
-      click_link 'cancel-student-guardian-link'
-      wait_until { !page.find('#edit-guardian-modal').visible? }
-    end
+      expect do 
+        click delete_link 
+        accept_alert
+        wait_until { within("#student-guardians") { page.should_not have_content @guardian.name } }
+      end.to change(@student.guardians, :count).by -1
 
-    it "should delete a student guardian", :js => true do
-      @student.guardians.size.should eql(1)
-      page.should have_content(@guardian.name)
-      within('.student-guardians-count') { page.should have_content('Guardians list(1)') }
-      within('#new-student-guardian-tab-link') { page.should have_content('Guardians(1)') }
-
-      find('.delete-student-guardian-link').click 
-      page.driver.browser.switch_to.alert.accept
-      
-      page.should_not have_content(@guardian.name)
-      within('.student-guardians-count') { page.should_not have_content('Guardians list(1)') }
-      within('#new-student-guardian-tab-link') { page.should_not have_content('Guardians(1)') }
-      @student.reload
-      @student.guardians.size.should eql(0)
+      within(count_div) { page.should_not have_content 'Guardians list(1)' }
+      within(tab_link)  { page.should_not have_content 'Guardians(1)' }
+      page.should_not have_content @guardian.name
+      flash_destroyed?
     end
   end
   
