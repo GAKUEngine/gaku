313920e6ba6d720f8fd39202486b06dbd153ab1b
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-10-30 17:03:39 +0200

    [models][migrations] fix models and audit migration

--- core/db/migrate/20121013102921_install_gaku_audited.rb
+++ core/db/migrate/20121013102921_install_gaku_audited.rb
@@ -1,6 +1,6 @@
 class InstallGakuAudited < ActiveRecord::Migration
   def self.up
-    create_table :gaku_audits, :force => true do |t|
+    create_table :audits, :force => true do |t|
       t.column :auditable_id, :integer
       t.column :auditable_type, :string
       t.column :associated_id, :integer
@@ -16,13 +16,13 @@ class InstallGakuAudited < ActiveRecord::Migration
       t.column :created_at, :datetime
     end
 
-    add_index :gaku_audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
-    add_index :gaku_audits, [:associated_id, :associated_type], :name => 'associated_index'
-    add_index :gaku_audits, [:user_id, :user_type], :name => 'user_index'
-    add_index :gaku_audits, :created_at
+    add_index :audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
+    add_index :audits, [:associated_id, :associated_type], :name => 'associated_index'
+    add_index :audits, [:user_id, :user_type], :name => 'user_index'
+    add_index :audits, :created_at
   end
 
   def self.down
-    drop_table :gaku_audits
+    drop_table :audits
   end
 end
