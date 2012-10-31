4859fde610860d52d60fd791b25d2ff98d424dde
Georgi Tapalilov <tapalilov@gmail.com>
2012-10-31 12:41:03 +0200

    change notable instance variable

--- core/app/controllers/gaku/class_groups_controller.rb
+++ core/app/controllers/gaku/class_groups_controller.rb
@@ -45,7 +45,7 @@ module Gaku
 
       def load_before_show
         @notable = ClassGroup.find(params[:id])
-        @notable_resource = @notable.class.to_s.underscore.gsub("_","-")
+        @notable_resource = @notable.class.to_s.underscore.split('/')[1].gsub("_","-")
         @course = Course.new
         @courses = Course.all
         @semester = Semester.new
