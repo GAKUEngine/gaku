5d7fdb18dd61d8204973669a228b430dfc8dab5a
Vassil Kalkov <vassilkalkov@gmail.com>
2012-10-31 13:54:54 +0200

    Fix core gaku gems and deface

--- admissions/app/overrides/admissions_admin_menu.rb
+++ admissions/app/overrides/admissions_admin_menu.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "gaku/shared/menu/admin",
+Deface::Override.new(:virtual_path => "gaku/shared/menu/_admin",
                      :name => "admissions_admin_menu",
-                     :insert_after => "#admin_menu",
-                     :text => "Hello")
+                     :insert_before => "[data-hook='types_sub_menu']",
+                     :partial => "gaku/shared/menu/admissions_sub_menu")
