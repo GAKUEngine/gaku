b871a4b0489e5523a4c72dec0781ba504ac49b88
Georgi Tapalilov <tapalilov@gmail.com>
2012-10-31 12:40:34 +0200

    remove devise error

--- core/app/controllers/gaku/admin/contact_types_controller.rb
+++ core/app/controllers/gaku/admin/contact_types_controller.rb
@@ -9,11 +9,6 @@ module Gaku
 
       before_filter :contact_types_count, :only => [:create, :destroy]
 
-      def current_user
-        # @current_user = 'ala-bala'
-        @current_user ||= user_from_remember_token
-      end
-
       private
         def contact_types_count 
         	@contact_types_count = ContactType.count
