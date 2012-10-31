ecfd5f86e6bf641a4514f4812be7932264cd11ec
Georgi Tapalilov <tapalilov@gmail.com>
2012-10-31 16:54:41 +0200

    [fix] syllabuses namespace by specs fails

--- core/app/controllers/gaku/syllabuses/exam_syllabuses_controller.rb
+++ core/app/controllers/gaku/syllabuses/exam_syllabuses_controller.rb
@@ -3,9 +3,9 @@ module Gaku
 
 		def destroy
 			@syllabus = Syllabus.find(params[:syllabus_id])
-			@exam_syllabus = ExamSyllabus.find(params[:id])
-			@exam_syllabus.destroy
-			
+			exam_syllabus = ExamSyllabus.find(params[:id])
+			exam_syllabus.destroy
+			logger.debug "#{@syllabus_exams.inspect}"
 			flash.now[:notice] = 'Exam was successfully destroyed.'
 			respond_to do |format|
 				format.js { render 'destroy' }
@@ -16,7 +16,6 @@ module Gaku
 			@syllabus = Syllabus.find(params[:syllabus_id])
 			@exam_syllabus = @syllabus.exam_syllabuses.build(params[:exam_syllabus])
 			@exam_syllabus.save
-			
 			respond_to do |format|
 				flash.now[:notice] = 'Exam added to Syllabus'
 				format.js { render 'create' }
