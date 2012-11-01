dd091092f6b50707d5c77087f49108b105ec10d0
Vassil Kalkov <vassilkalkov@gmail.com>
2012-11-01 14:32:09 +0200

    Rearrange backbone files

--- core/app/assets/javascripts/gaku_core.js
+++ core/app/assets/javascripts/gaku_core.js
@@ -31,9 +31,5 @@
 //= require underscore
 //= require backbone
 //
-// require .//gaku_engine
-// require_tree ..//templates
-// require_tree .//models
-// require_tree .//collections
-// require_tree .//views
-// require_tree ./routers
+
+//= require_tree .//backbone
