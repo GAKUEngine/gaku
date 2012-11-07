1908a0600da04238f87dc8cae6b643cf3d95224d
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-11-07 13:14:28 +0200

    [controllers][views][js] add admission_methods/admission_phases functionality

--- admissions/config/routes.rb
+++ admissions/config/routes.rb
@@ -1,7 +1,9 @@
 Gaku::Core::Engine.routes.prepend do
 	namespace :admin do 
 		resources :admissions
-    resources :admission_methods
+    resources :admission_methods do
+      resources :admission_phases, :controller => 'admission_methods/admission_phases'
+    end
 	end
 
 end
