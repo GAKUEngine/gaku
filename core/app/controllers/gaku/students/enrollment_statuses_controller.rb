1b3b7734d6eb1ad6f1e2fec2dd30f7962b28919b
Georgi Tapalilov <tapalilov@gmail.com>
2012-11-16 15:44:00 +0200

    [refacotr] refactor reverting

--- core/app/controllers/gaku/students/enrollment_statuses_controller.rb
+++ core/app/controllers/gaku/students/enrollment_statuses_controller.rb
@@ -4,29 +4,30 @@ module Gaku
 
   	respond_to :js, :html
 
+    before_filter :load_enrollment_status
   	before_filter :load_student, :only => [:revert, :edit, :update]
   
   	def history
-  		@enrollment_status = EnrollmentStatus.find(params[:id])
       @enrollment_status_history = @enrollment_status.history
-  		
+      
       respond_with(@enrollment_status_history) do |format| 
   			format.js { render 'history' }
   		end
   	end
 
     def revert 
-      @enrollment_status = EnrollmentStatus.find(params[:id])
-      prev_enrollment_status_type_id = @enrollment_status.audits.last.audited_changes["enrollment_status_type_id"][0]
-      @enrollment_status.update_attribute(:enrollment_status_type_id, prev_enrollment_status_type_id)
-      @enrollment_status.audits.last(2).each {|s| s.destroy}
+      @enrollment_status.revert unless @enrollment_status.audits.count.equal?(1)
 
       respond_with(@enrollment_status) do |format| 
         format.js { render 'revert' }
       end
     end
 
-  	private
+    private
+
+    def load_enrollment_status
+      @enrollment_status = EnrollmentStatus.find(params[:id])
+    end
 
   	def load_student
   		@student = Student.find(params[:student_id])
