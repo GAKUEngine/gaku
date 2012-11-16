06d3397e267fb766f2668c652fff142cfa1a7ebd
MKostova <mkostova@camplight.net>
2012-11-16 19:14:44 +0200

    [spec/controllers] added specs for admin presets

---
+++ core/spec/controllers/gaku/admin/presets_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Gaku::Admin::PresetsController do
+
+  describe "GET #students" do
+    it "is successful" do
+      gaku_get :students
+      response.should be_success
+    end
+
+    it "renders the :students view" do
+      gaku_get :students
+      response.should render_template :students
+    end
+  end 
+
+  describe "GET #locale" do
+    
+    it "is successful" do
+    gaku_get :locale
+    response.should be_success
+  end
+
+  it "renders the :locale view" do
+    gaku_get :locale
+    response.should render_template :locale
+    end
+  end
+
+  describe "GET #grading" do
+    it "is successful" do
+    gaku_get :grading
+    response.should be_success
+  end
+
+  it "renders the :grading view" do
+    gaku_get :grading
+    response.should render_template :grading
+    end
+  end
+
+end
