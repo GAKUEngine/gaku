642dc795f3ab79e5ddf4628bebcd781480f30f12
影月 零 <zero@genshin.org>
2012-11-20 16:22:20 +0900

    updated sample seeds for admissions and added users

--- sample/db/sample/gaku/admission_data.rb
+++ sample/db/sample/gaku/admission_data.rb
@@ -1,35 +1,7 @@
 # -*- encoding: utf-8 -*-
 
-#admission_methods = [
-#  { :name => "Summer Program",
-#    admission_phases = [
-#      { :name => "Written Application",
-#        admission_phase_states = [
-#          { :name => "Received" },
-#          { :name => "In Review" },
-#          { :name => "Accepted", :auto_progress => true, :can_progress => true },
-#          { :name => "Rejected", :can_progress => false }
-#        ]
-#      },
-#      { :name => "Written Report",
-#        admission_phase_states = [
-#          { :name => "In Review" },
-#          { :name => "Accepted", :auto_progress => true, :can_progress => true, :can_admit => true },
-#          { :name => "Rejected", :can_progress => false }
-#        ]
-#      },
-#      { :name => "Interview",
-#        states = [
-#          { :name => "Waiting for Interview" },
-#          { :name => "Accepted", :can_admit => true, :auto_admit => true },
-#          { :name => "Rejected", :can_admit => false }
-#        ]
-#      }
-#    ]
-#  },
 #  { :name => "Early Admissions"},
 #  { :name => "International Division Admissions"},
-#  { :name => "Regular Admissions"},
 #  { :name => "一般入学"},
 #  { :name => "推薦入学"},
 #  { :name => "国際コース入学"}
@@ -48,41 +20,155 @@
 #  end
 #end
 
-### Summer Program Sample Admission Method
-method = Gaku::AdmissionMethod.create({ :name => "Summer Program Admissions"})
-
-phase = Gaku::AdmissionPhase.create({ :name => "Written Application"})
-
-phase_states = [
-  { :name => "Received" },
-  { :name => "In Review" },
-  { :name => "Accepted", #:auto_progress => true, 
-    :can_progress => true },
-  { :name => "Rejected", :can_progress => false }
-]
-
+def add_states_to_phase(phase_states, phase)
 phase_states.each do |state_data|
   phase_state = Gaku::AdmissionPhaseState.create(state_data)
   phase.admission_phase_states << phase_state
 end
 phase.save
-
-method.admission_phases << phase
-method.save
-
-
-admission_periods = [
-  { :name => "Summer 2013"},
-  { :name => "Fall 2013 Early Admissions"},
-  { :name => "Fall 2013"},
-  { :name => "2013年夏短期プログラム"},
-  { :name => "2013年秋推薦"},
-  { :name => "2013年秋"}
-]
-
-admission_periods.each do |period_data|
-  Gaku::AdmissionPeriod.create(period_data)
 end
+
+def create_sample_admission_method(method_args, phase_array)
+  method = Gaku::AdmissionMethod.create(method_args)
+  phase_array.each do |phase_info|
+    phase = Gaku::AdmissionPhase.create(phase_info[:args])
+    add_states_to_phase(phase_info[:states], phase)
+    method.admission_phases << phase
+  end
+
+  # Save the method
+  method.save
+
+  return method
+end
+
+regular_method = create_sample_admission_method(
+  { :name => "Regular Admissions" },
+  [
+    {
+      :args => { :name => "Written Application" },
+      :states => [
+        { :name => "Received" },
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Written Report" },
+      :states => [
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true, :can_admit => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Exam" },
+      :states => [
+        { :name => "Pre-Exam" },
+        { :name => "Passed", :can_admit => true, :can_progress => true, :auto_progress => true },
+        { :name => "Rejected", :can_admit => false, :can_progress => false },
+        { :name => "Abscent", :can_admit => false, :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Interview" },
+      :states => [
+        { :name => "Waiting for Interview" },
+        { :name => "Accepted", :can_admit => true, :auto_admit => true },
+        { :name => "Rejected", :can_admit => false }
+      ]
+    }
+  ]
+)
+
+international_division_method = create_sample_admission_method(
+  { :name => "International Division Admissions" },
+  [
+    {
+      :args => { :name => "Written Application" },
+      :states => [
+        { :name => "Received" },
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Interview" },
+      :states => [
+        { :name => "Waiting for Interview" },
+        { :name => "Accepted", :can_admit => true, :auto_admit => true },
+        { :name => "Rejected", :can_admit => false }
+      ]
+    },{
+      :args => { :name => "Exam" },
+      :states => [
+        { :name => "Pre-Exam" },
+        { :name => "Passed", :can_admit => true, :can_progress => true, :auto_progress => true },
+        { :name => "Rejected", :can_admit => false, :can_progress => false },
+        { :name => "Abscent", :can_admit => false, :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Foreign Langauge Exam" },
+      :states => [
+        { :name => "Pre-Exam" },
+        { :name => "Passed with Fluent Score", :can_admit => true, :can_progress => true, :auto_progress => true, :auto_admit => true },
+        { :name => "Passed", :can_admit => true, :can_progress => true, :auto_progress => true },
+        { :name => "Rejected", :can_admit => false, :can_progress => false },
+        { :name => "Abscent", :can_admit => false, :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Written Report" },
+      :states => [
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true, :can_admit => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    }
+  ]
+)
+
+summer_method = create_sample_admission_method(
+  { :name => "Summer Program Admissions"},
+  [
+    {
+      :args => { :name => "Written Application" },
+      :states => [
+        { :name => "Received" },
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Written Report" },
+      :states => [
+        { :name => "In Review" },
+        { :name => "Accepted", :auto_progress => true, :can_progress => true, :can_admit => true },
+        { :name => "Rejected", :can_progress => false }
+      ]
+    },{
+      :args => { :name => "Interview" },
+      :states => [
+        { :name => "Waiting for Interview" },
+        { :name => "Accepted", :can_admit => true, :auto_admit => true },
+        { :name => "Rejected", :can_admit => false }
+      ]
+    }
+  ]
+)
+
+# Periods
+period = Gaku::AdmissionPeriod.create({ :name => "Summer 2013" })
+period.admission_methods << summer_method
+
+period = Gaku::AdmissionPeriod.create({ :name => "Fall 2013 Early Admissions" })
+
+period = Gaku::AdmissionPeriod.create({ :name => "Fall 2013" })
+period.admission_methods << regular_method
+period.admission_methods << international_division_method
+
+period = Gaku::AdmissionPeriod.create({ :name => "2013年夏短期プログラム" })
+
+period = Gaku::AdmissionPeriod.create({ :name => "2013年秋推薦" })
+
+period = Gaku::AdmissionPeriod.create({ :name => "2013年秋" })
 
 #{}"admission_method_id", "admitted", "created_at", 
 #{}"scholarship_status_id", "student_id", "updated_at"
