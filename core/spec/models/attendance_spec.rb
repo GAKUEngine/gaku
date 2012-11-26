0f96d27c266d917db8fd3452dc9e4ac3ab184d07
Georgi Tapalilov <tapalilov@gmail.com>
2012-11-26 14:03:17 +0200

    [spec] model specs for attendance type

--- core/spec/models/attendance_spec.rb
+++ core/spec/models/attendance_spec.rb
@@ -5,6 +5,7 @@ describe Gaku::Attendance do
   context "validations" do 
   	it { should have_valid_factory(:attendance) }
 
+  	it { should belong_to(:attendance_type)}
   	it { should belong_to(:attendancable) }
 
   	it { should allow_mass_assignment_of :reason }
