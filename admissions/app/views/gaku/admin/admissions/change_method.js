598f629f4b3167477d9f5f38711d6b7572ac83fc
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2012-11-29 16:01:48 +0200

    fix admission forms and links ids

--- admissions/app/views/gaku/admin/admissions/change_method.js
+++ admissions/app/views/gaku/admin/admissions/change_method.js
@@ -1,3 +1,3 @@
 alert('#{@admission_method}')
 $('#admission-phases').html('<%= render_js_partial("phases", {:admission_method => @admission_method}) %>');
-$('#new-admin-admission-links').html('<%= render_js_partial("links", {:admission_method => @admission_method}) %>');
+$('#new-admin-admission-link').html('<%= render_js_partial("links", {:admission_method => @admission_method}) %>');
