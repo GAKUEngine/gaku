756a3bcf2c39900db349e6da3d804bfbecd7fa0f
Georgi Tapalilov <tapalilov@gmail.com>
2012-11-30 14:27:38 +0200

    [controller] admin attendance types controller

--- core/app/controllers/gaku/admin/attendance_types_controller.rb
+++ core/app/controllers/gaku/admin/attendance_types_controller.rb
@@ -3,13 +3,20 @@ module Gaku
     class AttendanceTypesController < Admin::BaseController
 
     	inherit_resources
+      respond_to :js, :html, :json
+      before_filter :count, :only => [:create, :destroy, :index]
 
-    	respond_to :js, :html, :json
 
       def index
         super do |format|
           format.json { render :json => @attendance_types.to_json(:root => false)}  
         end
+      end
+
+      private
+
+      def count
+        @count = AttendanceType.count
       end
     end
   end
