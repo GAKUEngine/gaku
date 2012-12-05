e8463f96bb4b7d84c14e92af50522b08486c3a40
Marta Kostova <mkostova@camplight.net>
2012-12-05 16:32:24 +0200

    [spec/requests] refactored admissions and factories

--- admissions/spec/factories/admission_phase_factory.rb
+++ admissions/spec/factories/admission_phase_factory.rb
@@ -3,4 +3,36 @@ FactoryGirl.define do
     name 'Admission Phase 1'
     order 1
   end
+
+  factory :admission_phase_exam, :class => Gaku::AdmissionPhase do
+    name 'Exam'
+    order 1
+    after_build do |phase_exam|
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_pre_exam, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_passed, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_rejected, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_abscent, :admission_phase => phase_exam)
+    end
+  end
+  factory :admission_phase_interview, :class => Gaku::AdmissionPhase do
+    name 'Interview'
+    order 2
+    after_build do |phase_interview|
+      phase_interview.admission_phase_states << FactoryGirl.build(:admission_phase_state_waiting, :admission_phase => phase_interview)
+      phase_interview.admission_phase_states << FactoryGirl.build(:admission_phase_state_accepted, :admission_phase => phase_interview)
+      phase_interview.admission_phase_states << FactoryGirl.build(:admission_phase_state_rejected, :admission_phase => phase_interview)
+    end
+  end
+  factory :admission_phase_lang_exam, :class => Gaku::AdmissionPhase do
+    name 'Foreign Language Exam'
+    order 3
+    after_build do |phase_exam|
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_pre_exam, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_passed, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_passed_fluent, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_rejected, :admission_phase => phase_exam)
+      phase_exam.admission_phase_states << FactoryGirl.build(:admission_phase_state_abscent, :admission_phase => phase_exam)
+    end
+  end
+  
 end
--- admissions/spec/requests/admissions_spec.rb
+++ admissions/spec/requests/admissions_spec.rb
@@ -4,21 +4,9 @@ describe 'Admin Admissions' do
 
   stub_authorization!
 
-  let(:admission_period_no_methods) { create(:admission_period) }
+  let(:admission_period_no_methods) { create(:admission_period_no_methods) }
   let(:admission_period) { create(:admission_period) }
-  let(:admission_method_international) { create(:admission_method, name: 'International Division Admissions') }
-  let(:admission_method_standart) { create(:admission_method, name: 'Regular Admissions') }
-  let(:admission_phase_exam) { create(:admission_phase, name: 'Exam') }
-  let(:admission_phase_interview) { create(:admission_phase, name: 'Interview') }
-  let(:admission_phase_lang_exam) { create(:admission_phase, name: 'Foreign Language Exam') }
-  let(:admission_phase_state_pre_exam) { create(:admission_phase_state, name: 'Pre-Exam') }
-  let(:admission_phase_state_passed) { create(:admission_phase_state, name: 'Passed') }
-  let(:admission_phase_state_abscent) { create(:admission_phase_state, name: 'Abscent') }
-  let(:admission_phase_state_accepted) { create(:admission_phase_state, name: 'Accepted') }
-  let(:admission_phase_state_waiting) { create(:admission_phase_state, name: 'Waiting for interview') }
-  let(:admission_phase_state_rejected) { create(:admission_phase_state, name: 'Rejected') }
-
-
+  
   describe 'when select admission period', js: true do
     context 'when period has no methods' do
       before do
@@ -33,28 +21,8 @@ describe 'Admin Admissions' do
 
     context 'when period has methods' do
       before do
-        admission_phase_interview.admission_phase_states<<admission_phase_state_waiting
-        admission_phase_interview.admission_phase_states<<admission_phase_state_accepted
-        admission_phase_interview.admission_phase_states<<admission_phase_state_rejected
 
-        admission_phase_exam.admission_phase_states<<admission_phase_state_pre_exam
-        admission_phase_exam.admission_phase_states<<admission_phase_state_passed
-        admission_phase_exam.admission_phase_states<<admission_phase_state_abscent
-
-        admission_phase_lang_exam.admission_phase_states<<admission_phase_state_pre_exam
-        admission_phase_lang_exam.admission_phase_states<<admission_phase_state_passed
-        admission_phase_lang_exam.admission_phase_states<<admission_phase_state_abscent
-
-        admission_method_international.admission_phases<<admission_phase_lang_exam
-        admission_method_international.admission_phases<<admission_phase_exam
-        admission_method_international.admission_phases<<admission_phase_interview
-
-        admission_method_standart.admission_phases<<admission_phase_exam
-        admission_method_standart.admission_phases<<admission_phase_interview
-
-        admission_period.admission_methods<<admission_method_standart
-        admission_period.admission_methods<<admission_method_international
- 
+        admission_period
         visit gaku.admin_admissions_path
 
         @active_tab = page.find('.nav-tabs .active')
@@ -68,24 +36,24 @@ describe 'Admin Admissions' do
         end
 
         it 'selects the first method' do
-          within ('#admission-method-selection') { page.should have_content "#{admission_method_standart.name}" }
+          within ('#admission-method-selection') { page.should have_content "#{admission_period.admission_methods.first.name}" }
         end
 
         it 'shows first method\'s phases' do
           within (@nav_tabs) do
-            page.should have_content "#{admission_method_standart.admission_phases.first.name}"
-            page.should have_content "#{admission_method_standart.admission_phases.last.name}" 
+            page.should have_content "#{admission_period.admission_methods.first.admission_phases.first.name}"
+            page.should have_content "#{admission_period.admission_methods.first.admission_phases.last.name}" 
           end
         end
 
         it 'open first phase\'s tab' do
-          within(@active_tab) { page.should have_content "#{admission_method_standart.admission_phases.first.name}" }
+          within(@active_tab) { page.should have_content "#{admission_period.admission_methods.first.admission_phases.first.name}" }
         end
 
         it 'shows first phase states' do
           within(@active_tab_content) do
-            page.should have_content "#{admission_method_standart.admission_phases.first.admission_phase_states.first.name}"
-            page.should have_content "#{admission_method_standart.admission_phases.first.admission_phase_states.last.name}"
+            page.should have_content "#{admission_period.admission_methods.first.admission_phases.first.admission_phase_states.first.name}"
+            page.should have_content "#{admission_period.admission_methods.first.admission_phases.first.admission_phase_states.last.name}"
           end
         end
 
@@ -94,24 +62,34 @@ describe 'Admin Admissions' do
       context 'when change method' do
         before do
           select "#{admission_period.name}", from: 'admission_period'
-          within('#admission-method-selection') { select "#{admission_method_international.name}", from: 'admission_method' }
+          within('#admission-method-selection') { select "#{admission_period.admission_methods.last.name}", from: 'admission_method' }
         end
 
         it 'shows selected method\'s phases' do
-          within @nav_tabs  do
-            page.should have_content "#{admission_method_international.admission_phases.first.name}"
-            page.should have_content "#{admission_method_international.admission_phases.last.name}" 
+          within (@nav_tabs)  do
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.first.name}"
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.last.name}" 
           end
         end
 
         it 'open first phase\'s tab' do
-          within(@active_tab) { page.should have_content "#{admission_method_international.admission_phases.first.name}" }
+          within (@active_tab) { page.should have_content "#{admission_period.admission_methods.last.admission_phases.first.name}" }
         end
 
         it 'shows first phase states' do
-          within(@active_tab_content) do
-            page.should have_content "#{admission_phase_lang_exam.admission_phase_states.first.name}"
-            page.should have_content "#{admission_phase_lang_exam.admission_phase_states.last.name}"
+          within (@active_tab_content) do
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.first.admission_phase_states.first.name}"
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.first.admission_phase_states.last.name}"
+          end
+        end
+
+        it 'navigate thru phases' do
+          within (@nav_tabs) { click_on "#{admission_period.admission_methods.last.admission_phases.last.name}" }
+          wait_for_ajax
+          #@active_tab_content = page.find('.tab-content .active')
+          within (page.find('.tab-content .active')) do
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.last.admission_phase_states.first.name}"
+            page.should have_content "#{admission_period.admission_methods.last.admission_phases.last.admission_phase_states.last.name}"
           end
         end
 
