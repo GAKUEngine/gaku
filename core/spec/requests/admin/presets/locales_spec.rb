d197ad3f184132ecfac53274e36fadc65dd2f284
Marta Kostova <mkostova@camplight.net>
2012-12-07 15:05:48 +0200

    [request specs] admin presets

--- core/spec/requests/admin/presets/locales_spec.rb
+++ core/spec/requests/admin/presets/locales_spec.rb
@@ -4,4 +4,17 @@ describe 'Admin Presets Locales' do
 
   stub_authorization!
 
+  before do
+    visit gaku.locale_admin_presets_path
+  end
+  context '#default', js:true do
+    it 'saves' do
+
+      select 'en', from:'presets_language'
+      click '.btn' 
+
+      flash_updated?
+    end
+  end
+
 end
--- core/spec/requests/admin/presets/students_spec.rb
+++ core/spec/requests/admin/presets/students_spec.rb
@@ -4,4 +4,23 @@ describe 'Admin Presets Students' do
 
   stub_authorization!
 
+  let(:country) { create(:country) }
+  let(:state) { create(:state, country:country) }
+  let(:address) { create(:address, country:country, state:state) }
+
+  before do
+    address
+    visit gaku.students_admin_presets_path
+  end
+  context '#default', js:true do
+    it 'saves' do
+
+      select 'Female', from:'presets_students_gender'
+      select "#{address.country}", from:'presets_address_country'
+      fill_in 'presets_address_state', with: "#{address.state.name}"
+      click '.btn' 
+
+      flash_updated?
+    end
+  end
 end
