ac32ce7daf1c63e8a4876547916efe29e83a788e
Vassil Kalkov <vassilkalkov@gmail.com>
2012-12-11 14:46:55 +0200

    Refactor notes

--- core/app/controllers/gaku/notes_controller.rb
+++ core/app/controllers/gaku/notes_controller.rb
@@ -1,71 +1,37 @@
 module Gaku
   class NotesController < GakuController
 
-  	before_filter :load_notable
-    
+  	before_filter :notable
     inherit_resources
-
-    actions :index, :show, :new, :create, :update, :edit, :destroy
-
-    def index
-    	@notes = @notable.notes
-      render 'index'
-    end
+    respond_to :js, :html
 
     def new
       @note = @notable.notes.new
-      render 'new', :locals => {:notable_resource => @notable_resource}
+      new!
     end
 
     def create
     	@note = @notable.notes.new(params[:note])
-    	@note.save
-      flash.now[:notice] = t('notice.created', :resource => resource_name)
-    	render 'create', :locals => {:notable_resource => @notable_resource}
+      create!
     end
 
-    def edit
-      super do |format|
-        format.js { render 'edit' }  
-      end  
+    protected
+
+    def collection
+      @notes = @notable.notes
     end
 
-    def show
-      @note = Note.find(params[:id])
-      super do |format|
-        format.js { render 'show' }  
-      end  
-    end
-    
-    def update
-      super do |format|
-        format.js { render 'update' }  
-      end  
-    end
+    private
 
-    def destroy
-      @note = Note.find(params[:id])
-      @note.destroy
-      flash.now[:notice] = t('notice.destroyed', :resource => resource_name)
-      respond_to do |format|
-        format.js { render 'destroy', :locals => {:notable_resource => @notable_resource} }
-      end
-    end
-
-  private
-  	def load_notable
+  	def notable
       unnamespaced_klass = ''
-      klass = [Gaku::Student, Gaku::LessonPlan, Gaku::Syllabus, Gaku::ClassGroup, Gaku::Course, Gaku::Exam].detect do |c| 
+      klass = [Gaku::Student, Gaku::LessonPlan, Gaku::Syllabus, Gaku::ClassGroup, Gaku::Course, Gaku::Exam].detect do |c|
         unnamespaced_klass = c.to_s.split("::")
         params["#{unnamespaced_klass[1].underscore}_id"]
       end
 
       @notable = klass.find(params["#{unnamespaced_klass[1].underscore}_id"])
       @notable_resource = @notable.class.to_s.underscore.split('/')[1].gsub("_","-")
-    end
-
-    def resource_name
-      t('note.singular')
     end
 
   end
