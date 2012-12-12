24498ad642a6b2821902ef2d290f5d10da7ba300
Georgi Tapalilov <tapalilov@gmail.com>
2012-12-12 17:00:34 +0200

    [refactor] exam portion controller

--- core/app/controllers/gaku/exams/exam_portions_controller.rb
+++ core/app/controllers/gaku/exams/exam_portions_controller.rb
@@ -3,42 +3,21 @@ module Gaku
 
     inherit_resources
     actions :index, :show, :new, :create, :update, :edit, :destroy
+    belongs_to :exam
 
     respond_to :js, :html
 
     before_filter :exam
     before_filter :count, :only => [:create, :destroy]
 
-    def create
-      @exam_portion = @exam.exam_portions.build(params[:exam_portion])
-      if @exam_portion.save
-        respond_with @exam_portion
-      end
-    end
-
-
-    def destroy
-      @exam_portion = ExamPortion.find(params[:id])
-      @portion_id = @exam_portion.id
-      @exam_portion.destroy
-      @total_weight = get_total_weight(@exam.exam_portions)
-      super do |format|
-        format.js { render 'destroy' }
-      end
-    end
-
     private
 
-      def exam
-        @exam = Exam.find(params[:exam_id])
-      end
+    def exam
+      @exam = Exam.find(params[:exam_id])
+    end
 
-      def get_total_weight(portions)
-        portions.inject(0) {|sum, p| sum + p.weight }
-      end
-
-      def count
-        @count = @exam.exam_portions.count
-      end
+    def count
+      @count = @exam.exam_portions.count
+    end
   end
 end
