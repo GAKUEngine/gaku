6a5a671cd949055470203585448e35855c55f216
Vassil Kalkov <vassilkalkov@gmail.com>
2012-12-12 12:11:21 +0200

    Refactor admin/disposals

--- core/app/controllers/gaku/admin/disposals_controller.rb
+++ core/app/controllers/gaku/admin/disposals_controller.rb
@@ -16,14 +16,14 @@ module Gaku
 			end
 
 			private
+			
+		  def sort_column
+	      Student.column_names.include?(params[:sort]) ? params[:sort] : "name"
+	    end
 
-			  def sort_column
-		      Student.column_names.include?(params[:sort]) ? params[:sort] : "name"
-		    end
-
-		    def sort_direction
-		      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
-		    end
+	    def sort_direction
+	      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+	    end
 
 		end
 	end
