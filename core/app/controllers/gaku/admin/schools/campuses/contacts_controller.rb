09e5cf7ca29cc4fe8fa6a0c79954bfb40caef2bb
Vassil Kalkov <vassilkalkov@gmail.com>
2012-12-12 10:51:15 +0200

    Refactor admin/schools

--- core/app/controllers/gaku/admin/schools/campuses/contacts_controller.rb
+++ core/app/controllers/gaku/admin/schools/campuses/contacts_controller.rb
@@ -3,70 +3,58 @@ module Gaku
     class Schools::Campuses::ContactsController < GakuController
 
       inherit_resources
-      #actions :index, :show, :create, :update, :edit, :destroy
-
       respond_to :js, :html
 
-      before_filter :campus, :only => [ :new, :create, :edit, :update, :destroy ]
-      before_filter :contact_types, :only => [:new,:edit]
+      before_filter :campus
+      before_filter :contact_types, :only => [:new, :edit]
       before_filter :count, :only => [:create, :destroy]
-
-      def new
-        @contact = Contact.new
-        new!
-      end
-
+      before_filter :contact, :except => [:new, :create]
+      
       def create
         super do |format|
           if @contact.save && @campus.contacts << @contact
             @contact.make_primary_campus if params[:contact][:is_primary] == "1"
           end
-          format.js { render 'create' }
+          format.js { render :create }
         end
       end
 
       def update
-        super do |format|
-          @contacts = Contact.where(:campus_id => params[:campus_id])
-          @contact.make_primary_campus if params[:contact][:is_primary] == "1"
-          format.js { render 'update' }
-        end
+        @contacts = Contact.where(:campus_id => params[:campus_id])
+        @contact.make_primary_campus if params[:contact][:is_primary] == "1"
+        update!
       end
 
       def destroy
-        super do |format|
-          if @contact.is_primary?
-            @campus.contacts.first.make_primary_campus if @student.contacts.any?
-            format.js { render }
-          else
-            format.js { render 'destroy' }
-          end
-
-        end
+        @campus.contacts.first.make_primary_campus if @contact.is_primary?
+        destroy!
       end
 
       def make_primary
         @contact = Contact.find(params[:id])
         @contact.make_primary_campus
-
-        respond_with(@contact) do |format|
-          format.js { render 'make_primary' }
-        end
+        respond_with(@contact)
       end
 
       private
-        def campus
-          @campus = Campus.find(params[:campus_id])
-        end
 
-        def contact_types
-          @contact_types = ContactType.all
-        end
+      def contact
+        @contact = Contact.find(params[:id])
+      end
 
-        def count
-          campus
-          @count = @campus.contacts.count
-        end
+      def campus
+        @campus = Campus.find(params[:campus_id])
+      end
+
+      def contact_types
+        @contact_types = ContactType.all
+      end
+
+      def count
+        campus
+        @count = @campus.contacts.count
+      end
+
     end
   end
 end
