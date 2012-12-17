60e44b7808e89bc24f1b73bb426b4f46fdb8b83b
Marta Kostova <mkostova@camplight.net>
2012-12-17 19:42:55 +0200

    [specs|models] model validations

--- core/spec/models/role_spec.rb
+++ core/spec/models/role_spec.rb
@@ -5,6 +5,9 @@ describe Gaku::Role do
   context "validations" do 
   	it { should belong_to(:class_group_enrollment) }
   	it { should belong_to(:faculty) }
+
+    it { should allow_mass_assignment_of :name }
+    it { should allow_mass_assignment_of :class_group_enrollment_id }
   end
   
 end
