6f049298c9cea2d70e86aff9e5939ca1b44c095b
Marta Kostova <mkostova@camplight.net>
2012-12-20 14:23:50 +0200

    [models] validations

--- core/spec/models/school_history_spec.rb
+++ core/spec/models/school_history_spec.rb
@@ -3,10 +3,13 @@ require 'spec_helper'
 describe Gaku::SchoolHistory do
 
   context "validations" do 
-  	it { should belong_to :school }
-  	it { should belong_to :student }
+    it { should belong_to :school }
+    it { should belong_to :student }
+    it { should belong_to :admission }
+
 
     it { should allow_mass_assignment_of :school_id }
     it { should allow_mass_assignment_of :student_id }
+    it { should allow_mass_assignment_of :admission_id }
   end
 end
