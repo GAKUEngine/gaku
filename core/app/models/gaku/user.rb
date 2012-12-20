8fa5fca31246caedde2c4928449bb3b9f5db9686
Georgi Tapalilov <tapalilov@gmail.com>
2012-12-20 14:39:38 +0200

    [auth] auth by username or password

--- core/app/models/gaku/user.rb
+++ core/app/models/gaku/user.rb
@@ -25,19 +25,29 @@ module Gaku
     # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
-  	
-  	#in :accessors should be added all settings that user can set and check againts Preset         
+
+  	#in :accessors should be added all settings that user can set and check againts Preset
     store :settings, :accessors => [:locale]
 
-    #ActiveRecord::Store accessors can be validated as other model attributes 
+    attr_accessor :login
+    #ActiveRecord::Store accessors can be validated as other model attributes
     # validates :language, :numericality => true
 
-    attr_accessible :email, :password, :password_confirmation, :remember_me, :locale
-  
+    attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me, :locale
+
     before_create :default_language
 
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+
     private
-    
+
     def default_language
       self.settings[:locale] = Gaku::Preset.get('language')
     end
--- core/spec/models/user_spec.rb
+++ core/spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe Gaku::User do
 
-  context "validations" do 
+  context "validations" do
+
+    it { should allow_mass_assignment_of :username }
+    it { should allow_mass_assignment_of :login }
+    it { should respond_to(:login) }
   end
-  
+
 end
