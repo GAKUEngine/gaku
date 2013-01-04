8d50249d32a44364c74144e939979edc1140d224
Georgi Tapalilov <tapalilov@gmail.com>
2013-01-04 15:39:58 +0200

    [drag&drop] position refresh on exam portion delete

--- core/app/models/gaku/exam_portion.rb
+++ core/app/models/gaku/exam_portion.rb
@@ -33,6 +33,7 @@ module Gaku
 	  validates :max_score, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
 
 	  before_create :proper_position
+	  after_destroy :refresh_positions
 
 
 	  def student_score(student)
@@ -44,6 +45,13 @@ module Gaku
 	  def proper_position
 	 		self.position = self.exam.exam_portions.count
 	  end
+
+	  def refresh_positions
+      exam_portions = self.exam.exam_portions
+      exam_portions.pluck(:id).each_with_index do |id, index|
+        exam_portions.update_all( {:position => index}, {:id => id} )
+      end
+    end
 
 	end
 end
