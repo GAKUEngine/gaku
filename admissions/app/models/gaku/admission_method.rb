9de47b1b03b3595a4be413e0b200378438af883f
Georgi Tapalilov <tapalilov@gmail.com>
2013-01-04 15:01:36 +0200

    [drag&drop] admission phases, handle deleting and creating positioning

--- admissions/app/models/gaku/admission_method.rb
+++ admissions/app/models/gaku/admission_method.rb
@@ -1,6 +1,6 @@
 module Gaku
   class AdmissionMethod < ActiveRecord::Base
-  	has_many :admission_phases
+  	has_many :admission_phases, :order => :position
   	has_many :admissions
     has_many :admission_periods, :through => :period_method_associations
     has_many :period_method_associations
