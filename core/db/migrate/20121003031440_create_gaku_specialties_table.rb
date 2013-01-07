318ce1a5d63888b03c357713b520db99debe425d
Georgi Tapalilov <tapalilov@gmail.com>
2013-01-07 16:47:16 +0200

    [migration] rename specialities only_mayor filed to only_major

--- core/db/migrate/20121003031440_create_gaku_specialties_table.rb
+++ core/db/migrate/20121003031440_create_gaku_specialties_table.rb
@@ -3,7 +3,7 @@ class CreateGakuSpecialtiesTable < ActiveRecord::Migration
     create_table :gaku_specialties do |t|
     	t.string   :name
     	t.text     :description
-    	t.boolean  :mayor_only, :default => false
+    	t.boolean  :major_only, :default => false
 
       t.timestamps
     end
