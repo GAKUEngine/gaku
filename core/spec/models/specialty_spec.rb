60ef2813ac733597e4b905df8b9352ec3296b821
Vassil Kalkov <vassilkalkov@gmail.com>
2013-01-09 09:12:50 +0200

    Fix specialties

--- core/spec/models/specialty_spec.rb
+++ core/spec/models/specialty_spec.rb
@@ -6,6 +6,7 @@ describe Gaku::Specialty do
     it { should have_many(:student_specialties) }
     it { should have_many(:students).through(:student_specialties) }
 
+    it { should validate_presence_of :name }
     it { should allow_mass_assignment_of :name }
     it { should allow_mass_assignment_of :description }
     it { should allow_mass_assignment_of :major_only }
