773fa446b97057222a523099336c4ab51b3a2a5b
Vassil Kalkov <vassilkalkov@gmail.com>
2013-01-15 15:19:24 +0200

    [core][spec/requests] WIP : Fix students/guardians/address_spec

--- core/lib/gaku/core/testing_support/factories/guardian_factory.rb
+++ core/lib/gaku/core/testing_support/factories/guardian_factory.rb
@@ -2,6 +2,20 @@ FactoryGirl.define do
   factory :guardian, :class => Gaku::Guardian do
   	name { Faker::Name.first_name }
     surname { Faker::Name.last_name }
-    relationship "Relationship" 
+    relationship "Relationship"
   end
+
+  factory :guardian_with_one_address, :parent => :guardian do
+    after_create do |guardian|
+      FactoryGirl.create(:address, :addressable => guardian)
+    end
+  end
+
+  factory :guardian_with_two_addresses, :parent => :guardian do
+    after_create do |guardian|
+      FactoryGirl.create(:address, :addressable => guardian)
+      FactoryGirl.create(:address, :addressable => guardian)
+    end
+  end
+
 end
