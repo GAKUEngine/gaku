e6b796b8f7bd33e9d892f0f061b9a8c894d1d802
Georgi Tapalilov <tapalilov@gmail.com>
2013-01-16 11:52:39 +0200

    [refactor/sample data] refactor sample data for using polymorhic contacts and addresses

--- sample/db/sample/gaku/students.rb
+++ sample/db/sample/gaku/students.rb
@@ -5,18 +5,15 @@ require 'ffaker'
 student = Gaku::Student.where(:name => 'John', :surname => 'Doe').first_or_create!
 
 country = Gaku::Country.find_by_name('日本')
-# address = Gaku::Address.where(:address1 => Faker::Address.street_address, :city => 'Nagoya', :country_id => country.id).first_or_create!
-# Gaku::StudentAddress.where(:student_id => student.id, :address_id => address.id).first_or_create!
+address = student.addresses.create(:address1 => Faker::Address.street_address, :city => 'Nagoya', :country_id => country.id)
 
-# email = Gaku::ContactType.find_by_name('Email')
-# home_phone = Gaku::ContactType.find_by_name('Home Phone')
-# mobile_phone = Gaku::ContactType.find_by_name('Mobile Phone')
+email = Gaku::ContactType.find_by_name('Email')
+home_phone = Gaku::ContactType.find_by_name('Home Phone')
+mobile_phone = Gaku::ContactType.find_by_name('Mobile Phone')
 
-# student_email = Gaku::Contact.where(:data => 'john@example.com', :contact_type_id => email.id).first_or_create!
-# student_home_phone = Gaku::Contact.where(:data => Faker::PhoneNumber.phone_number, :contact_type_id => home_phone.id).first_or_create!
-# student_mobile_phone = Gaku::Contact.where(:data => Faker::PhoneNumber.phone_number, :contact_type_id => mobile_phone.id, :is_primary => true).first_or_create!
-
-# student.contacts << [student_email, student_home_phone, student_mobile_phone]
+student_email = student.contacts.create!(:data => 'john@example.com', :contact_type_id => email.id)
+student_home_phone = student.contacts.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => home_phone.id)
+student_mobile_phone = student.contacts.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => mobile_phone.id, :is_primary => true)
 
 note = Gaku::Note.where(:title => 'Excellent', :content => 'Excellent student').first_or_create!
 student.notes << note
@@ -24,14 +21,11 @@ student.notes << note
 #guardian
 guardian = Gaku::Guardian.create!(:name => Faker::Name.first_name, :surname => Faker::Name.last_name)
 
-# guardian_email = Gaku::Contact.create!(:data => "#{guardian.name}@example.com", :contact_type_id => email.id)
-# guardian_home_phone = Gaku::Contact.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => home_phone.id)
-# guardian_mobile_phone = Gaku::Contact.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => mobile_phone.id, :is_primary => true)
+guardian_email = guardian.contacts.create!(:data => "#{guardian.name}@example.com", :contact_type_id => email.id)
+guardian_home_phone =  guardian.contacts.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => home_phone.id)
+guardian_mobile_phone =  guardian.contacts.create!(:data => Faker::PhoneNumber.phone_number, :contact_type_id => mobile_phone.id, :is_primary => true)
 
-# guardian.contacts << [guardian_email, guardian_home_phone, guardian_mobile_phone]
-
-# guardian_address = Gaku::Address.create!(:address1 => Faker::Address.street_address, :city => 'Nagoya', :country_id => country.id)
-# Gaku::GuardianAddress.where(:guardian_id => guardian.id, :address_id => guardian_address.id).first_or_create!
+guardian_address = guardian.addresses.create!(:address1 => Faker::Address.street_address, :city => 'Nagoya', :country_id => country.id)
 
 student.guardians << guardian
 
