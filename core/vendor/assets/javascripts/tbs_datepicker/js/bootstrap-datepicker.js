7c23b45ca6878059f19bba1d97d2ce8bc44e252a
Nakaya Yukiharu <snowsunny1107@gmail.com>
2013-01-17 15:01:23 +0900

    fix admissions student form

--- core/vendor/assets/javascripts/tbs_datepicker/js/bootstrap-datepicker.js
+++ core/vendor/assets/javascripts/tbs_datepicker/js/bootstrap-datepicker.js
@@ -1,20 +1,16 @@
 /* =========================================================
- * bootstrap-datepicker.js 
- * http://www.eyecon.ro/bootstrap-datepicker
- * =========================================================
- * Copyright 2012 Stefan Petre
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * rails-bootstrap-datepicker.js
+ * 
+ * By Yukiharu [snowsunny] Nakaya
+ * 
+ * Based on bootstrap-datepicker.js
+ * [http://www.eyecon.ro/bootstrap-datepicker]
+ * bootstrap-datepicker.js is Copyright 2012 Stefan Petre
+ * 
+ * All new code Copyright 
+ * Yukiharu [snowsunny] Nakaya of Genshin Souzou K.K.
+ * 
+ * This library is dual licensed under the GPLv3 and AGPLv3
  * ========================================================= */
  
 !function( $ ) {
