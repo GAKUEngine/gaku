3a6284923ffef3ab9d7285d9b98feae21e6928e1
Georgi Tapalilov <tapalilov@gmail.com>
2013-01-23 17:03:41 +0200

    [migration] students has many achievements throught student achievements

---
+++ core/db/migrate/20130123142104_create_gaku_student_achievements_table.rb
@@ -0,0 +1,10 @@
+class CreateGakuStudentAchievementsTable < ActiveRecord::Migration
+  def change
+    create_table :gaku_student_achievements do |t|
+      t.references :student
+      t.references :achievement
+
+      t.timestamps
+    end
+  end
+end
