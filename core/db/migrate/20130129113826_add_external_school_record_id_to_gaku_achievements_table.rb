1c70570cf831bf496a70dc69325eebcf24cbea0c
Vassil Kalkov <vassilkalkov@gmail.com>
2013-01-29 14:11:18 +0200

    Clean core migrations

--- core/db/migrate/20130129113826_add_external_school_record_id_to_gaku_achievements_table.rb
+++
@@ -1,7 +0,0 @@
-class AddExternalSchoolRecordIdToGakuAchievementsTable < ActiveRecord::Migration
-  def change
-    change_table :gaku_achievements do |t|
-      t.references :external_school_record
-    end 
-  end
-end
