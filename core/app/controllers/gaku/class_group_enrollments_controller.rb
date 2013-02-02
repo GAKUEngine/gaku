773487d372c8719437a4e78570bca95befe4929c
Vassil Kalkov <vassilkalkov@gmail.com>
2013-02-02 14:13:17 +0200

    WIP: Refactor class_group_enrollments

--- core/app/controllers/gaku/class_group_enrollments_controller.rb
+++ core/app/controllers/gaku/class_group_enrollments_controller.rb
@@ -1,12 +1,14 @@
 module Gaku
   class ClassGroupEnrollmentsController < GakuController
 
+  include EnrollmentsController
+=begin
     def filtered_students
       @class_group_enrolled_students = ClassGroupEnrollment.where(:class_group_id => params[:class_group_id]).pluck(:student_id)
-     
+
       if @class_group_enrolled_students.blank?
         @students = Student.find(:all, :include => [:addresses, :class_groups, :class_group_enrollments])
-      else 
+      else
         @students = Student.find(:all, :conditions => ['id not in (?)', @class_group_enrolled_students], :include => [:addresses, :class_groups, :class_group_enrollments])
       end
 
@@ -27,7 +29,7 @@ module Gaku
 
     def autocomplete_filtered_students
       @class_group_enrolled_students = ClassGroupEnrollment.where(:class_group_id => params[:class_group_id]).pluck(:student_id)
-     
+
       if @class_group_enrolled_students.blank?
         @students = Student.includes([:addresses, :class_groups, :class_group_enrollments])
                            .where('(surname || " " || name LIKE ?) OR (name || " " || surname LIKE ?)', "%#{params[:term]}%", "%#{params[:term]}%")
@@ -35,49 +37,10 @@ module Gaku
         @students = Student.includes([:addresses, :class_groups, :class_group_enrollments])
                             .where('id not in (?)) and ((surname || " " || name LIKE ?) OR (name || " " || surname LIKE ?)',@class_group_enrolled_students ,"%#{params[:term]}%", "%#{params[:term]}%" )
       end
-      
+
       render json: @students.as_json(:methods => [:address_widget, :class_group_widget,:seat_number_widget])
     end
-
-    def enroll_students
-      params[:selected_students].nil? ? @selected_students = [] : @selected_students = params[:selected_students]
-      @err_enrollments = []
-      @enrollments = []
-      params[:selected_students].each {|student|
-        student_id = student.split("-")[1].to_i
-        class_group_enrollment = ClassGroupEnrollment.new(:class_group_id => params[:class_group_id], :student_id => student_id)   
-        if  class_group_enrollment.save
-          @enrollments << class_group_enrollment
-        else
-          @err_enrollments << class_group_enrollment
-        end
-      }
-      notice = ""
-      if !@enrollments.empty?
-        
-        @enrollments.each {|enrollment|
-          student = Student.find(enrollment.student_id)
-          notice+= "<p>" + student.name + " " + student.surname + ": " + "<span style='color:green;'>Successfully enrolled.</span>" + "</p>"
-        }
-        flash.now[:success] = notice.html_safe
-      end
-      if !@err_enrollments.empty?
-        
-        @err_enrollments.each {|enrollment|
-          student = Student.find(enrollment.student_id)
-          notice+= "<p>" + student.name + " " + student.surname + ": <span style='color:orange;'>" + enrollment.errors.full_messages.join(", ") + "</span></p>"
-        }
-        flash.now[:error] = notice.html_safe
-      end
-      if params[:source] == "class_groups"
-        @class_group = ClassGroup.find(params[:class_group_id])
-        @count = @class_group.class_group_enrollments.count
-        render 'gaku/class_groups/students/enroll_students'
-      else
-        flash.now[:notice] = notice.html_safe
-        render :partial => 'gaku/shared/flash', :locals => {:flash => flash}
-      end
-    end
+=end
 
   end
 end
--- core/spec/models/class_group_spec.rb
+++ core/spec/models/class_group_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe Gaku::ClassGroup do
 
-  context "validations" do 
-    it { should have_many :class_group_enrollments }
-    it { should have_many(:students).through(:class_group_enrollments) }
+  context "validations" do
+    it { should have_many :enrollments }
+    it { should have_many(:students).through(:enrollments) }
     it { should have_many(:class_group_course_enrollments).dependent(:destroy) }
     it { should have_many(:courses).through(:class_group_course_enrollments) }
 
     it { should have_many(:semesters) }
     it { should have_many(:notes) }
-    
+
     it { should validate_presence_of(:name) }
 
     it "is invalid without name" do
       FactoryGirl.build(:class_group, name: nil).should_not be_valid
     end
   end
-  
+
 end
