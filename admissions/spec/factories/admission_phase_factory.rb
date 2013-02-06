537437ad219e8dc26e06cd3cfe16647d345cdef7
Marta Kostova <mkostova@camplight.net>
2013-02-06 22:14:04 +0200

    [factories] fixes

--- admissions/spec/factories/admission_phase_factory.rb
+++ admissions/spec/factories/admission_phase_factory.rb
@@ -7,34 +7,37 @@ FactoryGirl.define do
   factory :admission_phase_exam, :class => Gaku::AdmissionPhase do
     name 'Exam'
     position 1
-    after_build do |phase_exam|
+    after_create do |phase_exam|
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_pre_exam, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_passed, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_rejected, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_abscent, :admission_phase => phase_exam)
-      phase_exam.exam = FactoryGirl.build(:exam)
+      phase_exam.exam = FactoryGirl.create(:exam)
+      phase_exam.save!
     end
   end
 
   factory :admission_phase_interview, :class => Gaku::AdmissionPhase do
     name 'Interview'
     position 2
-    after_build do |phase_interview|
+    after_create do |phase_interview|
       phase_interview.admission_phase_states << FactoryGirl.create(:admission_phase_state_waiting, :admission_phase => phase_interview)
       phase_interview.admission_phase_states << FactoryGirl.create(:admission_phase_state_accepted, :admission_phase => phase_interview)
       phase_interview.admission_phase_states << FactoryGirl.create(:admission_phase_state_rejected, :admission_phase => phase_interview)
+      phase_interview.save!
     end
   end
   
   factory :admission_phase_lang_exam, :class => Gaku::AdmissionPhase do
     name 'Foreign Language Exam'
     position 3
-    after_build do |phase_exam|
+    after_create do |phase_exam|
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_pre_exam, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_passed, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_passed_fluent, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_rejected, :admission_phase => phase_exam)
       phase_exam.admission_phase_states << FactoryGirl.create(:admission_phase_state_abscent, :admission_phase => phase_exam)
+      phase_exam.save!
     end
   end
   
