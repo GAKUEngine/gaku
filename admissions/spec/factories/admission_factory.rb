ddf0003c9dd2a2cb0391a33f8b436b0e7d798f2d
Marta Kostova <mkostova@camplight.net>
2013-02-07 14:50:58 +0200

    [factories] fix admission factory

--- admissions/spec/factories/admission_factory.rb
+++ admissions/spec/factories/admission_factory.rb
@@ -2,13 +2,14 @@ FactoryGirl.define do
   factory :admission, :class => Gaku::Admission do
     
     after_create do |admission|
+      admission.admission_period_id = FactoryGirl.create(:admission_period).id
       @admission_phase_record = FactoryGirl.create(:admission_phase_record, 
                                                     admission_id: admission.id, 
                                                     admission_phase_id: admission.admission_period.admission_methods.first.admission_phases.first.id,
                                                     admission_phase_state_id: admission.admission_period.admission_methods.first.admission_phases.first.admission_phase_states.first.id)
       
       admission.admission_method_id = admission.admission_period.admission_methods.first.id
-      #admission.admission_method.admission_period_id = admission.admission_period.id
+      admission.admission_method.admission_period_id = admission.admission_period.id
       admission.admission_phase_records = [@admission_phase_record]
       admission.admission_phase_record_id = @admission_phase_record.id
       admission.save!
