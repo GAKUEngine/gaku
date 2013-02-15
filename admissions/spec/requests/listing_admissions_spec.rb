ad1ed8d6397b6b9b0f5895fbbb894580cfffbbd9
Marta Kostova <mkostova@camplight.net>
2013-02-15 16:14:07 +0200

    [request specs] add specs for soft delete of admission from admission list

--- admissions/spec/requests/listing_admissions_spec.rb
+++ admissions/spec/requests/listing_admissions_spec.rb
@@ -45,42 +45,101 @@ describe 'Admin Listing Admissions' do
       page.should have_content "#{student.name}"
     end
 
-    context 'deleting' do
+    describe 'deleting admission' do
 
-      it 'deletes admission from index table' do
-        click delete_link
-        accept_alert
-        page.should_not have_content "#{student.name}"
-        visit gaku.listing_admissions_admin_admissions_path
-        page.should_not have_content "#{student.name}"
-        visit gaku.students_admin_disposals_path
-        page.should_not have_content "#{student.name}"
-        visit gaku.admin_admissions_path
-        click_on 'new-create-multiple-admissions-student-link'
-        wait_for_ajax
-        page.should have_content "#{student.name}"
+      context 'soft deleting from show' do
+      
+        before do
+          click '.show-link'
+          current_path.should eq "/admin/students/1"
+          page.should have_content "#{student.name}"
+          click '#delete-student-link'
+          within(modal) { click_on "Delete" }
+          accept_alert
+          student.reload
+        end
+
+        it 'deletes student from show view' do
+          page.should have_content "successfully"
+          current_path.should == "/admin/admissions/listing_admissions"
+          page.should_not have_content "#{student.name}"
+          visit gaku.listing_admissions_admin_admissions_path
+          page.should_not have_content "#{student.name}"
+        end
+
+        it 'shows the student in student disposals' do
+          visit gaku.students_admin_disposals_path
+          page.should have_content "#{student.name}"
+        end
+
+        it 'deletes student from students index' do
+          visit gaku.students_path
+          page.should_not have_content "#{student.name}"
+        end
+
+        xit 'shows the deleted admission' do
+          visit gaku.students_admin_disposals_path
+          page.should have_content "#{student.name}"
+          click '.show-link'
+          current_path.should == ""
+        end
+
+        xit 'revert deleting' do
+          visit gaku.students_admin_disposals_path
+          page.should have_content "#{student.name}"
+          click '.recovery-link'
+          visit gaku.listing_admissions_admin_admissions_path
+          page.should have_content "#{student.name}"
+        end
+
+        it 'deletes forever' do
+          visit gaku.students_admin_disposals_path
+          page.should have_content "#{student.name}"
+          click delete_link
+          accept_alert
+          page.should_not have_content "#{student.name}"
+          student.reload
+          visit gaku.students_admin_disposals_path
+          page.should_not have_content "#{student.name}"
+          visit gaku.listing_admissions_admin_admissions_path
+          page.should_not have_content "#{student.name}"
+          visit gaku.students_path
+          page.should_not have_content "#{student.name}"
+        end
+
       end
 
-      it 'deletes student from show view' do
-        click '.show-link'
-        current_path.should eq "/admin/students/1"
-        page.should have_content "#{student.name}"
-        click '#delete-student-link'
-        within(modal) { click_on "Delete" }
-        accept_alert
-        wait_for_ajax
-        student.reload
-        page.should have_content "successfully"
-        current_path.should == "/admin/admissions/listing_admissions"
-        page.should_not have_content "#{student.name}"
-        visit gaku.listing_admissions_admin_admissions_path
-        page.should_not have_content "#{student.name}"
-        visit gaku.students_path
-        page.should_not have_content "#{student.name}"
-        visit gaku.students_admin_disposals_path
-        page.should have_content "#{student.name}"
-      end
+      context 'soft deleting from index' do
+        
+        before do
+          click delete_link
+          accept_alert
+        end
 
+        it 'deletes admission from index table' do
+          page.should_not have_content "#{student.name}"
+          visit gaku.listing_admissions_admin_admissions_path
+          page.should_not have_content "#{student.name}"
+        end
+
+        it 'shows student in existing students list' do
+          visit gaku.admin_admissions_path
+          click_on 'new-create-multiple-admissions-student-link'
+          wait_for_ajax
+          page.should have_content "#{student.name}"
+        end
+
+        it 'doesn\'t delete the student' do
+          visit gaku.students_admin_disposals_path
+          page.should_not have_content "#{student.name}"
+        end
+
+        it 'still shows the student in applicants list' do
+          visit gaku.listing_applicants_admin_admissions_path
+          page.should have_content "#{student.name}"
+        end
+
+      end
     end
     
     it 'goes to admissions' do
