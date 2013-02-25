7994f7a1c009c7d2deb1b0bd7687c9f6337b04d9
Marta Kostova <mkostova@camplight.net>
2013-02-26 00:21:14 +0200

    [request specs] use shared examples for addressable resources

--- core/spec/requests/students/addresses_spec.rb
+++ core/spec/requests/students/addresses_spec.rb
@@ -17,11 +17,9 @@ describe 'Student Address' do
     before do
       country
       visit gaku.student_path(student)
-
-      it_behaves_like 'new address'
-
-      it { has_validations? }
     end
+
+    it_behaves_like 'new address'
 
   end
 
@@ -36,89 +34,37 @@ describe 'Student Address' do
         wait_until { page.has_content? 'Addresses list' }
       end
 
-      context 'edit', :js => true do
-        before do
-          click edit_link
-          wait_until_visible modal
-        end
+      it_behaves_like 'edit address'
 
+      context 'delete' do
         
-
-        it 'errors without required fields' do
-          fill_in 'address_address1',  :with => ''
-          fill_in 'address_city',      :with => ''
-
-          has_validations?
+        before do
+          @data = @student
+          within(tab_link)  { page.should have_content 'Addresses(1)' }
         end
 
-        it_behaves_like 'edit address'
+        it_behaves_like 'delete address'
 
-      end
+        after do
+          within(tab_link)  { page.should_not have_content 'Addresses(1)' }
+        end
 
-      it 'deletes', :js => true do
-        address_field = @student.addresses.first.address1
-
-        within(tab_link)  { page.should have_content 'Addresses(1)' }
-        within(count_div) { page.should have_content 'Addresses list(1)' }
-        page.should have_content address_field
-
-        expect do
-          ensure_delete_is_working
-        end.to change(@student.addresses, :count).by -1
-
-        within(count_div) { page.should_not have_content 'Addresses list(1)' }
-        within(tab_link)  { page.should_not have_content 'Addresses(1)' }
-        page.should_not have_content address_field
-        flash_destroyed?
       end
     end
 
     context 'two addresses' do
 
       before(:each) do
-        @student =  create(:student_with_two_addresses)
+        @student = create(:student_with_two_addresses)
         @student.reload
+        @data = @student
         visit gaku.student_path(@student)
         click tab_link
         wait_until { page.has_content? 'Addresses list' }
-
       end
 
-      it "delete primary", :js => true do
-        address1_tr = "#address-#{@student.addresses.first.id}"
-        address2_tr = "#address-#{@student.addresses.second.id}"
+      it_behaves_like 'primary addresses'
 
-        visit gaku.student_path(@student)
-
-        click tab_link
-
-        within("#{table} #{address2_tr}") { click_link 'set_primary_link' }
-        accept_alert
-
-        page.find("#{address2_tr} .primary_address a.btn-primary")
-
-        within("#{table} #{address2_tr}") { click '.delete-link'}
-        accept_alert
-
-        page.find("#{address1_tr} .primary_address a.btn-primary")
-
-        @student.addresses.first.primary? == true
-      end
-
-      it 'sets primary', :js => true do
-        @student.addresses.first.primary? == true
-        @student.addresses.second.primary? == false
-
-        visit gaku.student_path(@student)
-        click tab_link
-
-        #sleep 10
-        within("#{table} tr#address-#{@student.addresses.second.id}") { click_link 'set_primary_link' }
-        accept_alert
-
-        @student.addresses.first.primary? == false
-        @student.addresses.second.primary? == true
-      end
     end
   end
 end
--- core/spec/support/requests/addressable_spec.rb
+++ core/spec/support/requests/addressable_spec.rb
@@ -26,6 +26,11 @@ shared_examples 'new address' do
       ensure_cancel_creating_is_working
     end
 
+    it 'has validations', js:true do
+      click submit
+      has_validations?
+    end
+
   end
 
 end
@@ -51,15 +56,61 @@ shared_examples_for 'edit address' do
     ensure_cancel_modal_is_working
   end
 
+  it 'errors without required fields', js:true do
+    fill_in 'address_address1',  :with => ''
+    fill_in 'address_city',      :with => ''
+
+    has_validations?
+  end
+
 end
 
 shared_examples_for 'delete address' do
 
-  it "deletes", js:true do
-    ensure_delete_is_working
-    wait_until_visible new_link
+  it "deletes", :js => true do
+    address_field = @data.addresses.first.address1
 
+    within(count_div) { page.should have_content 'Addresses list(1)' }
+    page.should have_content address_field
+
+    expect do
+      ensure_delete_is_working
+    end.to change(@data.addresses, :count).by -1
+
+    within(count_div) { page.should_not have_content 'Addresses list(1)' }
+    page.should_not have_content address_field
     flash_destroyed?
   end
 
+end
+
+shared_examples_for 'primary addresses' do
+
+  it "sets primary", :js => true do
+    @data.addresses.first.primary? == true
+    @data.addresses.second.primary? == false
+
+    within("#{table} tr#address-#{@data.addresses.second.id}") { click_link 'set_primary_link' }
+    accept_alert
+
+    @data.addresses.first.primary? == false
+    @data.addresses.second.primary? == true
+  end
+
+  it "delete primary", :js => true do
+    address1_tr = "#address-#{@data.addresses.first.id}"
+    address2_tr = "#address-#{@data.addresses.second.id}"
+
+    within("#{table} #{address2_tr}") { click_link 'set_primary_link' }
+    accept_alert
+
+    page.find("#{address2_tr} .primary_address a.btn-primary")
+
+    within("#{table} #{address2_tr}") { click '.delete-link'}
+    accept_alert
+
+    page.find("#{address1_tr} .primary_address a.btn-primary")
+
+    @data.addresses.first.primary? == true
+  end
 end
