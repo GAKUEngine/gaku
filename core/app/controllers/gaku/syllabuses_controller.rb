1f65baf445501e8bdb70c946398c747261d660c0
Vassil Kalkov <vassilkalkov@gmail.com>
2013-02-25 17:51:05 +0200

    Clean syllabuses controller

--- core/app/controllers/gaku/syllabuses_controller.rb
+++ core/app/controllers/gaku/syllabuses_controller.rb
@@ -3,51 +3,36 @@ module Gaku
 
     load_and_authorize_resource :class =>  Gaku::Syllabus
 
-    #before_filter :load_before_index, :only => :index
-    #before_filter :load_before_show,  :only => [:show, :destroy]
-    before_filter :load_before_show,  :only => :show
+    before_filter :before_show,  :only => :show
     before_filter :count, :only => [:create, :destroy, :index]
 
     inherit_resources
-    actions :index, :show, :new, :create, :update, :edit, :destroy
-
     respond_to :js, :html
-
-    #def destroy
-    #  super do |format|
-    #    format.js { render }
-    #  end
-    #end
 
     private
 
-      #def load_before_index
-      #  @syllabus = Syllabus.new
-      #end
+    def syllabus
+    	@syllabus = Syllabus.includes(includes).find(params[:id])
+    end
 
-      def syllabus
-      	@syllabus = Syllabus.find(params[:id])
-      end
+    def includes
+      [{:exam_syllabuses => [:exam, :syllabus]}]
+    end
 
-      def grading_methods
-        @grading_methods = GradingMethod.all
-      end
+    def grading_methods
+      @grading_methods = GradingMethod.all
+    end
 
-      def load_before_show
-        syllabus
+    def before_show
+      syllabus
+      @notable = @syllabus
+      @notable_resource = @notable.class.to_s.underscore.split('/')[1].gsub("_","-")
+      grading_methods
+    end
 
-        #@exam = Exam.new
-        #@exam_syllabus = ExamSyllabus.new
-        #@exam.exam_portions.build
-        #@syllabus.assignments.build
-        @notable = @syllabus
-        @notable_resource = @notable.class.to_s.underscore.split('/')[1].gsub("_","-")
-        grading_methods
-      end
-
-      def count
-        @count = Syllabus.count
-      end
+    def count
+      @count = Syllabus.count
+    end
 
   end
 end
