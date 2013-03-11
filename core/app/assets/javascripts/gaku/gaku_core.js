165bf8be456a81dac4a8110346d98f0bfca6c72d
Georgi Tapalilov <tapalilov@gmail.com>
2013-03-11 15:05:20 +0200

    make weight widget inputs for exam portions work

--- core/app/assets/javascripts/gaku/gaku_core.js
+++ core/app/assets/javascripts/gaku/gaku_core.js
@@ -20,5 +20,6 @@
 //= require gaku/students
 //= require gaku/students/addresses/form
 //= require gaku/shared/address
+//= require gaku/exams
 //= require_tree ./backbone/
 //= require ./admin/disposals
