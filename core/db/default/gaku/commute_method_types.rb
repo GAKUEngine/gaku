4de2e694f987590d8274ba87a1ec56dbc28d8136
Radoslav Georgiev <radoslavgeorgiev.se@gmail.com>
2013-03-12 18:09:49 +0200

    [models][defaults][migrations] make commute_method_type globalized

--- core/db/default/gaku/commute_method_types.rb
+++ core/db/default/gaku/commute_method_types.rb
@@ -1,7 +1,29 @@
-# encoding: utf-8
+# -*- encoding: utf-8 -*-
 
-names = %w(Unknown Walking Bicycle Train Bus Car Metro)
+#names = %w(Unknown Walking Bicycle Train Bus Car Metro)
 
-names.each do |name|
-	Gaku::CommuteMethodType.where(:name => name).first_or_create!
+#names.each do |name|
+#	Gaku::CommuteMethodType.where(:name => name).first_or_create!
+#end
+
+# -*- encoding: utf-8 -*-
+# Array format ['en commute_method_type', 'ja commute_method_type', 'is_active?']
+commute_method_types = [
+  ['Unknown',  '不明'],
+  ['Walking',  '歩行'],
+  ['Bicycle',  '自転車'],
+  ['Train',    '列車'],
+  ['Bus',      'バス'],
+  ['Car',      '車'],
+  ['Metro',    '地下鉄']
+]
+
+commute_method_types.each do |type|
+  I18n.locale = :en
+  commute_method_type = Gaku::CommuteMethodType.where(:name => type[0]).first_or_create!
+
+  I18n.locale = :ja
+  commute_method_type.update_attribute(:name,  type[1])
 end
+
+I18n.locale = nil
