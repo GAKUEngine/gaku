8466f64fdf3b283b5b3cae8d540611010584583b
Marta Kostova <mkostova@camplight.net>
2013-03-18 23:26:00 +0200

    [specs] add specs for attachment soft delete and some refactoring

--- core/spec/requests/exams/exam_portions/attachments_spec.rb
+++ core/spec/requests/exams/exam_portions/attachments_spec.rb
@@ -12,9 +12,9 @@ describe 'Exam Portion Attachments' do
     set_resource 'exam-exam-portion-attachment'
   end
 
-  context '#new', :js => true do
+  context '#new', js: true do
     before do
-      exam;exam_portion
+      exam; exam_portion
       visit gaku.exam_exam_portion_path(exam, exam_portion)
       click new_link
       wait_until_visible submit
@@ -22,8 +22,8 @@ describe 'Exam Portion Attachments' do
 
     it 'creates and show' do
       expect do
-        fill_in 'attachment_name', :with => 'Attachment name'
-        fill_in 'attachment_description', :with => 'Attachment description'
+        fill_in 'attachment_name', with: 'Attachment name'
+        fill_in 'attachment_description', with: 'Attachment description'
 
         absolute_path = Rails.root + "../support/120x120.jpg"
         attach_file 'attachment_asset', absolute_path
@@ -41,18 +41,19 @@ describe 'Exam Portion Attachments' do
 
     it {has_validations?}
 
-    it 'cancels creating', :cancel => true do
+    it 'cancels creating' do
       ensure_cancel_creating_is_working
     end
   end
 
-  context 'existing', :js => true do
+  context 'when exists', js: true do
+    
     before do
       exam; exam_portion; attachment
       visit gaku.exam_exam_portion_path(exam, exam_portion)
     end
 
-    it 'deletes' do
+    it 'deletes from index table' do
       within(count_div) { page.should have_content 'Attachments list(1)' }
 
       expect do
@@ -60,9 +61,54 @@ describe 'Exam Portion Attachments' do
       end.to change(Gaku::Attachment, :count).by -1
 
       within(count_div) { page.should have_content 'Attachments list' }
-      page.should_not have_content('120x120.jpg')
+      page.should_not have_content("#{attachment.name}")
       flash_destroyed?
     end
+
+    context 'when deleted' do
+
+      before do
+        within(table) do
+          click delete_link
+          accept_alert
+        end
+        flash_destroyed?
+      end
+
+      it 'shows the attachment in attachment disposals' do
+        visit gaku.attachments_admin_disposals_path
+        page.should have_content "#{attachment.name}"
+      end
+
+      it 'deletes attachment from index table' do
+        visit gaku.exam_exam_portion_path(exam, exam_portion)
+        page.should_not have_content "#{attachment.name}"
+      end
+
+      it 'revert deleting' do
+        visit gaku.attachments_admin_disposals_path
+        page.should have_content "#{attachment.name}"
+        click '.recovery-link'
+        flash_recovered?
+        visit gaku.exam_exam_portion_path(exam, exam_portion)
+        page.should have_content "#{attachment.name}"
+      end
+
+      it 'deletes forever' do
+        visit gaku.attachments_admin_disposals_path
+        page.should have_content "#{attachment.name}"
+        click delete_link
+        accept_alert
+        flash_destroyed?
+        page.should_not have_content "#{attachment.name}"
+        
+        visit gaku.attachments_admin_disposals_path
+        page.should_not have_content "#{attachment.name}"
+        visit gaku.exam_exam_portion_path(exam, exam_portion)
+        page.should_not have_content "#{attachment.name}"
+      end
+    end
+
   end
 
 end
