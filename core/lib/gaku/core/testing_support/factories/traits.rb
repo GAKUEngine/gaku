ed111ec786df2e3167ec8e387a5492532c9dcc86
Georgi Tapalilov <tapalilov@gmail.com>
2013-03-28 13:03:31 +0200

    [traits] refactoring

--- core/lib/gaku/core/testing_support/factories/traits.rb
+++ core/lib/gaku/core/testing_support/factories/traits.rb
@@ -3,44 +3,43 @@ FactoryGirl.define do
   trait :with_note do
     after_create do |resource|
       FactoryGirl.create(:note, :notable => resource)
+      resource.notes.reload
     end
   end
 
   trait :with_notes do
     after_create do |resource|
-      FactoryGirl.create(:note, :notable => resource)
-      FactoryGirl.create(:note, :notable => resource)
+      FactoryGirl.create_list :note, 2, :notable => resource
+      resource.notes.reload
     end
   end
 
 
   trait :with_address do
     after_create do |resource|
-      resource.addresses << FactoryGirl.create(:address, :addressable => resource)
-      resource.save
+      FactoryGirl.create(:address, :addressable => resource)
+      resource.addresses.reload
     end
   end
 
   trait :with_addresses do
     after_create do |resource|
-      resource.addresses << FactoryGirl.create(:address, :addressable => resource)
-      resource.addresses << FactoryGirl.create(:address, :addressable => resource)
-      resource.save
+      FactoryGirl.create_list :address, 2, :addressable => resource
+      resource.addresses.reload
     end
   end
 
   trait :with_contact do
     after_create do |resource|
-      resource.contacts << FactoryGirl.create(:contact, :contactable => resource)
-      resource.save
+      FactoryGirl.create(:contact, :contactable => resource)
+      resource.contacts.reload
     end
   end
 
   trait :with_contacts do
     after_create do |resource|
-      resource.contacts << FactoryGirl.create(:contact, :contactable => resource)
-      resource.contacts << FactoryGirl.create(:contact, :contactable => resource)
-      resource.save
+      FactoryGirl.create_list :contact, 2, :contactable => resource
+      resource.contacts.reload
     end
   end
 
