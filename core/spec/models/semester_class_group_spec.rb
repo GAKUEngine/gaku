beac0c759a9711fd4b161669ca34078c92756f3c
Georgi Tapalilov <tapalilov@gmail.com>
2013-04-12 12:47:38 +0300

    [model/spec] testing uniqness scope validation for SemesterClassGroup

--- core/spec/models/semester_class_group_spec.rb
+++ core/spec/models/semester_class_group_spec.rb
@@ -11,6 +11,9 @@ module Gaku
       it { should validate_presence_of :class_group_id }
 
       it { should allow_mass_assignment_of :semester_id }
+
+      it { should validate_uniqueness_of(:semester_id).scoped_to(:class_group_id).with_message(/Semester already added to Class Group/) }
+
     end
   end
 end
