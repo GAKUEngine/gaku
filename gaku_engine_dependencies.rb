512d802a160a5a4708187e3855520f3348f5b2e1
Vassil Kalkov <vassilkalkov@gmail.com>
2013-04-16 16:18:49 +0300

    [gem] Add rubocop, improve gaku_engine_dependencies file

--- gaku_engine_dependencies.rb
+++ gaku_engine_dependencies.rb
@@ -5,8 +5,7 @@ gem 'mysql2'
 gem 'pg'
 gem 'sqlite3'
 
-gem 'execjs'
-gem 'therubyracer', '0.10.2'
+gem 'therubyracer', :require => 'v8'
 
 gem 'rails-i18n'
 
@@ -17,18 +16,23 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :development do
+  gem 'rubocop'
+end
+
 group :test do
+  gem 'capybara', '= 1.1.3'
+  gem 'selenium-webdriver'
+  gem 'poltergeist'
   gem 'spork', '~> 1.0rc'
+
   gem 'rspec-rails'
   gem 'factory_girl_rails', '~> 3.2.0'
   gem 'ffaker'
   gem 'shoulda-matchers'
-  gem 'capybara', '= 1.1.3'
-  gem 'selenium-webdriver'
+
   gem 'database_cleaner'
   gem 'launchy'
-
-  gem 'poltergeist'
 
   gem 'guard'
   gem 'guard-rspec'
