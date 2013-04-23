b4dbeb100510720baa33176f9ab6d1e7f2ff0447
Georgi Tapalilov <tapalilov@gmail.com>
2013-04-23 16:39:37 +0300

    [spec/req] testing class group index refacotring by semesters

--- core/spec/requests/class_groups/class_groups_spec.rb
+++ core/spec/requests/class_groups/class_groups_spec.rb
@@ -4,65 +4,78 @@ describe 'ClassGroups' do
 
   as_admin
 
-  let(:class_group){ create(:class_group, :grade => '1', :name => "Not so awesome class group", :homeroom => 'A1') }
-
-  before :all do
-    set_resource("class-group")
+  let(:class_group) do
+    create(:class_group, grade: '1',
+                         name: 'Not so awesome class group',
+                         homeroom: 'A1')
   end
 
-  context 'new', :js => true do
+  let(:class_group_with_semesters) { create(:class_group, :with_semesters) }
+
+  before :all do
+    set_resource('class-group')
+  end
+
+  context 'new', js: true do
     before do
       visit gaku.class_groups_path
-      
       click new_link
       wait_until_visible submit
     end
 
-    it 'creates and shows', :js => true do
+    it 'creates and shows', js: true do
       expect do
-        fill_in 'class_group_grade',    :with => '7'
-        fill_in 'class_group_name',     :with => 'Awesome class group'
-        fill_in 'class_group_homeroom', :with => 'room#7'
+        fill_in 'class_group_grade',    with: '7'
+        fill_in 'class_group_name',     with: 'Awesome class group'
+        fill_in 'class_group_homeroom', with: 'room#7'
         click submit
         wait_until_invisible form
       end.to change(Gaku::ClassGroup, :count).by 1
 
-      page.should have_content '7'
-      page.should have_content 'Awesome class group'
-      page.should have_content 'room#7'
+      within('#class-groups-without-semester-index') do
+        page.should have_content '7'
+        page.should have_content 'Awesome class group'
+        page.should have_content 'room#7'
+      end
+
       within(count_div) { page.should have_content 'Class Groups list(1)' }
       flash_created?
     end
 
-    it {has_validations?}
+    it { has_validations? }
 
-    it 'cancels creating', :cancel => true do
+    it 'cancels creating', cancel: true do
       ensure_cancel_creating_is_working
     end
   end
 
   context 'existing' do
     before do
-      class_group
+      class_group_with_semesters
       visit gaku.class_groups_path
     end
 
-    context 'edit', :js => true do
+    context 'edit', js: true do
       before do
         click edit_link
         wait_until_visible modal
       end
 
       it 'edits' do
-        fill_in 'class_group_grade',    :with => '2'
-        fill_in 'class_group_name',     :with => 'Really awesome class group'
-        fill_in 'class_group_homeroom', :with => 'B2'
+        semester, semester2 = class_group_with_semesters.semesters
+        fill_in 'class_group_grade',    with: '2'
+        fill_in 'class_group_name',     with: 'Really awesome class group'
+        fill_in 'class_group_homeroom', with: 'B2'
 
         click submit
 
-        page.should have_content 'Really awesome class group'
-        page.should have_content "2"
-        page.should have_content "B2"
+        %W( #{semester.id} #{semester2.id} ).each do |id|
+          within("#semester-#{id}-class-groups-index") do
+            page.should have_content 'Really awesome class group'
+            page.should have_content '2'
+            page.should have_content 'B2'
+          end
+        end
 
         page.should_not have_content 'Not so awesome class group'
         page.should_not have_content 'A1'
@@ -74,24 +87,24 @@ describe 'ClassGroups' do
         flash_updated?
       end
 
-      it 'cancels editting', :cancel => true do
+      it 'cancels editting', cancel: true do
         ensure_cancel_modal_is_working
       end
 
       it 'edits from show view' do
-        visit gaku.class_group_path(class_group)
+        visit gaku.class_group_path(class_group_with_semesters)
         click edit_link
         wait_until_visible modal
 
-        fill_in 'class_group_grade',    :with => '2'
-        fill_in 'class_group_name',     :with => 'Really awesome class group'
-        fill_in 'class_group_homeroom', :with => 'B2'
+        fill_in 'class_group_grade',    with: '2'
+        fill_in 'class_group_name',     with: 'Really awesome class group'
+        fill_in 'class_group_homeroom', with: 'B2'
 
         click submit
 
         page.should have_content 'Really awesome class group'
-        page.should have_content "2"
-        page.should have_content "B2"
+        page.should have_content '2'
+        page.should have_content 'B2'
 
         page.should_not have_content 'Not so awesome class group'
         page.should_not have_content 'A1'
@@ -109,28 +122,49 @@ describe 'ClassGroups' do
       end
     end
 
-    it 'deletes', :js => true do
-      page.should have_content class_group.name
+    it 'deletes', js: true do
+      semester, semester2 = class_group_with_semesters.semesters
+
+      %W( #{semester.id} #{semester2.id} ).each do |id|
+        within("#semester-#{id}-class-groups-index") do
+          page.should have_content class_group_with_semesters.name
+        end
+      end
+
       within(count_div) { page.should have_content 'Class Groups list(1)' }
 
       expect do
-        ensure_delete_is_working
-      end.to change(Gaku::ClassGroup,:count).by -1
+        rows = "#semester-#{semester.id}-class-groups-index tr"
+        tr_count = size_of rows
 
-      page.should_not have_content class_group.name
+        within("#semester-#{semester.id}-class-groups-index") do
+          click delete_link
+        end
+
+        accept_alert
+        wait_until { size_of(rows) == tr_count - 1 }
+
+      end.to change(Gaku::ClassGroup, :count).by(-1)
+
+      %W( #{semester.id} #{semester2.id} ).each do |id|
+        within("#semester-#{id}-class-groups-index") do
+          page.should_not have_content class_group_with_semesters.name
+        end
+      end
+
       within(count_div) { page.should_not have_content 'Class Groups list(1)' }
       flash_destroyed?
     end
 
     it 'returns to class groups index when back is selected' do
-      visit gaku.class_group_path(class_group)
+      visit gaku.class_group_path(class_group_with_semesters)
       click_link('back-class-group-link')
-      page.should have_content ('Class Groups list')
+      page.should have_content('Class Groups list')
     end
 
     it 'redirects to show view when show btn selected' do
       within(table) { click show_link }
-      page.should have_content ('Show')
+      page.should have_content('Show')
     end
 
   end
