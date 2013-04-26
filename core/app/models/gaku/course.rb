b2b169d3e08b08d0fc695488ab5edf9d21aef12b
Georgi Tapalilov <tapalilov@gmail.com>
2013-04-26 15:25:54 +0300

    [model] scope courses without semester and class method for grouping courses by semester id

--- core/app/models/gaku/course.rb
+++ core/app/models/gaku/course.rb
@@ -3,30 +3,33 @@ module Gaku
 
     include Notes
 
-    has_many :enrollments, class_name: "Gaku::CourseEnrollment", :dependent => :destroy
-    has_many :students, :through => :enrollments
+    has_many :enrollments, class_name: "Gaku::CourseEnrollment", dependent: :destroy
+    has_many :students, through: :enrollments
 
     has_many :course_group_enrollments
-    has_many :course_groups, :through => :course_group_enrollments
+    has_many :course_groups, through: :course_group_enrollments
 
-    has_many :class_groups, :through => :class_group_course_enrollments
-    has_many :class_group_course_enrollments, :dependent => :destroy
+    has_many :class_groups, through: :class_group_course_enrollments
+    has_many :class_group_course_enrollments, dependent: :destroy
 
-    has_many :semester_courses
-    has_many :semesters, :through => :semester_courses
+    has_many :semester_courses, dependent: :destroy
+    has_many :semesters, through: :semester_courses
 
     has_many :exam_schedules
 
     belongs_to :syllabus
     belongs_to :class_group
 
-    delegate :name, :code, :to => :syllabus, :prefix => true, :allow_nil => true
+    delegate :name, :code, to: :syllabus, prefix: true, allow_nil: true
 
     accepts_nested_attributes_for :enrollments
 
     attr_accessible :code, :class_group_id, :syllabus_id
 
     validates_presence_of :code
+
+    scope :without_semester, -> { includes(:semester_courses).where(gaku_semester_courses: { course_id: nil }) }
+
 
     def enroll_class_group(class_group)
     	unless class_group.blank?
