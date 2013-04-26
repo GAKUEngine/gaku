ab3560e991b0a9c260404b127a3a202b74eff90d
Georgi Tapalilov <tapalilov@gmail.com>
2013-04-26 15:27:41 +0300

    [spec] refactor specs to match  grouping for courses

--- core/spec/requests/courses/courses_spec.rb
+++ core/spec/requests/courses/courses_spec.rb
@@ -4,15 +4,21 @@ describe 'Courses' do
 
   as_admin
 
-  let(:syllabus) { create(:syllabus, :name => 'biology2012', :code => 'bio') }
-  let(:syllabus2) { create(:syllabus, :name => 'biology2013Syllabus', :code => 'biology') }
-  let(:course) { create(:course, :syllabus => syllabus) }
-
-  before :all do
-    set_resource "course"
+  let(:syllabus) { create(:syllabus, name: 'biology2012', code: 'bio') }
+  let(:syllabus2) do
+    create(:syllabus, name: 'biology2013Syllabus', code: 'biology')
+  end
+  let(:course) { create(:course, syllabus: syllabus) }
+  let(:course_with_semesters) do
+    create(:course, :with_semesters, syllabus: syllabus)
   end
 
-  context '#new', :js => true do
+
+  before :all do
+    set_resource 'course'
+  end
+
+  context '#new', js: true do
     before do
       syllabus
       visit gaku.courses_path
@@ -20,38 +26,41 @@ describe 'Courses' do
       wait_until_visible submit
     end
 
-    it "creates new course" do
+    it 'creates new course' do
       expect do
-        fill_in 'course_code', :with => 'SUMMER2012'
-        select "#{syllabus.name}", :from => 'course_syllabus_id'
+        fill_in 'course_code', with: 'SUMMER2012'
+        select "#{syllabus.name}", from: 'course_syllabus_id'
         click submit
         wait_until_invisible form
-        within(table) { page.should have_content(syllabus.name) }
-      end.to change(Gaku::Course, :count).by 1
+        within('#courses-without-semester-index') do
+          page.should have_content(syllabus.name)
+        end
+      end.to change(Gaku::Course, :count).by(1)
 
       within(count_div) { page.should have_content('Courses list(1)') }
       wait_until_invisible '#new-course'
       flash_created?
     end
 
-    it {has_validations?}
+    it { has_validations? }
 
-    it 'cancels creating', :cancel => true do
+    it 'cancels creating', cancel: true do
       ensure_cancel_creating_is_working
     end
   end
 
-  context "existing course" do
+  context 'existing course' do
     before do
-      course
+      course_with_semesters
+      syllabus
       syllabus2
       visit gaku.courses_path
       within(count_div) { page.should have_content('Courses list(1)') }
     end
 
-    it "lists and shows existing courses" do
-      within(table) do
-        page.should have_content("biology")
+    it 'lists and shows existing courses' do
+      within("#semester-#{course_with_semesters.semesters.first.id}-courses-index") do
+        page.should have_content('biology')
         click show_link
       end
 
@@ -59,8 +68,8 @@ describe 'Courses' do
       page.should have_content('biology')
     end
 
-    context "when select back btn" do
-      it 'returns to index view', js:true do
+    context 'when select back btn' do
+      it 'returns to index view', js: true do
         visit gaku.course_path(course)
         click_on('Back')
         page.current_path.should eq gaku.courses_path
@@ -69,56 +78,89 @@ describe 'Courses' do
 
     context ' #edit ' do
       before do
-        within(table) { click edit_link }
-        page.should have_content("Edit Course")
+        click edit_link
+        page.should have_content('Edit Course')
       end
 
-      it 'has validations', :js => true do
-        fill_in 'course_code', :with => ''
+      it 'has validations', js: true do
+        fill_in 'course_code', with: ''
         has_validations?
       end
 
-      it "edits a course", :js => true  do
-        fill_in 'course_code', :with => 'biology2013'
-        page.select "biology2013Syllabus", :from => 'course_syllabus_id'
+      it 'edits a course', js: true  do
+        semester, semester2 = course_with_semesters.semesters
+
+        fill_in 'course_code', with: 'biology2013'
+        select syllabus2.name, from: 'course_syllabus_id'
+
         click submit
 
-        page.should have_content "biology2013Syllabus"
-        page.should have_content "biology2013"
+        %W( #{semester.id} #{semester2.id} ).each do |id|
+          within("#semester-#{id}-courses-index") do
+            page.should have_content 'biology2013'
+            page.should have_content syllabus2.name
+            page.should_not have_content syllabus.name
+          end
+        end
+
         flash_updated?
       end
     end
 
-    it "edits a course from show", :js => true do
-      within(table) { click show_link }
-      page.should have_content("Show")
+    it 'edits a course from show', js: true do
+      within("#semester-#{course_with_semesters.semesters.first.id}-courses-index") do
+        click show_link
+      end
+
+      page.should have_content('Show')
 
       click edit_link
       wait_until_visible(modal)
 
-      page.should have_content("Edit Course")
-      fill_in 'course_code', :with => 'biology2013'
-      page.select "biology2013Syllabus", :from => 'course_syllabus_id'
+      page.should have_content('Edit Course')
+      fill_in 'course_code', with: 'biology2013'
+      page.select 'biology2013Syllabus', from: 'course_syllabus_id'
       click submit
 
-      page.should have_content "biology2013Syllabus"
-      page.should have_content "biology2013"
+      page.should have_content 'biology2013Syllabus'
+      page.should have_content 'biology2013'
 
       flash_updated?
     end
 
-    it "should delete a course", :js => true do
-      tr_count = page.all(table_rows).size
+    it 'deletes', js: true do
+      semester, semester2 = course_with_semesters.semesters
+
+      %W( #{semester.id} #{semester2.id} ).each do |id|
+        within("#semester-#{id}-courses-index") do
+          page.should have_content course_with_semesters.code
+        end
+      end
+
       within(count_div) { page.should have_content('Courses list(1)') }
-      within(table) { page.should have_content(course.code) }
 
       expect do
-        ensure_delete_is_working
-      end.to change(Gaku::Course, :count).by -1
+        rows = "#semester-#{semester.id}-courses-index tr"
+        tr_count = size_of rows
+
+        within("#semester-#{semester.id}-courses-index") do
+          click delete_link
+        end
+
+        accept_alert
+        wait_until { size_of(rows) == tr_count - 1 }
+
+      end.to change(Gaku::Course, :count).by(-1)
+
+      %W( #{semester.id} #{semester2.id} ).each do |id|
+        within("#semester-#{id}-courses-index") do
+          page.should_not have_content course_with_semesters.code
+        end
+      end
 
       within(count_div) { page.should_not have_content('Courses list(1)') }
-      within(table) { page.should_not have_content(course.code) }
       flash_destroyed?
+
     end
   end
 end
