832735fbeb17fab1c9a4122ccbf0a0612c69f04e
Georgi Tapalilov <tapalilov@gmail.com>
2013-05-02 16:43:19 +0300

    [spec] for grading method set, join models, and adding new attributes for grading_method

--- core/spec/models/grading_method_spec.rb
+++ core/spec/models/grading_method_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe Gaku::GradingMethod do
 
-  context "validations" do 
+  context "validations" do
   	it { should have_one(:exam) }
   	it { should have_one(:exam_portion) }
   	it { should have_one(:assignment) }
+    it { should have_many(:grading_method_set_items) }
+    it { should have_many(:grading_method_sets).through(:grading_method_set_items) }
 
     it { should allow_mass_assignment_of :description }
     it { should allow_mass_assignment_of :method }
     it { should allow_mass_assignment_of :name }
+    it { should allow_mass_assignment_of :curved }
+    it { should allow_mass_assignment_of :arguments }
   end
-  
+
 end
