1ee02ed8c907cc791bd7b7e7d53cf6dbd86242f4
Georgi Tapalilov <tapalilov@gmail.com>
2013-05-03 11:14:09 +0300

    [migration] change default boolean values for grading method set

--- core/db/migrate/20130502101704_create_gaku_grading_method_sets_table.rb
+++ core/db/migrate/20130502101704_create_gaku_grading_method_sets_table.rb
@@ -2,10 +2,10 @@ class CreateGakuGradingMethodSetsTable < ActiveRecord::Migration
   def change
     create_table :gaku_grading_method_sets do |t|
       t.string :name
-      t.boolean :primary, :default => false
-      t.boolean :display_deviation, :default => false
-      t.boolean :display_rank, :default => false
-      t.boolean :rank_order, :default => false
+      t.boolean :primary, default: false
+      t.boolean :display_deviation, default: true
+      t.boolean :display_rank, default: true
+      t.boolean :rank_order, default: true
 
       t.timestamps
     end
