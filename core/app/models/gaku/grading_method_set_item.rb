95e92b0fd8a18caaae53ed54a67a356d1549a307
Georgi Tapalilov <tapalilov@gmail.com>
2013-05-13 11:48:22 +0300

    [validation] add uniquness/scope validation to grading_method_set_item

--- core/app/models/gaku/grading_method_set_item.rb
+++ core/app/models/gaku/grading_method_set_item.rb
@@ -3,5 +3,8 @@ module Gaku
     belongs_to :grading_method
     belongs_to :grading_method_set
     attr_accessible :order, :grading_method_id, :grading_method_set_id
+
+    validates :grading_method_id, presence: true, uniqueness: { scope: :grading_method_set_id, message: I18n.t(:'grading_method_set_item.already') }
+
   end
 end
