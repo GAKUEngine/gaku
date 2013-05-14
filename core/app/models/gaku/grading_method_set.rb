20adc490a406a4cfe5aa6ea130fd4a9805a7dbda
Georgi Tapalilov <tapalilov@gmail.com>
2013-05-14 11:19:26 +0300

    [model] grading method set assoc and instance method

--- core/app/models/gaku/grading_method_set.rb
+++ core/app/models/gaku/grading_method_set.rb
@@ -2,12 +2,13 @@ module Gaku
   class GradingMethodSet < ActiveRecord::Base
     attr_accessible :display_deviation, :display_rank, :name, :is_primary, :rank_order
 
-    has_many :grading_method_set_items
+    has_many :grading_method_set_items, order: :position
     has_many :grading_methods, through: :grading_method_set_items
 
     validates :name, presence:true, uniqueness: true
 
     before_save :ensure_first_is_primary, on: :create
+
 
     def make_primary
       GradingMethodSet.update_all( { is_primary: false }, ['id != ?', id] )
@@ -17,6 +18,10 @@ module Gaku
 
     def ensure_first_is_primary
       self.is_primary = true if Gaku::GradingMethodSet.all.empty?
+    end
+
+    def update_items_positions(id, index)
+      self.grading_method_set_items.update_all( { position: index }, { id: id } )
     end
 
   end
