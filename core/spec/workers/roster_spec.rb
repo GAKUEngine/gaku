f7c04bbf5b74516f21967a8403c24fd06182303a
Georgi Tapalilov <tapalilov@gmail.com>
2013-06-12 15:20:27 +0300

    [spec] testing sidekiq job

--- core/spec/workers/roster_spec.rb
+++ core/spec/workers/roster_spec.rb
@@ -4,8 +4,12 @@ describe Gaku::Core::Importers::Students::RosterWorker do
 
 it { should be_retryable true }
 
-it 'opa' do
-  expect( subject.perform('opa') ).to have_enqueued_jobs(1)
+it 'add new job to worker' do
+  file_f = create(:import_file)
+  worker = Gaku::Core::Importers::Students::RosterWorker
+  expect do
+    worker.perform_async(file_f.id)
+  end.to change(worker.jobs, :size).by(1)
 end
 
 end
