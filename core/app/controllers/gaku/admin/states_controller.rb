b055d57e3db0b9cfc0f87e78cb33671500a914fa
Georgi Tapalilov <tapalilov@gmail.com>
2013-06-25 13:38:31 +0300

    [controller] admin states

---
+++ core/app/controllers/gaku/admin/states_controller.rb
@@ -0,0 +1,37 @@
+module Gaku
+  module Admin
+    class StatesController < Admin::BaseController
+
+      load_and_authorize_resource class: Gaku::State
+
+      inherit_resources
+      respond_to :js, :html
+
+      before_filter :count, only: [:create, :destroy, :index]
+      before_filter :load_country_preset, only: [:index]
+      before_filter :load_data
+
+      def country_states
+        @country = Country.where(:iso => params[:state][:country_iso]).first
+        @states = @country.states
+        respond_with @states
+      end
+
+      private
+
+      def load_data
+        @countries = Gaku::Country.all.collect { |c| [c, c.iso ]}
+      end
+
+      def load_country_preset
+        @country_preset ||= Gaku::Preset.get('address_country')
+        @default_country = Gaku::Country.where(iso: @country_preset).first
+      end
+
+      def count
+        @count = State.count
+      end
+
+    end
+  end
+end
