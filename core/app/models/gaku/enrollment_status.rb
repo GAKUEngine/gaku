1f679b7fbef7a25848c59cdbcd4b3585cfddc243
Georgi Tapalilov <tapalilov@gmail.com>
2013-07-10 14:08:09 +0300

    [model] enrollment status and student assoc, scope for find active enrollment statuses

--- core/app/models/gaku/enrollment_status.rb
+++ core/app/models/gaku/enrollment_status.rb
@@ -1,7 +1,7 @@
 module Gaku
   class EnrollmentStatus < ActiveRecord::Base
 
-    has_many :students
+    has_many :students, foreign_key: :enrollment_status_code, primary_key: :code
 
     translates :name
 
@@ -10,6 +10,8 @@ module Gaku
     validates :code, presence: true
 
     before_create :set_name
+
+    scope :active,  -> { where(is_active: true) }
 
     def set_name
       self.name = code if name.nil?
