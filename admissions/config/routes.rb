5c2405a45075b7dfbbac80a9d52cbfe33f1730c6
Vassil Kalkov <vassilkalkov@gmail.com>
2013-07-12 13:35:13 +0300

    [admissions] Beautify routes

--- admissions/config/routes.rb
+++ admissions/config/routes.rb
@@ -1,5 +1,6 @@
 Gaku::Core::Engine.routes.prepend do
 	namespace :admin do
+
 		resources :admissions do
       collection do
         get :change_admission_period
@@ -11,21 +12,20 @@ Gaku::Core::Engine.routes.prepend do
         post :admit_students
         get :listing_admissions
         get :listing_applicants
-        resources :importer, :controller => "admissions/importer" do
-          collection do
-            post :import_sheet
-          end
+
+        resources :importer, controller: 'admissions/importer' do
+          post :import_sheet, on: :collection
         end
       end
-      member do
-        delete :soft_delete
-      end
+
+      delete :soft_delete, on: :member
+
       get :new_applicant
     end
 
     resources :admission_phases do
       resources :exams do
-        get :grading, :on => :member
+        get :grading, on: :member
         resources :exam_portion_scores do
           resources :attendances
         end
@@ -33,29 +33,31 @@ Gaku::Core::Engine.routes.prepend do
     end
 
     resources :admission_methods do
-      resources :admission_phases, :controller => 'admission_methods/admission_phases' do
-        post :sort, :on => :collection
-        resources :admission_phase_states, :controller => 'admission_methods/admission_phases/admission_phase_states' do
-          post :make_default, :on => :member
+      resources :admission_phases, controller: 'admission_methods/admission_phases' do
+        post :sort, on: :collection
+        resources :admission_phase_states, controller: 'admission_methods/admission_phases/admission_phase_states' do
+          post :make_default, on: :member
         end
-        resources :exams, :controller => 'admission_methods/admission_phases/exams' do
-          get :existing, :on => :collection
-          post :assign_existing, :on => :collection
-          delete :destroy_connection, :on => :member
+
+        resources :exams, controller: 'admission_methods/admission_phases/exams' do
+          collection do
+            get :existing, on: :collection
+            post :assign_existing, on: :collection
+          end
+          delete :destroy_connection, on: :member
         end
-        member do
-          get :show_phase_states
-        end
+
+        get :show_phase_states, on: :member
       end
     end
+
     resources :admission_periods do
-      member do
-        get :show_methods
-      end
+      get :show_methods, on: :member
     end
 
     resources :students do
       get :soft_delete, on: :member
     end
+
 	end
 end
