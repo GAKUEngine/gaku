69d58175d3069e9af5f6ad18738411fcc42704ec
Vassil Kalkov <vassilkalkov@gmail.com>
2013-07-12 12:39:52 +0300

    [admissions] Fix admission_methods

--- admissions/app/controllers/gaku/admin/admission_methods_controller.rb
+++ admissions/app/controllers/gaku/admin/admission_methods_controller.rb
@@ -5,31 +5,42 @@ module Gaku
       inherit_resources
       respond_to :js, :html
 
-      before_filter :count, :only => [:create, :destroy, :index]
-      before_filter :admission_method, :only => [:create]
-      
+      before_filter :count, only: %i(create destroy index)
+
       def create
+        @admission_method = AdmissionMethod.new(admission_method_params)
+
         if @admission_method.save
-          if !@admission_method.admission_phases.any?
-            admission_phase = AdmissionPhase.create(name: "Default phase", admission_method_id: @admission_method.id, phase_handler: 1)
-            #admission_phase_state = AdmissionPhaseState.create(@default_phase_data)
-            #admission_phase.admission_phase_states << @phase_state_data
+
+          unless @admission_method.admission_phases.any?
+            admission_phase = AdmissionPhase.create(name: 'Default phase', admission_method_id: @admission_method.id, phase_handler: 1)
           end
-          respond_to do |format|
-            flash.now[:notice] = t('notice.created', :resource => t('admission_phases.singular'))
-            format.js { render 'create' }
-          end
+
+          respond_with @admission_method
         end
       end
 
-      private
-        def count
-          @count = AdmissionMethod.count
-        end
+      protected
 
-        def admission_method
-          @admission_method = AdmissionMethod.new(params[:admission_method])
-        end
+      def resource_params
+        return [] if request.get?
+        [params.require(:admission_method).permit(admission_method_attr)]
+      end
+
+      private
+
+      def admission_method_params
+        params.require(:admission_method).permit(admission_method_attr)
+      end
+
+      def admission_method_attr
+        %i(name starting_applicant_number)
+      end
+
+      def count
+        @count = AdmissionMethod.count
+      end
+
     end
   end
 end
