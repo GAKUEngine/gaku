6167b015dbc390c5e2991bb077b8a659a21e6838
Georgi Tapalilov <tapalilov@gmail.com>
2013-07-16 19:48:20 +0300

    [fix] remove deprecations warning about process()

--- core/lib/gaku/core/testing_support/controller_requests.rb
+++ core/lib/gaku/core/testing_support/controller_requests.rb
@@ -28,22 +28,22 @@ module Gaku
     module TestingSupport
       module ControllerRequests
         def gaku_get(action, parameters = nil, session = nil, flash = nil)
-          process_gaku_action(action, parameters, session, flash, 'GET')
+          process_gaku_action(action,'GET', parameters, session, flash)
         end
 
         # Executes a request simulating POST HTTP method and set/volley the response
         def gaku_post(action, parameters = nil, session = nil, flash = nil)
-          process_gaku_action(action, parameters, session, flash, 'POST')
+          process_gaku_action(action, 'POST', parameters, session, flash)
         end
 
         # Executes a request simulating PUT HTTP method and set/volley the response
         def gaku_put(action, parameters = nil, session = nil, flash = nil)
-          process_gaku_action(action, parameters, session, flash, 'PUT')
+          process_gaku_action(action, 'PUT', parameters, session, flash)
         end
 
         # Executes a request simulating DELETE HTTP method and set/volley the response
         def gaku_delete(action, parameters = nil, session = nil, flash = nil)
-          process_gaku_action(action, parameters, session, flash, 'DELETE')
+          process_gaku_action(action, 'DELETE', parameters, session, flash)
         end
 
         def gaku_js_get(action, parameters = nil, session = nil, flash = nil)
@@ -69,9 +69,9 @@ module Gaku
         
         private
 
-          def process_gaku_action(action, parameters = nil, session = nil, flash = nil, method = 'GET')
+          def process_gaku_action(action, method = 'GET' ,parameters = nil, session = nil, flash = nil)
             parameters ||= {}
-            process(action, parameters.merge!(use_route: :gaku), session, flash, method)
+            process(action, method, parameters.merge!(use_route: :gaku), session, flash)
           end
 
       end
