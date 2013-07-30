093723e988f5d93aa6b0cbf1c545157001b890cb
Georgi Tapalilov <tapalilov@gmail.com>
2013-07-31 00:50:24 +0300

    [model] template model

---
+++ core/app/models/gaku/template.rb
@@ -0,0 +1,20 @@
+module Gaku
+  class Template < ActiveRecord::Base
+
+    has_attached_file :file
+
+    validates :name, :context, presence: true
+    validates_presence_of :file, on: :create
+
+    validates_attachment_content_type :file,
+        message: I18n.t(:'template.file_type_error'),
+        content_type:
+          [
+            'text/plain',
+            'application/vnd.ms-excel',
+            'application/vnd.oasis.opendocument.spreadsheet',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+          ]
+
+  end
+end
