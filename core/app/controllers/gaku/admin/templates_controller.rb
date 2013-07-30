80c508aee2138d20370cf279fe5f2e24f49a5eba
Georgi Tapalilov <tapalilov@gmail.com>
2013-07-31 00:39:20 +0300

    [controller] admin templates controller

---
+++ core/app/controllers/gaku/admin/templates_controller.rb
@@ -0,0 +1,49 @@
+module Gaku
+  module Admin
+    class TemplatesController < Admin::BaseController
+
+      load_and_authorize_resource class: Template
+
+      respond_to :js, :html
+
+      inherit_resources
+
+      before_action :count, only: %i(create destroy index)
+
+      def create
+        super do |format|
+          format.html { redirect_to :back }
+        end
+      end
+
+      def update
+        super do |format|
+          format.html { redirect_to :back }
+        end
+      end
+
+      def download
+        @template = Template.find(params[:id])
+        send_file @template.file.path
+      end
+
+      protected
+
+      def resource_params
+        return [] if request.get?
+        [params.require(:template).permit(attributes)]
+      end
+
+      private
+
+      def count
+        @count = Template.count
+      end
+
+      def attributes
+        %i(name context is_locked file)
+      end
+
+    end
+  end
+end
