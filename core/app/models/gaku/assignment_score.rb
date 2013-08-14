04fdb6f3fb378cc8d4559a608ed58f83a930e0cd
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-14 14:23:17 +0300

    [models] Improve AssignmentScore

--- core/app/models/gaku/assignment_score.rb
+++ core/app/models/gaku/assignment_score.rb
@@ -2,7 +2,12 @@ module Gaku
   class AssignmentScore < ActiveRecord::Base
     belongs_to :student
 
-    validates :score, presence: true
+    validates :score, :student, presence: true
     validates_associated :student
+
+    def to_s
+      score
+    end
+
   end
 end
--- core/spec/models/assignment_score_spec.rb
+++ core/spec/models/assignment_score_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe Gaku::AssignmentScore do
 
-  describe 'associations' do
+  describe 'relations' do
     it { should belong_to :student }
   end
 
   describe 'validations' do
-    it 'is invalid without a score' do
-      build(:assignment_score, score: nil).should_not be_valid
-    end
+    it { should validate_presence_of :score }
+    it { should validate_presence_of :student }
+  end
+
+  describe '#to_s' do
+    let(:assignment_score) { build(:assignment_score) }
+    specify { assignment_score.to_s.should eq assignment_score.score }
   end
 
 end
