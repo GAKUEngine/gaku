654c41c984d2350276933ab11e4eb8d507fd2a99
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-14 14:23:40 +0300

    [spec/models] Improve attachment_spec

--- core/spec/models/attachment_spec.rb
+++ core/spec/models/attachment_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 
 describe Gaku::Attachment do
 
+  describe 'concerns' do
+    it_behaves_like 'thrashable'
+  end
+
   describe 'associations' do
     it { should belong_to :attachable }
   end
 
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of(:asset).on(:create) }
     it { should have_attached_file :asset }
+  end
+
+  describe '#to_s' do
+    let(:attachment) { build(:attachment) }
+    specify { attachment.to_s.should eq attachment.name }
   end
 
 end
