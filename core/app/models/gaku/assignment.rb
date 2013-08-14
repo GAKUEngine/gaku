e9f5580c216f54f3fd9bb75f31f1084ecdcdc563
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-14 14:13:34 +0300

    [models] Improve Assignment

--- core/app/models/gaku/assignment.rb
+++ core/app/models/gaku/assignment.rb
@@ -1,9 +1,15 @@
 module Gaku
   class Assignment < ActiveRecord::Base
+
     belongs_to :syllabus
     belongs_to :grading_method
 
     validates :name, presence: true
     validates_associated :syllabus, :grading_method
+
+    def to_s
+      name
+    end
+
   end
 end
--- core/spec/models/assignment_spec.rb
+++ core/spec/models/assignment_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 
 describe Gaku::Assignment do
 
-  describe 'associations' do
+  describe 'relations' do
     it { should belong_to :syllabus }
     it { should belong_to :grading_method }
   end
 
   describe 'validations' do
     it { should validate_presence_of :name }
+  end
 
-    it 'is invalid without name' do
-      build(:assignment, name: nil).should_not be_valid
-    end
+  describe '#to_s' do
+    let(:assignment) { build(:assignment) }
+    specify { assignment.to_s.should eq assignment.name }
   end
 
 end
