9f3d895300c329925fd81fec963309c3c1c5bd33
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-14 19:39:18 +0300

    [models] Improve ClassGroupCourseEnrollment

--- core/app/models/gaku/class_group_course_enrollment.rb
+++ core/app/models/gaku/class_group_course_enrollment.rb
@@ -4,13 +4,14 @@ module Gaku
     belongs_to :class_group
     belongs_to :course
 
+    validates :class_group_id, presence: true
+
     validates :course_id,
               presence: true,
               uniqueness: {
                             scope: :class_group_id,
                             message: I18n.t(:'class_group.already_enrolled')
                           }
-
 
   end
 end
--- core/spec/models/class_group_course_enrollment_spec.rb
+++ core/spec/models/class_group_course_enrollment_spec.rb
@@ -8,6 +8,7 @@ describe Gaku::ClassGroupCourseEnrollment do
   end
 
   describe 'validations' do
+    it { should validate_presence_of :class_group_id }
     it { should validate_presence_of :course_id }
     it { should validate_uniqueness_of(:course_id).scoped_to(:class_group_id).with_message(/Already enrolled to the class group!/) }
   end
