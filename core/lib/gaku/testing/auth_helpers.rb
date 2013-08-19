ff3fa4d76d759c802765c116cab4b88b4e095ae0
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-19 23:41:19 +0300

    [spec] Clean auth_helpers

--- core/lib/gaku/testing/auth_helpers.rb
+++ core/lib/gaku/testing/auth_helpers.rb
@@ -4,13 +4,6 @@ include Warden::Test::Helpers
 module Gaku::Testing::AuthHelpers
 
   module Controller
-    def as_admin
-      before(:each) do
-        @request.env['devise.mapping'] = ::Devise.mappings[:admin]
-        sign_in create(:admin)
-      end
-    end
-
     def as(user)
       @request.env['devise.mapping'] = ::Devise.mappings[:admin] if user == 'admin'
       sign_in create("#{user.to_sym}_user")
@@ -20,18 +13,6 @@ module Gaku::Testing::AuthHelpers
   module Request
     def as(user)
       login_as create("#{user.to_sym}_user"), scope: :user
-    end
-
-    def log_in_as_student
-      user = create(:student_user)
-      login_as user, scope: :user
-    end
-
-    def sign_in_as!(user)
-      visit '/users/sign_in'
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: 'secret'
-      click_button 'sign_in'
     end
   end
 
