6d9303ff8106326fd3b6eb6a40b082affe7139e7
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-28 12:18:47 +0300

    [spec/requests] Add sign_up_spec

--- core/app/controllers/gaku/devise/registrations_controller.rb
+++ core/app/controllers/gaku/devise/registrations_controller.rb
@@ -7,6 +7,25 @@ class Gaku::Devise::RegistrationsController < Devise::RegistrationsController
     respond_with resource
   end
 
+  def create
+    build_resource(user_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def create_admin
     build_resource(user_params)
     admin_role = Gaku::Role.where(name: 'Admin').first_or_create!
