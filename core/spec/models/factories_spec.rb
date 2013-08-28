8ccf1b4a718bb1cbd143cdff170693ae906fc25c
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-28 13:34:31 +0300

    [spec/models] Add spec for invalid_factory in factories_spec

--- core/spec/models/factories_spec.rb
+++ core/spec/models/factories_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 FactoryGirl.factories.each do |factory|
   describe "Factory for :#{factory.name}" do
-    it 'is valid' do
-      build(factory.name).should be_valid
+    if factory.name.to_s.include? 'invalid'
+      it('is invalid') { build(factory.name).should be_invalid }
+    else
+      it('is valid') { build(factory.name).should be_valid }
     end
   end
 end
