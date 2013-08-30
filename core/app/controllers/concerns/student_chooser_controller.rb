d4aea5d21c916d88031a153d37442051bbd9f10b
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-30 12:24:07 +0300

    [controllers/concerns] Refactor StudentChooserController

--- core/app/controllers/concerns/student_chooser_controller.rb
+++ core/app/controllers/concerns/student_chooser_controller.rb
@@ -3,33 +3,50 @@ module StudentChooserController
   include Gaku::ClassNameDetector
 
   def student_chooser
+    set_enrollment_statuses
+    set_countries
+    set_students
 
+    set_collection
+    set_resource
+
+    respond_to { |format| format.js }
+  end
+
+  private
+
+  def set_countries
+    @countries = Gaku::Country.all.sort_by(&:name).collect{|s| [s.name, s.id]}
+  end
+
+  def set_enrollment_statuses
     @enrollment_status_applicant_code = Gaku::EnrollmentStatus.first_or_create(code: 'applicant').code
     @enrollment_status_enrolled_code = Gaku::EnrollmentStatus.first_or_create(code: 'enrolled').code
-
-
-    active_enrollment_statuses_codes = Gaku::EnrollmentStatus.active.pluck(:code)
-    @search = Gaku::Student.search(params[:q])
-    @students = @search.result.where(enrollment_status_code: active_enrollment_statuses_codes).page(params[:page]).per(Gaku::Preset.students_per_page)
-
-    @countries = Gaku::Country.all.sort_by(&:name).collect{|s| [s.name, s.id]}
     @enrollment_statuses =  Gaku::EnrollmentStatus.all.collect { |es| [es.name, es.code] }
     @enrollment_statuses << [t('undefined'), nil]
+  end
 
-    instance_variable_set("@#{class_name_underscored_plural}", class_name.constantize.all)
-    #@class_groups = class_name.constantize.all
-
-    instance_variable_set("@#{class_name_underscored}", class_name.constantize.find(params[:id]))
-    #@class_group = class_name.constantize.find(params[:id])
-
-    @enrolled_students = instance_variable_get("@#{class_name_underscored}").students.map {|i| i.id.to_s }
-    #@enrolled_students = @class_group.students.map {|i| i.id.to_s }
+  def set_students
+    active_enrollment_statuses_codes = Gaku::EnrollmentStatus.active.pluck(:code)
+    @search = Gaku::Student.search(params[:q])
+    @students = @search.result.where(enrollment_status_code: active_enrollment_statuses_codes)
+                              .page(params[:page])
+                              .per(Gaku::Preset.students_per_page)
 
     params[:selected_students].nil? ? @selected_students = [] : @selected_students = params[:selected_students]
 
-    respond_to do |format|
-      format.js
-    end
+    # Example: @enrolled_students = @class_group.students.map {|i| i.id.to_s }
+    @enrolled_students = instance_variable_get("@#{class_name_underscored}").students.map {|i| i.id.to_s }
+  end
+
+  def set_collection
+    # Example: @class_groups = class_name.constantize.all
+    instance_variable_set("@#{class_name_underscored_plural}", class_name.constantize.all)
+  end
+
+  def set_resource
+    # Example: @class_group = class_name.constantize.find(params[:id])
+    instance_variable_set("@#{class_name_underscored}", class_name.constantize.find(params[:id]))
   end
 
 end
