0a0c8b8eeaf4d5f41ebfeebcca3dd9a5ea6b676f
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-30 11:46:00 +0300

    [spec] Move include into flash_helpers

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -82,7 +82,6 @@ Spork.each_run do
     config.include Paperclip::Shoulda::Matchers
     config.include Devise::TestHelpers, type: :controller
     config.include Gaku::Core::UrlHelpers
-    config.include Gaku::Testing::FlashHelpers, type: :request
     config.include HandyControllerHelpers::AllHelpers, type: :controller
 
     config.alias_it_should_behave_like_to :ensures, 'ensures'
