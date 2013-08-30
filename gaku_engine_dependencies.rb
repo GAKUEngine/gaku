e910f137b8adb8d041074f9c8b685d9246897c4b
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-30 09:48:23 +0300

    [gem] Remove rack-mini-profiler

--- gaku_engine_dependencies.rb
+++ gaku_engine_dependencies.rb
@@ -9,10 +9,6 @@ gem 'uglifier',     '>= 1.0.3'
 gem 'paper_trail', github: 'airblade/paper_trail', branch: 'master'
 gem 'globalize3',  github: 'svenfuchs/globalize3', branch: 'rails4'
 
-group :development, :production do
-  gem 'rack-mini-profiler'
-end
-
 group :development do
   gem 'guard'
   gem 'guard-rspec'
