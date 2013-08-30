df98fab78027540c6699e34e146d5f8eff8b0032
Vassil Kalkov <vassilkalkov@gmail.com>
2013-08-30 11:48:38 +0300

    [spec] Add random order and migration check to spec_helper

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -47,6 +47,7 @@ end
 
 Spork.each_run do
   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
     config.mock_with :rspec
@@ -76,6 +77,7 @@ Spork.each_run do
 
     config.use_transactional_fixtures = false
     config.infer_base_class_for_anonymous_controllers = false
+    config.order = 'random'
 
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.include FactoryGirl::Syntax::Methods
