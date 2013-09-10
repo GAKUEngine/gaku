013415e8578b77ec2a27e66d4177a8c463613ea3
Vassil Kalkov <vassilkalkov@gmail.com>
2013-09-10 17:51:43 +0300

    [factories] Add invalid_commute_method_type factory

--- core/lib/gaku/testing/factories/commute_method_type_factory.rb
+++ core/lib/gaku/testing/factories/commute_method_type_factory.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
 
   factory :commute_method_type, class: Gaku::CommuteMethodType do
     name 'Car'
+
+    factory :invalid_commute_method_type do
+      name nil
+    end
   end
 
 end
