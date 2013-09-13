4831fc904600e70d0aa4370037441d3b64e05fe4
Vassil Kalkov <vassilkalkov@gmail.com>
2013-09-13 12:23:45 +0300

    [spec/models] Add to_s spec to achievement_spec

--- core/spec/models/achievement_spec.rb
+++ core/spec/models/achievement_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Gaku::Achievement do
 
-  describe 'associations' do
+  describe 'relations' do
     it { should have_many :student_achievements }
     it { should have_many(:students).through(:student_achievements) }
     it { should belong_to :external_school_record }
@@ -11,6 +11,11 @@ describe Gaku::Achievement do
   describe 'validations' do
     it { should have_attached_file :badge }
     it { should validate_presence_of :name }
+  end
+
+  describe '#to_s' do
+    let(:achievement) { build(:achievement) }
+    specify { achievement.to_s.should eq achievement.name }
   end
 
 end
