61bc9a501bf4015d0f784c8a36a99569255b9cda
Vassil Kalkov <vassilkalkov@gmail.com>
2013-09-13 12:18:52 +0300

    [controllers] Use responders in admin/achievements and improve controller spec

--- core/app/controllers/gaku/admin/achievements_controller.rb
+++ core/app/controllers/gaku/admin/achievements_controller.rb
@@ -1,35 +1,63 @@
 module Gaku
   class Admin::AchievementsController < Admin::BaseController
 
-    load_and_authorize_resource class: Achievement
+    #load_and_authorize_resource class: Achievement
 
-    respond_to :js, :html
+    respond_to :js,   only: %i( new edit destroy )
+    respond_to :html, only: %i( index create update )
 
-    inherit_resources
+    before_action :set_achievement, only: %i( edit update destroy )
 
-    before_filter :count, only: %i(index create destroy)
+    def index
+      @achievements = Achievement.all
+      @count = Achievement.count
+      respond_with @achievements
+    end
+
+    def new
+      @achievement = Achievement.new
+      respond_with @achievement
+    end
 
     def create
-      create! { [:admin, :achievements] }
+      @achievement = Achievement.new(achievement_params)
+      @achievement.save
+      @count = Achievement.count
+      flash[:notice] = t(:'notice.created', resource: t_resource)
+      respond_with [:admin, :achievements]
     end
 
-     def update
-      update! { [:admin, :achievements] }
+    def edit
     end
 
-    def resource_params
-      return [] if request.get?
-      [params.require(:achievement).permit(attributes)]
+    def update
+      @achievement.update(achievement_params)
+      flash[:notice] = t(:'notice.updated', resource: t_resource)
+      respond_with [:admin, :achievements]
+    end
+
+    def destroy
+      @achievement.destroy
+      @count = Achievement.count
+      respond_with @achievement
     end
 
     private
 
-    def count
-      @count = Achievement.count
+    def set_achievement
+      @achievement = Achievement.find(params[:id])
+    end
+
+    def achievement_params
+      params.require(:achievement).permit(attributes)
     end
 
     def attributes
       %i(name description authority badge external_school_record)
+    end
+
+    def t_resource
+      t(:'achievement.singular')
     end
 
   end
