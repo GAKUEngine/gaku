26ccac69e717e20f040138ae7e7deb5cb366a34f
snowsunny <snowsunny1107@gmail.com>
2013-09-16 22:03:15 +0900

    WIP students#index: fixed studetns chooser styles

--- core/app/helpers/gaku/autocomplete_helper.rb
+++ core/app/helpers/gaku/autocomplete_helper.rb
@@ -2,29 +2,28 @@ module Gaku::AutocompleteHelper
 
   def autocomplete_text_field(form, options = {})
     content_tag :div, class: 'col-sm-3' do
-      concat form.label options[:object_name], options[:tag_name]
-      concat form.text_field options[:object_name],
-                             class: 'js-autocomplete col-sm-12',
+      concat form.label options[:object_name], options[:tag_name], class: 'control-label'
+      concat form.text_field options[:object_name], class: 'form-control input-sm js-autocomplete',
         data: { autocomplete_source: load_autocomplete_data_students_path(class_name: options[:class_name], column: options[:column]) }
     end
   end
 
   def autocomplete_date_field(form, options = {})
     content_tag :div, class: 'col-sm-3' do
-      concat form.label options[:object_name], options[:tag_name]
-      concat form.text_field options[:object_name], class: 'col-sm-12', placeholder: t(:'date_placeholder')
+      concat form.label options[:object_name], options[:tag_name], class: 'control-label'
+      concat form.text_field options[:object_name], class: 'form-control input-sm', placeholder: t(:'date_placeholder')
     end
   end
 
   def autocomplete_select(form, options = {})
     content_tag :div, class: 'col-sm-3' do
-      concat form.label options[:object_name], options[:tag_name]
-      concat form.select  options[:object_name],
-                          options[:collection],
-                          { prompt: options[:prompt],
-                            selected: options[:selected]
-                          },
-                          options[:html_options]
+      concat form.label options[:object_name], options[:tag_name], class: 'control-label'
+      concat form.select(
+        options[:object_name],
+        options[:collection],
+        {prompt: options[:prompt], selected: options[:selected]},
+        class: 'form-control input-sm'
+      )
     end
   end
 
