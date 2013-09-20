7314df8c99f8d3192d80e269229ea931a99db702
Vassil Kalkov <vassilkalkov@gmail.com>
2013-09-20 13:38:56 +0300

    [controllers] Improve admin/disposals_controller and its spec

--- core/app/controllers/gaku/admin/disposals_controller.rb
+++ core/app/controllers/gaku/admin/disposals_controller.rb
@@ -8,39 +8,72 @@ module Gaku
     end
 
     def students
-      @students = Student.deleted.page(params[:page]).per(Preset.default_per_page)
+      @students = Student.deleted
+                         .page(params[:page])
+                         .per(Preset.default_per_page)
     end
 
     def teachers
-      @teachers = Teacher.deleted.page(params[:page]).per(Preset.default_per_page)
+      @teachers = Teacher.deleted
+                         .page(params[:page])
+                         .per(Preset.default_per_page)
     end
 
     def guardians
-      @guardians = Guardian.deleted.page(params[:page]).per(Preset.default_per_page)
+      @guardians = Guardian.deleted
+                           .page(params[:page])
+                           .per(Preset.default_per_page)
     end
 
     def exams
-      @exams = Exam.deleted.page(params[:page]).per(Preset.default_per_page)
+      @exams = Exam.deleted
+                   .page(params[:page])
+                   .per(Preset.default_per_page)
     end
 
     def course_groups
-      @course_groups = CourseGroup.deleted.page(params[:page]).per(Preset.default_per_page)
+      @course_groups = CourseGroup.deleted
+                                  .page(params[:page])
+                                  .per(Preset.default_per_page)
     end
 
     def attachments
-      @attachments = Attachment.includes(:attachable).deleted.page(params[:page]).per(Preset.default_per_page)
+      @attachments = Attachment.includes(:attachable)
+                               .deleted
+                               .page(params[:page])
+                               .per(Preset.default_per_page)
     end
 
     def addresses
-      @student_addresses = Address.includes(:addressable, :country).deleted.students.page(params[:page]).per(Preset.default_per_page)
-      @teacher_addresses = Address.includes(:addressable, :country).deleted.teachers.page(params[:page]).per(Preset.default_per_page)
+      @student_addresses = Address.includes(:addressable, :country)
+                                  .deleted
+                                  .students
+                                  .page(params[:page])
+                                  .per(Preset.default_per_page)
+
+      @teacher_addresses = Address.includes(:addressable, :country)
+                                  .deleted
+                                  .teachers
+                                  .page(params[:page])
+                                  .per(Preset.default_per_page)
+
       @students_count = Address.deleted.students.count
       @teachers_count = Address.deleted.teachers.count
     end
 
     def contacts
-      @student_contacts = Contact.includes(:contactable, :contact_type).deleted.students.page(params[:page]).per(Preset.default_per_page)
-      @teacher_contacts = Contact.includes(:contactable, :contact_type).deleted.teachers.page(params[:page]).per(Preset.default_per_page)
+      @student_contacts = Contact.includes(:contactable, :contact_type)
+                                 .deleted
+                                 .students
+                                 .page(params[:page])
+                                 .per(Preset.default_per_page)
+
+      @teacher_contacts = Contact.includes(:contactable, :contact_type)
+                                 .deleted
+                                 .teachers
+                                 .page(params[:page]).
+                                 per(Preset.default_per_page)
+
       @students_count = Contact.deleted.students.count
       @teachers_count = Contact.deleted.teachers.count
     end
