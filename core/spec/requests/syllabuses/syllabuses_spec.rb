002727ffaddb486e37fc085a1d7a33939039353a
Vassil Kalkov <vassilkalkov@gmail.com>
2013-10-09 12:02:48 +0300

    WIP: Refactor syllabuses

--- core/spec/requests/syllabuses/syllabuses_spec.rb
+++ core/spec/requests/syllabuses/syllabuses_spec.rb
@@ -3,14 +3,11 @@ require 'spec_helper'
 describe 'Syllabus' do
 
   before { as :admin }
+  before(:all) { set_resource 'syllabus' }
 
   let(:syllabus) { create(:syllabus, name: 'Biology', code: 'bio') }
 
-  before do
-    set_resource 'syllabus'
-  end
-
-  context '#new ', js: true do
+  context 'new', js: true do
 
     before do
       visit gaku.syllabuses_path
@@ -19,7 +16,6 @@ describe 'Syllabus' do
     end
 
     it 'creates' do
-      tr_count = size_of(table_rows)
       expect do
         fill_in 'syllabus_name', with: 'Syllabus1'
         fill_in 'syllabus_code', with: 'code1'
@@ -28,16 +24,12 @@ describe 'Syllabus' do
         wait_until_invisible submit
       end.to change(Gaku::Syllabus, :count).by 1
 
-      page.find(new_link).visible?
-      size_of(table_rows).should eq tr_count+1
+      has_content? 'Syllabus1'
+      has_content? 'code1'
+      count? '1'
     end
 
     it {has_validations?}
-
-    it 'cancels adding' do
-      ensure_cancel_creating_is_working
-    end
-
   end
 
   context 'existing' do
@@ -47,26 +39,7 @@ describe 'Syllabus' do
       visit gaku.syllabuses_path
     end
 
-    context 'lists' do
-
-      it 'lists existing syllabuses' do
-        page.should have_content('List Syllabuses')
-
-        within(table) do
-          page.should have_content('Biology')
-          page.should have_content('bio')
-        end
-        size_of(table_rows).should eq 2
-      end
-
-      it 'shows' do
-        within(table) { click show_link }
-        current_path.should == "/syllabuses/#{syllabus.id}"
-      end
-
-    end
-
-    context '#edit ', js: true do
+    context 'edit', js: true do
 
       before do
         syllabus
@@ -85,23 +58,17 @@ describe 'Syllabus' do
         fill_in 'syllabus_description', with: 'Maths Description'
 
         click submit
-
-        page.should have_content('Maths')
-        page.should have_content('math')
-
-        page.should_not have_content 'Biology'
-        page.should_not have_content 'bio'
-
-        edited_syllabus = Gaku::Syllabus.last
-        edited_syllabus.name.should eq 'Maths'
-        edited_syllabus.code.should eq 'math'
-        edited_syllabus.description.should eq 'Maths Description'
         flash_updated?
+
+        has_content? 'Maths'
+        has_content? 'math'
+
+        has_no_content? 'Biology'
+        has_no_content? 'bio'
+
+        expect(syllabus.reload.name).to eq 'Maths'
       end
 
-      it 'cancels editting' do
-        ensure_cancel_modal_is_working
-      end
 
       it 'edits from show view' do
         visit gaku.syllabus_path(syllabus)
@@ -114,11 +81,11 @@ describe 'Syllabus' do
 
         click submit
 
-        page.should have_content('Maths')
-        page.should have_content('math')
+        has_content?('Maths')
+        has_content?('math')
 
-        page.should_not have_content 'Biology'
-        page.should_not have_content 'bio'
+        has_no_content? 'Biology'
+        has_no_content? 'bio'
 
         edited_syllabus = Gaku::Syllabus.last
         edited_syllabus.name.should eq 'Maths'
@@ -134,7 +101,7 @@ describe 'Syllabus' do
         ensure_delete_is_working
       end.to change(Gaku::Syllabus, :count).by -1
 
-      page.should_not have_content("#{syllabus.code}")
+      has_no_content?("#{syllabus.code}")
 
     end
 
