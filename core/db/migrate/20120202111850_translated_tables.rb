8476716776f0d484c36c8e12b0defa08a150cf95
Vassil Kalkov <vassilkalkov@gmail.com>
2013-10-14 10:42:05 +0300

    Fix globalize

---
+++ core/db/migrate/20120202111850_translated_tables.rb
@@ -0,0 +1,52 @@
+class TranslatedTables < ActiveRecord::Migration
+
+  def up
+    create_table :gaku_attendance_types do |t|
+      t.string   :name
+      t.string   :color_code
+      t.boolean  :counted_absent
+      t.boolean  :disable_credit
+      t.float    :credit_rate
+      t.boolean  :auto_credit
+      t.timestamps
+    end
+    Gaku::AttendanceType.create_translation_table! name: :string
+
+    create_table :gaku_commute_method_types do |t|
+      t.string   :name
+      t.timestamps
+    end
+    Gaku::CommuteMethodType.create_translation_table! name: :string
+
+    create_table :gaku_enrollment_statuses do |t|
+      t.string   :code
+      t.string   :name
+      t.boolean  :active
+      t.boolean  :immutable
+      t.timestamps
+    end
+    Gaku::EnrollmentStatus.create_translation_table! name: :string
+
+    create_table :gaku_scholarship_statuses do |t|
+      t.string   :name
+      t.boolean  :default
+      t.timestamps
+    end
+    Gaku::ScholarshipStatus.create_translation_table! name: :string
+  end
+
+  def down
+    drop_table :gaku_attendance_types
+    Gaku::AttendanceType.drop_translation_table!
+
+    drop_table :gaku_commute_method_types
+    Gaku::CommuteMethodType.drop_translation_table!
+
+    drop_table :gaku_enrollment_statuses
+    Gaku::EnrollmentStatus.drop_translation_table!
+
+    drop_table :gaku_scholarship_statuses
+    Gaku::ScholarshipStatus.drop_translation_table!
+  end
+
+end
--- gaku_dependencies.rb
+++ gaku_dependencies.rb
@@ -1,7 +1,5 @@
 source 'http://production.s3.rubygems.org'
 
-gem 'globalize3',  github: 'globalize/globalize', branch: 'rails4', ref: '82b3b36308677745b261bd147dda00ed4560a25d'
-
 group :development do
   gem 'guard'
   gem 'rubocop'
