3089f0510ca5a09e54e5d19274fd9beafcfb07d6
Georgi Tapalilov <tapalilov@gmail.com>
2013-10-29 14:11:10 +0200

    [factory] exam and exam portion

--- core/lib/gaku/testing/factories/exam_factory.rb
+++ core/lib/gaku/testing/factories/exam_factory.rb
@@ -4,12 +4,16 @@ FactoryGirl.define do
     name 'Math exam'
     weight 4
     use_weighting true
-    after(:build) do |exam|
-      exam.exam_portions << build(:exam_portion, exam: exam)
-    end
 
     factory :invalid_exam do
       name nil
+    end
+  end
+
+  trait :with_portion do
+    after(:create) do |exam|
+      exam.exam_portions << create(:exam_portion, exam: exam)
+      exam.save
     end
   end
 
