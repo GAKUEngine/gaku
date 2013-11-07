e75473a77fbf5e50bdabb5202bc09590a9099f03
Vassil Kalkov <vassilkalkov@gmail.com>
2013-11-07 14:34:17 +0200

    Fix paperclip placeholder

--- core/app/models/concerns/picture.rb
+++ core/app/models/concerns/picture.rb
@@ -2,7 +2,7 @@ module Picture
   extend ActiveSupport::Concern
 
   included do
-    has_attached_file :picture, styles: {thumb: '256x256>'}, default_url: '/assets/pictures/thumb/missing.png'
+    has_attached_file :picture, styles: {thumb: '256x256>'}, default_url: ':placeholder'
   end
 
 end
