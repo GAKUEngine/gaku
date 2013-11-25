b3168c8ff5675d959b11a271ba60830a8b1d8c85
Vassil Kalkov <vassilkalkov@gmail.com>
2013-11-25 12:15:26 +0200

    Refactor class_groups/semesters_class_groups controller

--- core/app/controllers/gaku/class_groups/semester_class_groups_controller.rb
+++ core/app/controllers/gaku/class_groups/semester_class_groups_controller.rb
@@ -1,37 +1,62 @@
 module Gaku
   class ClassGroups::SemesterClassGroupsController < GakuController
 
-    load_and_authorize_resource :class_group, class: ClassGroup
+    respond_to :js, only: %i( new create edit update destroy )
 
-    respond_to :js, :html
+    before_action :set_class_group
+    before_action :set_semester_class_group, only: %i( edit update destroy )
+    before_action :set_semesters, only: %i( new edit )
 
-    inherit_resources
+    def new
+      @semester_class_group = SemesterClassGroup.new
+    end
 
-    belongs_to :class_group, parent_class: ClassGroup
+    def create
+      @semester_class_group = @class_group.semester_class_groups.build(semester_class_group_params)
+      @semester_class_group.save
+      set_count
+      respond_with @semester_class_group
+    end
 
-    before_filter :count, only: %I(create destroy)
-    before_filter :load_data, only: %I(new edit)
+    def edit
+    end
 
-    protected
+    def update
+      @semester_class_group.update(semester_class_group_params)
+      respond_with @semester_class_group
+    end
 
-    def resource_params
-      return [] if request.get?
-      [params.require(:semester_class_group).permit(attributes)]
+    def destroy
+      @semester_class_group.destroy
+      set_count
+      respond_with @semester_class_group
     end
 
     private
 
+    def semester_class_group_params
+      params.require(:semester_class_group).permit(attributes)
+    end
+
     def attributes
-      %i(semester_id)
+      %i( semester_id )
     end
 
-    def load_data
-      @semesters = Semester.all.map { |s| [s.to_s, s.id] }
+    def set_semesters
+      @semesters = Semester.all
     end
 
-    def count
-      class_group = ClassGroup.find(params[:class_group_id])
-      @count = class_group.semesters.count
+    def set_class_group
+      @class_group =  ClassGroup.find(params[:class_group_id])
+    end
+
+    def set_semester_class_group
+      #@semester_class_group =  SemesterClassGroup.find(params[:id])
+      @semester_class_group =  @class_group.semester_class_groups.find(params[:id])
+    end
+
+    def set_count
+      @count = @class_group.semesters.count
     end
 
   end
