60d991174f320a2290e0c1695c5186c10ed37ce7
Vassil Kalkov <vassilkalkov@gmail.com>
2014-01-13 14:11:52 +0200

    [core] Fix seeds validation isuues, use first_or_create! instead of create!

--- core/db/default/gaku/departments.rb
+++ core/db/default/gaku/departments.rb
@@ -7,7 +7,7 @@ departments = [
 
 departments.each do |d|
   I18n.locale = :en
-  department = Gaku::Department.create!(name: d[:name])
+  department = Gaku::Department.where(name: d[:name]).first_or_create!
 
   I18n.locale = :ja
   department.update_attributes(name: d[:name_ja])
