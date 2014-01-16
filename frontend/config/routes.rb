72cd41c4472dad950d0b0a0d2ba7a96f64b0741f
Georgi Tapalilov <tapalilov@gmail.com>
2014-01-16 15:16:39 +0200

    [routes] badges

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -83,7 +83,7 @@ Gaku::Core::Engine.routes.draw do
   resources :students, concerns: %i( addresses contacts notes pagination ) do
     resources :simple_grades,        controller: 'students/simple_grades', except: :show
     resources :commute_methods,      controller: 'students/commute_methods'
-    resources :student_achievements, controller: 'students/student_achievements', except: :show
+    resources :badges, controller: 'students/badges', except: :show
     resources :student_specialties,  controller: 'students/student_specialties',  except: :show
     resources :external_school_records,  controller: 'students/external_school_records',  except: :show
 
