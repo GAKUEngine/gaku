b2c6973de9fbf9673c4d3496c9ca9fade2e8db9b
Georgi Tapalilov <tapalilov@gmail.com>
2014-01-18 21:52:08 +0200

    [model] simple grades delegate methods to return simple grade type attributes

--- core/app/models/gaku/simple_grade.rb
+++ core/app/models/gaku/simple_grade.rb
@@ -4,10 +4,14 @@ module Gaku
     belongs_to :student
     belongs_to :simple_grade_type
 
+    %i( name school grading_method max_score passing_score ).each do |attribute|
+      delegate attribute, to: :simple_grade_type, allow_nil: true, prefix: true
+    end
+
     validates :student_id, :simple_grade_type_id, :score,  presence: true
 
     def to_s
-      name
+      simple_grade_type_name
     end
 
   end
