b54f2cf8094df666a6085ca049dbf52a1601fee6
Georgi Tapalilov <tapalilov@gmail.com>
2014-01-29 13:58:40 +0200

    [spec/feature] rename arguments to criteria

--- admin/spec/features/grading/grading_methods_spec.rb
+++ admin/spec/features/grading/grading_methods_spec.rb
@@ -6,7 +6,7 @@ describe 'Admin Grading Methods' do
   before(:all) { set_resource 'admin-grading-method' }
 
   let(:grading_method) { create(:grading_method, name: 'Bulgarian',
-                        arguments: {"A" => 95, "B" => 85 } ) }
+                        criteria: {"A" => 95, "B" => 85 } ) }
 
 
   context 'new', js: true do
@@ -27,7 +27,7 @@ describe 'Admin Grading Methods' do
 
       has_content? 'Bulgarian'
       count? 'Grading Methods list(1)'
-      expect(Gaku::GradingMethod.last.arguments).to eq({"A"=>"85"})
+      expect(Gaku::GradingMethod.last.criteria).to eq({"A"=>"85"})
     end
 
     it { has_validations? }
@@ -55,27 +55,27 @@ describe 'Admin Grading Methods' do
         has_no_content? 'Bulgarian'
         grading_method.reload
         expect(grading_method.name).to eq 'Japanese'
-        expect(grading_method.reload.arguments).to eq({'C' => '95', 'B' => '85'})
+        expect(grading_method.reload.criteria).to eq({'C' => '95', 'B' => '85'})
       end
 
-      it 'add arguments' do
-        click '.add-argument-row'
+      it 'add criteria' do
+        click '.add-criteria-row'
         all(:css, 'input.dynamicAttributeName').last.set 'C'
         all(:css, 'input.dynamicAttributeValue').last.set '75'
 
         click submit
 
         flash_updated?
-        expect(grading_method.reload.arguments).to eq({'A' => '95', 'B' => '85', 'C' => '75'})
+        expect(grading_method.reload.criteria).to eq({'A' => '95', 'B' => '85', 'C' => '75'})
       end
 
-      it 'remove arguments' do
-        all(:css, ".remove-argument-row").first.click
+      it 'remove criteria' do
+        all(:css, ".remove-criteria-row").first.click
         accept_alert
         click submit
 
         flash_updated?
-        expect(grading_method.reload.arguments).to eq({"B" => '85'})
+        expect(grading_method.reload.criteria).to eq({"B" => '85'})
       end
 
       it 'has validations' do
