f1b1e8562ab8e448a310256f8a4805db97d1f1c7
Vassil Kalkov <vassilkalkov@gmail.com>
2014-01-29 11:34:19 +0200

    [sample] Add sample data for school_years and semesters

--- sample/db/sample/gaku/class_groups.rb
+++ sample/db/sample/gaku/class_groups.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
+school_year = Gaku::SchoolYear.where(starting: Time.now - 3.months, ending: Time.now + 9.months).first_or_create!
+active_semester = Gaku::Semester.where(starting: Time.now - 2.months, ending: Time.now + 2.months).first_or_create!
+not_active_semester = Gaku::Semester.where(starting: Time.now + 3.months, ending: Time.now + 9.months).first_or_create!
+
 class_groups = [
   { name: 'Ms. Moore 3rd Grade', grade: 3 },
   { name: 'Advanced', grade: 5 },
@@ -20,3 +24,9 @@ say "Creating #{class_groups.size} class groups ...".yellow
 class_groups.each do |class_group|
   Gaku::ClassGroup.where(class_group).first_or_create!
 end
+
+active_class_group = Gaku::ClassGroup.where(name: 'Mr.Kalkov').first
+not_active_class_group = Gaku::ClassGroup.where(name: 'Mr.Kagetsuki').first
+
+Gaku::SemesterClassGroup.where(class_group_id: active_class_group.id, semester_id: active_semester.id).first_or_create!
+Gaku::SemesterClassGroup.where(class_group_id: not_active_class_group.id, semester_id: not_active_semester.id).first_or_create!
