d078166dc192f819dabc8bb83321f6906f67c7e2
Georgi Tapalilov <tapalilov@gmail.com>
2014-01-31 12:19:04 +0200

    [migration] remove attedance_rate column

--- core/db/migrate/20140130182100_change_fields_external_school_record.rb
+++ core/db/migrate/20140130182100_change_fields_external_school_record.rb
@@ -1,6 +1,7 @@
-class AddFieldsToExternalSchoolRecord < ActiveRecord::Migration
+class ChangeFieldsExternalSchoolRecord < ActiveRecord::Migration
   def change
     add_column :gaku_external_school_records, :units_absent, :integer
     add_column :gaku_external_school_records, :total_units,  :integer
+    remove_column :gaku_external_school_records, :attendance_rate
   end
 end
