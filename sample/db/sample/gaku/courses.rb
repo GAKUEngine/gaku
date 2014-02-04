09f169a1156cfa087146cc61cbab119a48edd465
影月 零 <zero@genshin.org>
2014-02-04 19:28:12 +0900

    slightly better sample data

--- sample/db/sample/gaku/courses.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-codes = ['Spring2012', 'Fall2012', 'Spring2013', 'Fall2013']
-
-say "Creating #{codes.size} courses ...".yellow
-
-codes.each do |code|
-  Gaku::Course.where(code: code).first_or_create!
-end
