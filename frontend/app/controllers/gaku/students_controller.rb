c80547dcaaf0c1353c9c6079ba6f6632b1473db7
Vassil Kalkov <vassilkalkov@gmail.com>
2014-02-04 12:25:17 +0200

    WIP: Save sstudent search in session

--- frontend/app/controllers/gaku/students_controller.rb
+++ frontend/app/controllers/gaku/students_controller.rb
@@ -41,18 +41,19 @@ module Gaku
     end
 
     def index
-      @search = Student.active.search(params[:q])
-      results = @search.result(distinct: true)
-      @students = results.page(params[:page])
-      @count = results.count
+      search
+      # @search = Student.active.search(params[:q])
+      # results = @search.result(distinct: true)
+      # @students = results.page(params[:page])
+      # @count = results.count
 
-      respond_with(@students) do |format|
-        format.pdf do
-          send_data render_to_string, filename: 'sido_yoroku.pdf',
-                                      type: 'application/pdf',
-                                      disposition: 'attachment'
-        end
-      end
+      # respond_with(@students) do |format|
+      #   format.pdf do
+      #     send_data render_to_string, filename: 'sido_yoroku.pdf',
+      #                                 type: 'application/pdf',
+      #                                 disposition: 'attachment'
+      #   end
+      # end
     end
 
     def chosen
@@ -75,21 +76,25 @@ module Gaku
     end
 
     def search
-      if params[:q]
-        if params[:q][:graduated_gteq]  || params[:q][:graduated_lteq] || params[:q][:admitted_gteq] || params[:q][:admitted_lteq]
-          @search = Student.search(params[:q])
+      if session[:q] != params[:q] && !params[:q].nil?
+        session[:q] = params[:q]
+      end
+
+      if session[:q]
+        if session[:q][:graduated_gteq]  || session[:q][:graduated_lteq] || session[:q][:admitted_gteq] || session[:q][:admitted_lteq]
+          @search = Student.search(session[:q])
         else
-          @search = Student.active.search(params[:q])
+          @search = Student.active.search(session[:q])
         end
 
-        if params[:q][:birth_date_gteq]  || params[:q][:birth_date_lteq] || params[:q][:age_gteq] ||params[:q][:age_lteq]
+        if session[:q][:birth_date_gteq]  || session[:q][:birth_date_lteq] || session[:q][:age_gteq] ||session[:q][:age_lteq]
           @search.sorts = 'birth_date desc'
         else
           @search.sorts = 'created_at desc'
         end
 
       else
-        @search = Student.active.search(params[:q])
+        @search = Student.active.search(session[:q])
       end
 
       results = @search.result(distinct: true)
