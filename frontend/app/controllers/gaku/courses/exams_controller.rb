34756b3374946d6299ee4dc38f97df4647dab9b7
Georgi Tapalilov <tapalilov@gmail.com>
2014-02-04 11:51:53 +0200

    [controller] exans

---
+++ frontend/app/controllers/gaku/courses/exams_controller.rb
@@ -0,0 +1,28 @@
+module Gaku
+  class Courses::ExamsController < GakuController
+
+    respond_to :html
+
+    def grading
+      @course = Course.find(params[:course_id])
+      @exam = Exam.find(params[:id])
+      @students = @course.students
+      init_portion_scores
+
+      respond_with @exam
+    end
+
+    private
+
+    def init_portion_scores
+      @students.each do |student|
+        @exam.exam_portions.each do |portion|
+          unless portion.exam_portion_scores.pluck(:student_id).include?(student.id)
+            ExamPortionScore.create!(exam_portion: portion, student: student)
+          end
+        end
+      end
+    end
+
+  end
+end
