209f6d5f161a6ac42af5375b7699978e959d11bc
影月 零 <zero@genshin.org>
2014-02-05 19:18:40 +0900

    made grading_method_router static

--- core/lib/gaku/grading_method_router.rb
+++ core/lib/gaku/grading_method_router.rb
@@ -1,25 +1,32 @@
 module Gaku
   class GradingMethodRouter
-    attr_reader :methods 
 
-    def initialize
-      @methods = { score:       Gaku::GradingMethods::Score,
-                   percentage:  Gaku::GradingMethods::Percentage,
-                   ordinal:     Gaku::GradingMethods::Ordinal,
-                   interval:    Gaku::GradingMethods::Interval,
-                   ratio:       Gaku::GradingMethods::Ratio,
-                   pass_fail:   Gaku::GradingMethods::PassFail
-      }
+    @@grading_methods = { score:       Gaku::GradingMethods::Score,
+        percentage:  Gaku::GradingMethods::Percentage,
+        ordinal:     Gaku::GradingMethods::Ordinal,
+        interval:    Gaku::GradingMethods::Interval,
+        ratio:       Gaku::GradingMethods::Ratio,
+        pass_fail:   Gaku::GradingMethods::PassFail
+    }
+
+    def self.grading_methods
+      @@grading_methods
     end
 
-    def get_class(method_symbol)
-      return @methods[method_symbol] if @methods[method_symbol].present?
-      return @methods[:score] # fallback to score - always safe
+    def self.get_class(method_symbol)
+      if @@grading_methods[method_symbol].present? then
+        return @@grading_methods[method_symbol]
+      end
+
+      return @@grading_methods[:score] # fallback to score - always safe
     end
 
-    def get_instance(method_symbol, gradable, students, criteria)
-      return @methods[method_symbol].new(gradable, students, cirteria) if @methods[method_symbol].present?
-      return @methods[:score].new(gradable, students, criteria) # fallback to score - always safe
+    def self.get_instance(method_symbol, gradable, students, criteria)
+      if @@grading_methods[method_symbol].present? then
+        return @@grading_methods[method_symbol].new(gradable, students, cirteria)
+      end
+
+      return @@grading_methods[:score].new(gradable, students, criteria) # fallback to score - always safe
     end
   end
 end
