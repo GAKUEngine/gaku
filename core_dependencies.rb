05af00a2a39689632cb43058f133c1b862b70190
Vassil Kalkov <vassilkalkov@gmail.com>
2014-02-22 16:24:20 +0200

    Update test gems

--- core_dependencies.rb
+++ core_dependencies.rb
@@ -1,10 +1,10 @@
 source 'http://production.s3.rubygems.org'
 
 group :test do
-  gem 'rspec-rails',              '~> 2.14'
+  gem 'rspec-rails',              '~> 2.14.1'
   gem 'factory_girl_rails',       '~> 4.2.1'
-  gem 'database_cleaner',         '~> 1.1.1'
-  gem 'shoulda-matchers',         '~> 2.4.0'
+  gem 'database_cleaner',         '~> 1.2'
+  gem 'shoulda-matchers',         '~> 2.5.0'
   gem 'simplecov'
   gem 'coveralls', require: false
 end
