d8f552ba541c1e9b8014314b2e88b900b15f7e6c
Georgi Tapalilov <tapalilov@gmail.com>
2014-02-24 13:53:22 +0200

    [gem] update factorygirl

--- core_dependencies.rb
+++ core_dependencies.rb
@@ -2,7 +2,7 @@ source 'http://production.s3.rubygems.org'
 
 group :test do
   gem 'rspec-rails',              '~> 2.14'
-  gem 'factory_girl_rails',       '~> 4.2.1'
+  gem 'factory_girl_rails',       '~> 4.4.0'
   gem 'database_cleaner',         '~> 1.1.1'
   gem 'shoulda-matchers',         '~> 2.4.0'
   gem 'simplecov'
