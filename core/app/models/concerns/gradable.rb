dbce714f30ad1cad2b5b3a5fdbca4c2365c30d13
Georgi Tapalilov <tapalilov@gmail.com>
2014-02-24 13:39:35 +0200

    [concern] gradable

---
+++ core/app/models/concerns/gradable.rb
@@ -0,0 +1,14 @@
+module Gradable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :grading_method_connectors, as: :gradable
+    has_many :grading_methods, through: :grading_method_connectors
+  end
+
+  def use_primary_grading_method_set
+    if Gaku::GradingMethodSet.primary
+      self.grading_methods << Gaku::GradingMethodSet.primary.grading_methods
+    end
+  end
+end
