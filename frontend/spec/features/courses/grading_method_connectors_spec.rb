b2f9faaf616c4f915393df771e7bb9aa1d0e6278
Georgi Tapalilov <tapalilov@gmail.com>
2014-02-27 10:04:08 +0200

    [spec/features] handle adding set grading methods

--- frontend/spec/features/courses/grading_method_connectors_spec.rb
+++ frontend/spec/features/courses/grading_method_connectors_spec.rb
@@ -5,6 +5,9 @@ describe 'Course Graidng Method Connectors' do
   let!(:course) { create(:course) }
   let(:grading_method) { create(:grading_method) }
 
+  let(:grading_method_set) { create(:grading_method_set, :with_grading_method)}
+
+
   before(:all) { set_resource 'course-grading-method-connector' }
 
   before { as :admin }
@@ -38,6 +41,42 @@ describe 'Course Graidng Method Connectors' do
     end
 
     it { has_validations? }
+  end
+
+  context 'grading method set', js: true do
+    before do
+      grading_method_set
+    end
+
+    it 'add set grading methods if not in collection' do
+      visit gaku.edit_course_path(course)
+
+      click '#course-grading-method-connectors-tab-link'
+      click '#new-set-course-grading-method-connector-link'
+      expect do
+        select grading_method_set.name,
+          from: 'grading_method_set_id'
+        click '#submit-set-course-grading-method-connector-button'
+        flash? 'Grading methods from grading method set added'
+
+      end.to change(Gaku::GradingMethodConnector, :count).by 1
+      within(table) { has_content? grading_method_set.grading_methods.first.name }
+    end
+
+    it 'do not add set grading methods if already in collection' do
+      grading_method_set
+      course.grading_method_connectors.create( grading_method: grading_method_set.grading_methods.first)
+      visit gaku.edit_course_path(course)
+
+      click '#course-grading-method-connectors-tab-link'
+      click '#new-set-course-grading-method-connector-link'
+      expect do
+        select grading_method_set.name,
+          from: 'grading_method_set_id'
+        click '#submit-set-course-grading-method-connector-button'
+        flash? 'Grading methods are already added'
+      end.to change(Gaku::GradingMethodConnector, :count).by 0
+    end
   end
 
   context 'existing', js: true do
