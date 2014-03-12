7a679cc3e4f1c0d6dcb1b86891518160b842e207
Georgi Tapalilov <tapalilov@gmail.com>
2014-03-12 12:33:57 +0200

    [spec/feature] clear comments

--- frontend/spec/features/students/picture_spec.rb
+++ frontend/spec/features/students/picture_spec.rb
@@ -17,18 +17,10 @@ describe 'Student Picture' do
   it 'upload avatar', js: true do
     sleep 0.5
     click '#upload-picture-link'
-    # absolute_path = Rails.root + '../support/120x120.jpg'
-    # attach_file @file_name, absolute_path
     expect do
     attach_file :student_picture,
             File.join(Rails.root + '../support/120x120.jpg')
     click_button 'Upload'
-    # counter = 0
-    # while page.execute_script("return $.active").to_i > 0
-    #   counter += 1
-    #   sleep(0.1)
-    # raise "AJAX request took longer than 5 seconds." if counter >= 50
-    # end
     wait_for_ajax
     student.reload
     flash_updated?
