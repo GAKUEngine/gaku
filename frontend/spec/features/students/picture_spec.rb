fd6b15dda8b03b36a16a8895c604c1ca9fc10cee
Georgi Tapalilov <tapalilov@gmail.com>
2014-03-12 12:32:28 +0200

    [spec/feature] student ajax photo upload

--- frontend/spec/features/students/picture_spec.rb
+++ frontend/spec/features/students/picture_spec.rb
@@ -11,13 +11,28 @@ describe 'Student Picture' do
   end
 
   context 'show avatar' do
-    before { visit gaku.student_path(student) }
     it_behaves_like 'show avatar'
   end
 
-  context 'upload avatar' do
-    before { visit gaku.edit_student_path(student) }
-    it_behaves_like 'upload avatar'
+  it 'upload avatar', js: true do
+    sleep 0.5
+    click '#upload-picture-link'
+    # absolute_path = Rails.root + '../support/120x120.jpg'
+    # attach_file @file_name, absolute_path
+    expect do
+    attach_file :student_picture,
+            File.join(Rails.root + '../support/120x120.jpg')
+    click_button 'Upload'
+    # counter = 0
+    # while page.execute_script("return $.active").to_i > 0
+    #   counter += 1
+    #   sleep(0.1)
+    # raise "AJAX request took longer than 5 seconds." if counter >= 50
+    # end
+    wait_for_ajax
+    student.reload
+    flash_updated?
+    end.to change(student, :picture_content_type).from(nil).to('image/jpeg')
   end
 
 end
