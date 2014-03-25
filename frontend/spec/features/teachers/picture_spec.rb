b091da1ee5820b277be55bcc3b49f4dd867544b2
Georgi Tapalilov <tapalilov@gmail.com>
2014-03-25 14:28:40 +0200

    [spec/feature] ajax upload picture teacher

--- frontend/spec/features/teachers/picture_spec.rb
+++ frontend/spec/features/teachers/picture_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 describe 'Teacher Picture' do
 
-  let(:teacher) { create(:teacher) }
+   let(:teacher) { create(:teacher) }
 
   before do
     as :admin
-    @file_name = 'teacher_picture'
+    visit gaku.edit_teacher_path(teacher)
   end
 
   context 'show avatar' do
-    before { visit gaku.teacher_path(teacher) }
     it_behaves_like 'show avatar'
   end
 
-  context 'upload avatar' do
-    before { visit gaku.edit_teacher_path(teacher) }
-    it_behaves_like 'upload avatar'
+  it 'upload avatar', js: true do
+    sleep 0.5
+    click '#avatar-picture'
+    expect do
+    attach_file :teacher_picture,
+            File.join(Rails.root + '../support/120x120.jpg')
+    click_button 'Upload'
+    wait_for_ajax
+    teacher.reload
+    flash_updated?
+    end.to change(teacher, :picture_content_type).from(nil).to('image/jpeg')
   end
 
 end
