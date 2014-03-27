e0e0daacb1fb8fdb756bab2de410b97b13375a58
Vassil Kalkov <vassilkalkov@gmail.com>
2014-03-27 11:24:05 +0200

    [core] Fix simple_grade_type factory

--- core/lib/gaku/testing/factories/simple_grade_type_factory.rb
+++ core/lib/gaku/testing/factories/simple_grade_type_factory.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
 
   factory :simple_grade_type, class: Gaku::SimpleGradeType do
     sequence(:name) { |n | "ruby_#{n}" }
-    #grading_method
-    #school
+    grading_method
+    school
 
     factory :invalid_simple_grade_type do
       name nil
