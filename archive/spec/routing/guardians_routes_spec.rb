ed79ef1327c0c016507a1e2f202a4dbbcd6b9a03
Vassil Kalkov <vassilkalkov@gmail.com>
2014-04-03 10:58:56 +0300

    [archive] Fix failing specs after guardians refactoring

--- archive/spec/routing/guardians_routes_spec.rb
+++ archive/spec/routing/guardians_routes_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_routing'
 
-describe Gaku::Students::GuardiansController do
+describe Gaku::GuardiansController do
 
   routes { Gaku::Core::Engine.routes }
 
@@ -8,7 +8,7 @@ describe Gaku::Students::GuardiansController do
 
     it 'routes to #soft_delete' do
       expect(patch: '/students/1/guardians/1/soft_delete').to route_to(
-        controller: 'gaku/students/guardians',
+        controller: 'gaku/guardians',
         action: 'soft_delete',
         student_id: '1',
         id: '1'
@@ -17,7 +17,7 @@ describe Gaku::Students::GuardiansController do
 
     it 'routes to #recovery' do
       expect(patch: '/students/1/guardians/1/recovery').to route_to(
-        controller: 'gaku/students/guardians',
+        controller: 'gaku/guardians',
         action: 'recovery',
         student_id: '1',
         id: '1'
@@ -26,7 +26,7 @@ describe Gaku::Students::GuardiansController do
 
     it 'routes to #destroy' do
       expect(delete: '/students/1/guardians/1').to route_to(
-        controller: 'gaku/students/guardians',
+        controller: 'gaku/guardians',
         action: 'destroy',
         student_id: '1',
         id: '1'
