7665c0dd3e7b9c5c6fba2246c6747020dd56e30d
Vassil Kalkov <vassilkalkov@gmail.com>
2014-04-11 11:41:33 +0300

    [admin] Fix routing specs

--- admin/spec/routing/admin/campuses/campuses_routing_spec.rb
+++ admin/spec/routing/admin/campuses/campuses_routing_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper_routing'
 
-describe Gaku::Admin::Schools::CampusesController do
+describe Gaku::Admin::CampusesController do
 
   routes { Gaku::Core::Engine.routes }
 
   describe 'member' do
     it 'routes to #show' do
       expect(get: 'admin/schools/1/campuses/1').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'show',
         school_id: '1',
         id: '1'
@@ -16,7 +16,7 @@ describe Gaku::Admin::Schools::CampusesController do
 
     it 'routes to #edit' do
       expect(get: 'admin/schools/1/campuses/1/edit').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'edit',
         school_id: '1',
         id: '1'
@@ -25,7 +25,7 @@ describe Gaku::Admin::Schools::CampusesController do
 
     it 'routes to #update' do
       expect(patch: 'admin/schools/1/campuses/1').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'update',
         school_id: '1',
         id: '1'
@@ -34,7 +34,7 @@ describe Gaku::Admin::Schools::CampusesController do
 
     it 'routes to #destroy' do
       expect(delete: 'admin/schools/1/campuses/1').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'destroy',
         school_id: '1',
         id: '1'
@@ -46,7 +46,7 @@ describe Gaku::Admin::Schools::CampusesController do
 
     it 'routes to #new' do
       expect(get: 'admin/schools/1/campuses/new').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'new',
         school_id: '1'
       )
@@ -54,7 +54,7 @@ describe Gaku::Admin::Schools::CampusesController do
 
     it 'routes to #create' do
       expect(post: 'admin/schools/1/campuses').to route_to(
-        controller: 'gaku/admin/schools/campuses',
+        controller: 'gaku/admin/campuses',
         action: 'create',
         school_id: '1'
       )
