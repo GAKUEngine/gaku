f7c0918a0d8f3c163f7147f80e6168bcd9c2dff9
Georgi Tapalilov <tapalilov@gmail.com>
2014-04-29 19:06:41 +0300

    [model] handle course, class_group and extracurricular activity counts in student record

--- core/app/models/gaku/enrollment.rb
+++ core/app/models/gaku/enrollment.rb
@@ -2,7 +2,7 @@ module Gaku
   class Enrollment < ActiveRecord::Base
 
     belongs_to :student
-    belongs_to :enrollmentable, polymorphic: true
+    belongs_to :enrollmentable, polymorphic: true, counter_cache: true
 
     validates :enrollmentable_type, :enrollmentable_id, :student_id, presence: true
 
@@ -17,6 +17,26 @@ module Gaku
         in: %w(Gaku::Course Gaku::ClassGroup Gaku::ExtracurricularActivity),
         message: "%{value} is not a valid"
       }
+
+    before_create :increment_enrollmentable_counter
+    before_destroy :decrement_enrollmentable_counter
+
+    private
+
+
+    def increment_enrollmentable_counter
+      Student.increment_counter(resource_name_counter, student.id) if student
+    end
+
+    def decrement_enrollmentable_counter
+      Student.decrement_counter(resource_name_counter, student.id) if student
+    end
+
+    def resource_name_counter
+      if enrollmentable_type
+        enrollmentable_type.demodulize.underscore.pluralize.concat('_count')
+      end
+    end
 
   end
 end
