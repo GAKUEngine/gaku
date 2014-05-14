39e4f7dc1e89b1fdff2537c182cbc98a992a5fb7
Georgi Tapalilov <tapalilov@gmail.com>
2014-05-15 00:10:40 +0300

    [factory] add factories to semester for different state

--- core/lib/gaku/testing/factories/semester_factory.rb
+++ core/lib/gaku/testing/factories/semester_factory.rb
@@ -8,6 +8,22 @@ FactoryGirl.define do
       starting { Date.parse('2014-04-08') }
       ending { Date.parse('2013-04-08') }
     end
+
+    factory :active_semester do
+      starting { Time.now - 1.day }
+      ending  { Time.now + 1.day }
+    end
+
+    factory :not_started_semester do
+      starting { Time.now + 1.day }
+      ending  { Time.now + 2.day }
+    end
+
+    factory :ended_semester do
+      starting { Time.now  - 2.day }
+      ending  { Time.now - 1.day }
+    end
+
   end
 
 end
