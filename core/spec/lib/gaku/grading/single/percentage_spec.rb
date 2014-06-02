76761b0523c1256ba74748efac2e356eae37c1f0
Georgi Tapalilov <tapalilov@gmail.com>
2014-06-02 14:58:53 +0300

    [spec/libs] grading methods

--- core/spec/lib/gaku/grading/single/percentage_spec.rb
+++ core/spec/lib/gaku/grading/single/percentage_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper_models'
 
-describe Gaku::Grading::Single::Score do
+describe Gaku::Grading::Single::Percentage do
 
   let(:student) { create(:student) }
   let(:exam) { create(:exam) }
   let(:exam_portion1) { create(:exam_portion, exam: exam) }
   let(:exam_portion2) { create(:exam_portion, exam: exam) }
   let(:exam_portion_score1) do
-    create(:exam_portion_score, score: 5, student:student, exam_portion: exam_portion1)
+    create(:exam_portion_score, score: 28, student:student, exam_portion: exam_portion1)
   end
   let(:exam_portion_score2) do
-    create(:exam_portion_score, score: 10, student:student, exam_portion: exam_portion2)
+    create(:exam_portion_score, score: 35, student:student, exam_portion: exam_portion2)
   end
 
   subject { described_class.new(exam, student) }
@@ -23,7 +23,7 @@ describe Gaku::Grading::Single::Score do
 
   describe 'without exam portion scores' do
     before do
-      exam_portion1; exam_portion2
+      exam_portion1; exam_portion2;
     end
     it 'creates exam_portion_score if not exist' do
       expect do
@@ -33,13 +33,11 @@ describe Gaku::Grading::Single::Score do
   end
 
   describe 'with exam portion scores' do
-
     before do
       exam
-      exam_portion1; exam_portion2
-      exam_portion_score1;exam_portion_score2
+      exam_portion1;exam_portion2
+      exam_portion_score1; exam_portion_score2
     end
-
     it "doesn't create exam_portion_score if exists" do
       expect do
         subject.grade_exam
@@ -47,7 +45,7 @@ describe Gaku::Grading::Single::Score do
     end
 
     it 'calculates scores from exam_portion_scores' do
-      expect(subject.grade_exam).to eq({ id: student.id, score: 15.0 })
+      expect(subject.grade_exam).to eq({ id: student.id, score: 31.5 })
     end
   end
 
