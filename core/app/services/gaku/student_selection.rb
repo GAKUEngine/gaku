758b6e714689e52ae4a42a5c7810f22c311ea349
Georgi Tapalilov <tapalilov@gmail.com>
2014-06-17 15:53:01 +0300

    [service] handle collection and remove collection in student selection

--- core/app/services/gaku/student_selection.rb
+++ core/app/services/gaku/student_selection.rb
@@ -22,10 +22,35 @@ module Gaku
         self.all
       end
 
+      def collection(students)
+        prepared_students = prepare_not_added_students(students)
+        $redis.rpush(:student_selection, prepared_students) unless prepared_students.blank?
+        self.all
+      end
+
+      def remove_collection(students)
+        students.map{ |student| json_for(student) }.each do |student|
+          $redis.lrem(:student_selection, 0, student)
+        end
+        self.all
+      end
+
       private
 
       def json_for(student)
-        { id: "#{student.id}", full_name: "#{student.surname} #{student.name}" }.to_json
+        hash_for(student).to_json
+      end
+
+      def hash_for(student)
+        { id: "#{student.id}", full_name: "#{student.surname} #{student.name}" }
+      end
+
+      def not_added_students(students)
+        students.reject {|student| all.include?(hash_for(student).stringify_keys) }
+      end
+
+      def prepare_not_added_students(students)
+        not_added_students(students).map{ |student| json_for(student) }
       end
 
     end
