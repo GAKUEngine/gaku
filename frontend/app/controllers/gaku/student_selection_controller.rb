e03c1320b06f22acf48f854ca50b93041af928b4
Georgi Tapalilov <tapalilov@gmail.com>
2014-06-17 15:51:04 +0300

    [controller] handle check/uncheck

--- frontend/app/controllers/gaku/student_selection_controller.rb
+++ frontend/app/controllers/gaku/student_selection_controller.rb
@@ -3,20 +3,31 @@ module Gaku
 
     respond_to :js,             only: %i( index add remove clear )
     before_action :set_student, only: %i( add remove )
+    before_action :set_students, only: %i( collection remove_collection )
 
     def add
       @selection = Gaku::StudentSelection.add(@student)
       respond_with @selection
     end
 
+
     def remove
       @selection = Gaku::StudentSelection.remove(@student)
       respond_with @selection
     end
 
+    def collection
+      @selection = Gaku::StudentSelection.collection(@students)
+      render 'index'
+    end
+
+    def remove_collection
+      @selection = Gaku::StudentSelection.remove_collection(@students)
+      render 'index'
+    end
+
     def index
       @selection = Gaku::StudentSelection.all
-      respond_with @selection
     end
 
     def clear
@@ -28,6 +39,10 @@ module Gaku
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def set_students
+      @students = Student.where(id: params[:student_ids] )
     end
 
   end
