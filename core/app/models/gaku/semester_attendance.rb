ab61268dc63ab5019a21a9ff72d7a6add052e9e1
Georgi Tapalilov <tapalilov@gmail.com>
2014-07-17 15:33:25 +0300

    [model] semester attendance and assoc

---
+++ core/app/models/gaku/semester_attendance.rb
@@ -0,0 +1,14 @@
+module Gaku
+  class SemesterAttendance < ActiveRecord::Base
+
+    belongs_to :student
+    belongs_to :semester
+
+    def self.grouped_for_table
+      all.group_by(&:student_id).map do |k, v|
+        [k, v.group_by(&:semester_id).map {|x,y| [x, y.first]}.to_h ]
+      end.to_h
+    end
+
+  end
+end
