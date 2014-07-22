c58acfa29789c4120c969950a9281bec46d9a8ed
Georgi Tapalilov <tapalilov@gmail.com>
2014-07-22 17:41:46 +0300

    [migration] student review categories table

---
+++ core/db/migrate/20140718105226_create_student_review_categories_table.rb
@@ -0,0 +1,13 @@
+class CreateStudentReviewCategoriesTable < ActiveRecord::Migration
+  def up
+    create_table :gaku_student_review_categories do |t|
+      t.string :name
+      t.timestamps
+    end
+    Gaku::StudentReviewCategory.create_translation_table! name: :string
+  end
+  def down
+    drop_table :gaku_student_review_categories
+    Gaku::StudentReviewCategory.drop_translation_table!
+  end
+end
