94391597e2ad835f78a5765b11f4ca0a0d9b8ce1
Georgi Tapalilov <tapalilov@gmail.com>
2014-08-05 17:38:26 +0300

    [spec/service] use initializer

--- core/spec/services/student_selection_spec.rb
+++ core/spec/services/student_selection_spec.rb
@@ -4,6 +4,7 @@ describe Gaku::StudentSelection do
 
   let(:student) { create(:student) }
   let(:student2) { create(:student) }
+  let!(:user) { create(:user) }
 
   before do
     $redis.del(:student_selection)
@@ -11,53 +12,53 @@ describe Gaku::StudentSelection do
 
   describe '.add' do
     it 'adds student to selection' do
-      expect(described_class.add(student)).to eq([student])
+      expect(described_class.new(user).add(student)).to eq([student])
     end
   end
 
   describe '.remove' do
     it 'removes student from selection' do
-      described_class.add(student)
-      expect(described_class.remove(student)).to eq []
+      described_class.new(user).add(student)
+      expect(described_class.new(user).remove(student)).to eq []
     end
   end
 
   describe '.all' do
     it 'returns all selected students' do
-      described_class.add(student)
-      expect(described_class.all).to eq([student.id.to_s])
+      described_class.new(user).add(student)
+      expect(described_class.new(user).all).to eq([student.id.to_s])
     end
   end
 
   describe '.students' do
     it 'returns students object selected students' do
-      described_class.add(student)
-      expect(described_class.students).to eq([student])
+      described_class.new(user).add(student)
+      expect(described_class.new(user).students).to eq([student])
     end
   end
 
   describe '.remove_all' do
     it 'removes all selected students' do
-      described_class.add(student)
-      expect(described_class.remove_all).to eq []
+      described_class.new(user).add(student)
+      expect(described_class.new(user).remove_all).to eq []
     end
   end
 
   describe '.collection' do
     it 'add collection to selected students' do
 
-      described_class.collection([student, student2])
-      expect(described_class.all).to eq [student.id.to_s, student2.id.to_s]
-      expect(described_class.students).to eq [student, student2]
+      described_class.new(user).collection([student, student2])
+      expect(described_class.new(user).all).to eq [student.id.to_s, student2.id.to_s]
+      expect(described_class.new(user).students).to eq [student, student2]
     end
   end
 
   describe '.remove_collection' do
     it 'add collection to selected students' do
-      described_class.collection([student, student2])
-      described_class.remove_collection([student, student2])
-      expect(described_class.all).to eq []
-      expect(described_class.students).to eq []
+      described_class.new(user).collection([student, student2])
+      described_class.new(user).remove_collection([student, student2])
+      expect(described_class.new(user).all).to eq []
+      expect(described_class.new(user).students).to eq []
     end
   end
 
