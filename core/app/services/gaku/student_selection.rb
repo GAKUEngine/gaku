9b894e36f933f78e9eeae02430f7714885269986
Georgi Tapalilov <tapalilov@gmail.com>
2014-08-05 17:39:07 +0300

    [service] student selection service refactor

--- core/app/services/gaku/student_selection.rb
+++ core/app/services/gaku/student_selection.rb
@@ -1,52 +1,56 @@
 module Gaku
   class StudentSelection
 
-    class << self
+    attr_accessor :user
 
+    def initialize(user)
+      @user = user
+    end
 
-      def all
-        $redis.lrange(:student_selection, 0, -1)
+    def all
+      $redis.lrange(user_selection, 0, -1)
+    end
+
+    def remove_all
+      $redis.del(user_selection)
+      self.students
+    end
+
+    def add(student)
+      $redis.rpush(user_selection, student.id)
+      self.students
+    end
+
+    def remove(student)
+      $redis.lrem(user_selection, 0, student.id)
+      self.students
+    end
+
+    def collection(students)
+      new_students = not_added_students(students)
+      $redis.rpush(user_selection, new_students) unless new_students.blank?
+      self.students
+    end
+
+    def remove_collection(students)
+      students.each do |student|
+        $redis.lrem(user_selection  , 0, student.id)
       end
+      self.students
+    end
 
-      def remove_all
-        $redis.del(:student_selection)
-        self.students
-      end
+    def students
+      Student.where(id: all)
+    end
 
-      def add(student)
-        $redis.rpush(:student_selection, student.id)
-        self.students
-      end
+    private
 
-      def remove(student)
-        $redis.lrem(:student_selection, 0, student.id)
-        self.students
-      end
+    def user_selection
+      %Q(user:#{user.id}:student_selection)
+    end
 
-      def collection(students)
-        new_students = not_added_students(students)
-        puts new_students.inspect
-        $redis.rpush(:student_selection, new_students) unless new_students.blank?
-        self.students
-      end
-
-      def remove_collection(students)
-        students.each do |student|
-          $redis.lrem(:student_selection, 0, student.id)
-        end
-        self.students
-      end
-
-      def students
-        Student.where(id: all)
-      end
-
-      private
-
-      def not_added_students(students)
-        students.reject { |student| all.include?(student.id) }.map(&:id)
-      end
-
+    def not_added_students(students)
+      students.reject { |student| all.include?(student.id) }.map(&:id)
     end
 
   end
