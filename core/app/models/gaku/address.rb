7c8d2cd7ccbfc53285e42ab0def93022a1319acf
Georgi Tapalilov <tapalilov@gmail.com>
2014-08-06 00:59:44 +0300

    [fixes] #2

--- core/app/models/gaku/address.rb
+++ core/app/models/gaku/address.rb
@@ -23,7 +23,7 @@ module Gaku
     def make_primary
       addresses.where.not(id: id).update_all(primary: false)
       update_attribute(:primary, true)
-      update_primary_address_field
+      # update_primary_address_field
     end
 
     def primary?
