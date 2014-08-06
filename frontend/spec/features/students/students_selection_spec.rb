ad797c49ba3443ca9701e116fa5d67c943ab2fa6
snowsunny <snowsunny1107@gmail.com>
2014-08-06 19:17:38 +0900

    fixed spec & top_bar

--- frontend/spec/features/students/students_selection_spec.rb
+++ frontend/spec/features/students/students_selection_spec.rb
@@ -38,7 +38,7 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student.id}").set(true)
       find(:css, "input#student-#{student2.id}").set(true)
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
 
       visit gaku.root_path
       visit gaku.students_path
@@ -46,14 +46,14 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student.id}").should be_checked
       find(:css, "input#student-#{student2.id}").should be_checked
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
     end
 
     it 'unchecks preselected students', js: true do
       find(:css, "input#student-#{student.id}").set(true)
       find(:css, "input#student-#{student2.id}").set(true)
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
 
       visit gaku.root_path
       visit gaku.students_path
@@ -64,39 +64,43 @@ describe 'Selecting Students', type: :feature do
 
       find(:css, "input#student-#{student.id}").should_not be_checked
       page.has_selector? '#students-checked-div'
-      expect(page.has_content?('Chosen students(1)')).to eq true
+      expect(page.has_content?('Collector1')).to eq true
     end
 
     it 'clears preselected students', js: true do
       find(:css, "input#student-#{student.id}").set(true)
       find(:css, "input#student-#{student2.id}").set(true)
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
 
       visit gaku.root_path
       visit gaku.students_path
 
       find(:css, "input#student-#{student.id}").should be_checked
       find(:css, "input#student-#{student2.id}").should be_checked
-      find(:css, '#clear-student-selection').click
 
+      find(:css, '#btn-students-chooser').click
+      sleep 1
+      
+      find(:css, '#clear-student-selection').click
       sleep 1
 
       find(:css, "input#student-#{student.id}").should_not be_checked
       find(:css, "input#student-#{student2.id}").should_not be_checked
       !page.has_selector? '#students-checked-div'
-      expect(page.has_content?('Chosen students(1)')).to eq false
+      expect(page.has_content?('Collector1')).to eq false
     end
 
     it 'removes selected student', js: true do
       find(:css, "input#student-#{student.id}").set(true)
+      find(:css, '#btn-students-chooser').click
+      sleep 1
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(1)'
-      click_link 'Show'
+      page.has_content? 'Collector1'
       find(:css, '.remove-student').click
       sleep 1
       find(:css, "input#student-#{student.id}").should_not be_checked
-      page.has_content? 'Chosen students(1)'
+      page.has_content? 'Collector1'
 
       visit gaku.root_path
       visit gaku.students_path
@@ -104,14 +108,14 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student.id}").should_not be_checked
 
       !page.has_selector? '#students-checked-div'
-      expect(page.has_content?('Chosen students(1)')).to eq false
+      expect(page.has_content?('Collector1')).to eq false
     end
 
     it 'click icon and add all selected students', js: true do
       click '.check-all'
 
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
 
       visit gaku.root_path
       visit gaku.students_path
@@ -119,7 +123,7 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student.id}").should be_checked
       find(:css, "input#student-#{student2.id}").should be_checked
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
     end
 
     it 'click icon twice and remove all selected students', js: true do
@@ -129,7 +133,7 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student2.id}").should be_checked
 
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(2)'
+      page.has_content? 'Collector2'
 
       click '.check-all'
 
@@ -137,7 +141,7 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student2.id}").should_not be_checked
 
       page.has_selector? '#students-checked-div'
-      page.has_content? 'Chosen students(0)'
+      page.has_content? 'Collector0'
 
       visit gaku.root_path
       visit gaku.students_path
@@ -145,7 +149,7 @@ describe 'Selecting Students', type: :feature do
       find(:css, "input#student-#{student.id}").should_not be_checked
       find(:css, "input#student-#{student2.id}").should_not be_checked
       !page.has_selector? '#students-checked-div'
-      !page.has_content? 'Chosen students(2)'
+      !page.has_content? 'Collector2'
     end
 
   end
