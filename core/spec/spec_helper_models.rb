b5858de7c810a5b0883a910ffa412ebfbe8fa652
Vassil Kalkov <vassilkalkov@gmail.com>
2015-02-09 14:17:27 +0200

    [core] Temporary switch off migration check in test env

--- core/spec/spec_helper_models.rb
+++ core/spec/spec_helper_models.rb
@@ -15,7 +15,7 @@ require 'shoulda-matchers'
 require 'gaku/testing/factories'
 
 Dir["#{File.dirname(__FILE__)}/support/models/**/*.rb"].each { |f| require f }
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
 
