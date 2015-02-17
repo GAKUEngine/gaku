5a9732cd56b0a2f6cd8e402ec5f74a970c2cbc27
Vassil Kalkov <vassilkalkov@gmail.com>
2015-02-17 17:18:51 +0200

    Add countries and states seeds based on carmen gem

---
+++ core/db/default/gaku/countries.rb
@@ -0,0 +1,14 @@
+require 'carmen'
+
+Carmen::Country.all.each do |country|
+  unless Gaku::Country.exists?(iso: country.alpha_2_code)
+
+    Gaku::Country.create!(name: country.name,
+      iso3: country.alpha_3_code,
+      iso: country.alpha_2_code,
+      iso_name: country.name.upcase,
+      numcode: country.numeric_code,
+      states_required: country.subregions?)
+
+  end
+end
