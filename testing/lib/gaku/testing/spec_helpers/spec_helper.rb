5b671276d7312ba25a2e6158cff02c342cadfb19
Vassil Kalkov <vassilkalkov@gmail.com>
2015-02-19 16:58:25 +0200

    [testing] Stop checking for pending migrations

--- testing/lib/gaku/testing/spec_helpers/spec_helper.rb
+++ testing/lib/gaku/testing/spec_helpers/spec_helper.rb
@@ -18,7 +18,7 @@ require 'gaku/core/url_helpers'
 
 # require 'gaku/testing/support/features'
 
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
 
