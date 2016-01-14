ec2e0bd62ade667f7e4cc572892d01b2d558f746
Georgi Tapalilov <tapalilov@gmail.com>
2016-01-14 21:28:33 +0200

    fix

--- frontend/config/initializers/assets.rb
+++ frontend/config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( gakuengine20.png gaku-logo.png )
+Rails.application.config.assets.precompile += %w( gakuengine20.png gaku-logo.png missing_thumb.png )
