6be2bcd890f96d6e9e8dc378c5447d6506774f00
Georgi Tapalilov <tapalilov@gmail.com>
2016-01-16 15:40:50 +0200

    [migrations] change student session

--- core/db/migrate/20140304101025_create_gaku_student_exam_session_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateGakuStudentExamSessionTable < ActiveRecord::Migration
-  def change
-    create_table :gaku_student_exam_sessions do |t|
-      t.references :student
-      t.references :exam_session
-    end
-  end
-end
