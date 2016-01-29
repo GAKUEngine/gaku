69c728e7985186775b70553d7c9b18dc5e520369
Georgi Tapalilov <tapalilov@gmail.com>
2016-01-30 00:18:43 +0200

    [lib/grading] only check exam portion

--- core/lib/gaku/grading/single/score.rb
+++ core/lib/gaku/grading/single/score.rb
@@ -4,7 +4,7 @@ class Gaku::Grading::Single::Score < Gaku::Grading::Single::BaseMethod
     @score = nil
     exam = @gradable
     exam.exam_portions.each_with_index do |exam_portion, index|
-      ep_score = @student.exam_portion_scores.where(gradable_type: gradable_scope.class.to_s, gradable_id: gradable_scope.id, exam_portion_id: exam_portion.id).first_or_create!
+      ep_score = @student.exam_portion_scores.find_by(gradable: gradable_scope, exam_portion_id: exam_portion.id)
       @score = @score.to_f + ep_score.score if ep_score.score
     end
 
