f83d254a423c9c74369fd9ab547dc7c1fb4e16d5
Georgi Tapalilov <tapalilov@gmail.com>
2018-02-05 14:13:10 +0200

    [core] validation and ransack attributes

--- core/app/models/gaku/student_guardian.rb
+++ core/app/models/gaku/student_guardian.rb
@@ -2,5 +2,7 @@ module Gaku
   class StudentGuardian < ActiveRecord::Base
     belongs_to :student, counter_cache: :guardians_count
     belongs_to :guardian
+
+    validates :student_id, uniqueness: { scope: :guardian_id }
   end
 end
