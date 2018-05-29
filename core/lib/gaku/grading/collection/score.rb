91b6ccea1264f2c3c50330048159111520eaedd2
Georgi Tapalilov <tapalilov@gmail.com>
2018-05-29 23:21:09 +0300

    Remove debug

--- core/lib/gaku/grading/collection/score.rb
+++ core/lib/gaku/grading/collection/score.rb
@@ -1,6 +1,5 @@
 class Gaku::Grading::Collection::Score < Gaku::Grading::Collection::BaseMethod
   def grade_exam
-    binding.pry
     @students.each do |student|
       @result << Gaku::Grading::Single::Score.new(@gradable, student, gradable_scope).grade_exam
     end
